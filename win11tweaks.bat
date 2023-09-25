807a856d2b5a7c6ccd3cbea71e0e66bb066b1a59
Vincent Grande <shoober420@gmail.com>
2023-09-25 04:08:18 -0700

    Update win11tweaks.bat

--- win11tweaks.bat
+++ win11tweaks.bat
@@ -1782,6 +1782,22 @@ winget uninstall "windows web experience pack"
 
 
 
+rem Remove Microsoft Edge
+takeown /s %computername% /u %username% /f "C:\Program Files (x86)\Microsoft\Edge"
+takeown /s %computername% /u %username% /f "C:\Program Files (x86)\Microsoft\EdgeCore"
+takeown /s %computername% /u %username% /f "C:\Program Files (x86)\Microsoft\EdgeUpdate"
+takeown /s %computername% /u %username% /f "C:\Program Files (x86)\Microsoft\EdgeWebView"
+icacls "C:\Program Files (x86)\Microsoft\Edge" /grant:r %username%:F
+icacls "C:\Program Files (x86)\Microsoft\EdgeCore" /grant:r %username%:F
+icacls "C:\Program Files (x86)\Microsoft\EdgeUpdate" /grant:r %username%:F
+icacls "C:\Program Files (x86)\Microsoft\EdgeWebView" /grant:r %username%:F
+taskkill /im MicrosoftEdgeUpdate.exe /f
+taskkill /im EdgeUpdate.exe /f
+ren "C:\Program Files (x86)\Microsoft\Edge" "Edge.bak"
+ren "C:\Program Files (x86)\Microsoft\EdgeCore" "EdgeCore.bak"
+ren "C:\Program Files (x86)\Microsoft\EdgeUpdate" "EdgeUpdate.bak"
+ren "C:\Program Files (x86)\Microsoft\EdgeWebView" "EdgeWebView.bak"
+
 rem Disable Microsoft Edge
 takeown /s %computername% /u %username% /f "C:\Windows\SystemApps\Microsoft.MicrosoftEdgeDevToolsClient_8wekyb3d8bbwe"
 icacls "C:\Windows\SystemApps\Microsoft.MicrosoftEdgeDevToolsClient_8wekyb3d8bbwe" /grant:r %username%:F
