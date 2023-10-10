3b7714d97c3d9be041c114dae4f4f82551b1dc37
Vincent Grande <shoober420@gmail.com>
2023-10-10 16:44:57 -0700

    Create removewindowsapps.bat

---
+++ removewindowsapps.bat
@@ -0,0 +1,25 @@
+rem Remove bloat windows apps
+
+winget uninstall Microsoft.OneDrive
+winget uninstall Microsoft.WindowsCamera
+winget uninstall Microsoft.GetHelp
+winget uninstall Microsoft.People
+winget uninstall Microsoft.Windows.PeopleExperienceHost
+winget uninstall Microsoft.Windows.CloudExperienceHost
+winget uninstall Microsoft.549981C3F5F10
+winget uninstall Microsoft.Windows.Photos
+winget uninstall Microsoft.YourPhone
+winget uninstall Microsoft.XboxGameOverlay
+winget uninstall Microsoft.XboxGamingOverlay
+winget uninstall Microsoft.XboxIdentityProvider
+winget uninstall Microsoft.XboxSpeechToTextOverlay
+winget uninstall Microsoft.XboxGameCallableUI
+winget uninstall Microsoft.MicrosoftEdge
+winget uninstall Microsoft.MicrosoftEdgeBeta
+winget uninstall Microsoft.MicrosoftEdgeStable
+winget uninstall Microsoft.MicrosoftEdgeDevToolsClient
+winget uninstall Microsoft.GamingServices
+winget uninstall MicrosoftWindows.Client.WebExperience
+winget uninstall Microsoft.Windows.ShellExperienceHost
+winget uninstall Microsoft.Windows.StartMenuExperienceHost
+winget uninstall "windows web experience pack"
