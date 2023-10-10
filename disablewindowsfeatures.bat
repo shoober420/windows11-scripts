74f44e0a31563d6448328efafb1ec1e61d8c8a43
Vincent Grande <shoober420@gmail.com>
2023-10-10 16:22:20 -0700

    Update disablewindowsfeatures.bat

--- disablewindowsfeatures.bat
+++ disablewindowsfeatures.bat
@@ -75,3 +75,30 @@ dism /online /Remove-Package /PackageName:Microsoft-Windows-WMPNetworkSharingSer
 dism /online /Remove-Package /PackageName:OpenSSH-Client /NoRestart
 dism /online /Remove-Package /PackageName:Microsoft-Windows-Internet-Browser-Package /NoRestart
 dism /online /Remove-Package /PackageName:Microsoft-Windows-MicrosoftEdgeDevToolsClient-Package /NoRestart
+
+rem Disable Windows update services required for DISM
+
+rem TLS 1.2 or lower is required for Windows Update to work, TLS 1.3 not supported yet
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client" /v "Enabled" /t REG_DWORD /d "0" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client" /v "DisabledByDefault" /t REG_DWORD /d "1" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\DTLS 1.2\Client" /v "Enabled" /t REG_DWORD /d "0" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\DTLS 1.2\Client" /v "DisabledByDefault" /t REG_DWORD /d "1" /f
+
+net stop UsoSvc
+net stop wisvc
+net stop wuauserv
+net stop BITS
+net stop TrustedInstaller
+net stop WaaSMedicSvc
+net stop cryptsvc
+net stop InventorySvc
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\UsoSvc" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\wisvc" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\wuauserv" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\BITS" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\TrustedInstaller" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\WaaSMedicSvc" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\InventorySvc" /v "Start" /t REG_DWORD /d "4" /f
+
+PAUSE
