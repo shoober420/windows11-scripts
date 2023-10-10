d8495e77c3108f05977dc088834aac154ae6c216
Vincent Grande <shoober420@gmail.com>
2023-10-10 11:12:54 -0700

    Update enablewmi.bat

--- enablewmi.bat
+++ enablewmi.bat
@@ -1,5 +1,6 @@
 rem Enable WMI (Windows Management Instrumentation)
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Winmgmt" /v "Start" /t REG_DWORD /d "2" /f
+sc config winmgmt start= auto
 net start winmgmt
 
 PAUSE
