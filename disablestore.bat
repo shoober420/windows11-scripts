3a29e1d718064e38bc5bdaebbdb753321f479bc9
Vincent Grande <shoober420@gmail.com>
2023-10-11 13:41:00 -0700

    Update disablestore.bat

--- disablestore.bat
+++ disablestore.bat
@@ -10,6 +10,8 @@ net stop InstallService
 net stop OneSyncSvc
 net stop ClipSVC
 net stop PushToInstall
+net stop cbdhsvc
+net stop LicenseManager
 
 rem reg add "HKLM\SYSTEM\CurrentControlSet\Services\AppXSvc" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\NcbService" /v "Start" /t REG_DWORD /d "4" /f
@@ -21,6 +23,8 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Services\InstallService" /v "Start" /t RE
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\OneSyncSvc" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\ClipSVC" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\PushToInstall" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\cbdhsvc" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\LicenseManager" /v "Start" /t REG_DWORD /d "4" /f
 
 rem Application Frame Host (to restore run "sfc /scannow")
 takeown /s %computername% /u %username% /f "%WinDir%\System32\ApplicationFrameHost.exe"
