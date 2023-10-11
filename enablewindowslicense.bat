25738c38162de5c8c347d241941b3178ae09b53b
Vincent Grande <shoober420@gmail.com>
2023-10-11 13:56:08 -0700

    Delete enablewindowslicense.bat

--- enablewindowslicense.bat
+++
@@ -1,7 +0,0 @@
-rem Enable Windows License Manager Service for activating Windows product key
-
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\LicenseManager" /v "Start" /t REG_DWORD /d "2" /f
-
-net start LicenseManager
-
-PAUSE
