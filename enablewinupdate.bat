4632d77788586eb13b48638e14b28ffa5a6fe973
Vincent Grande <shoober420@gmail.com>
2023-10-11 13:04:51 -0700

    Update enablewinupdate.bat

--- enablewinupdate.bat
+++ enablewinupdate.bat
@@ -7,6 +7,8 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Services\BITS" /v "Start" /t REG_DWORD /d
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\TrustedInstaller" /v "Start" /t REG_DWORD /d "2" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\WaaSMedicSvc" /v "Start" /t REG_DWORD /d "2" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\InventorySvc" /v "Start" /t REG_DWORD /d "2" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\DoSvc" /v "Start" /t REG_DWORD /d "2" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\DiagTrack" /v "Start" /t REG_DWORD /d "2" /f
 
 net start UsoSvc
 net start wisvc
@@ -16,6 +18,8 @@ net start TrustedInstaller
 net start WaaSMedicSvc
 net start cryptsvc
 net start InventorySvc
+net start DoSvc
+net start DiagTrack
 
 rem TLS 1.2 or lower is required for Windows Update to work, TLS 1.3 not supported yet
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client" /v "Enabled" /t REG_DWORD /d "1" /f
