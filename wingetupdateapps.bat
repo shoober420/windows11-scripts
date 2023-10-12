7ff09cf45e81b0026c5cfcec01dd32c9022703d2
Vincent Grande <shoober420@gmail.com>
2023-10-12 12:35:28 -0700

    Update wingetupdateapps.bat

--- wingetupdateapps.bat
+++ wingetupdateapps.bat
@@ -1,5 +1,13 @@
 rem Update winget apps
 
+rem winget requires "Application Information" service to be running
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Appinfo" /v "Start" /t REG_DWORD /d "2" /f
+net start Appinfo
+
 winget upgrade --all --include-unknown
+
+rem Disable "Application Information" service
+net stop Appinfo
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Appinfo" /v "Start" /t REG_DWORD /d "4" /f
 
 PAUSE
