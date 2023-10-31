8c219c0f2025d74e2607819057d257d518a0c441
Vincent Grande <shoober420@gmail.com>
2023-10-31 13:10:53 -0700

    Update setacl.bat

--- setacl.bat
+++ setacl.bat
@@ -1,5 +1,13 @@
 rem Script for taking ownership of Windows Defender services
 
+SetACL -on "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\SecurityHealthService" -ot file -actn trustee -trst n1:MyDomain\SomeUserOrGroup;ta:remtrst;w:dacl
+SetACL -on "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\wscsvc" -ot file -actn trustee -trst n1:MyDomain\SomeUserOrGroup;ta:remtrst;w:dacl
+SetACL -on "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\WdBoot" -ot file -actn trustee -trst n1:MyDomain\SomeUserOrGroup;ta:remtrst;w:dacl
+SetACL -on "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\WdFilter" -ot file -actn trustee -trst n1:MyDomain\SomeUserOrGroup;ta:remtrst;w:dacl
+SetACL -on "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\WdNisDrv" -ot file -actn trustee -trst n1:MyDomain\SomeUserOrGroup;ta:remtrst;w:dacl
+SetACL -on "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\WdNisSvc" -ot file -actn trustee -trst n1:MyDomain\SomeUserOrGroup;ta:remtrst;w:dacl
+SetACL -on "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\WinDefend" -ot file -actn trustee -trst n1:MyDomain\SomeUserOrGroup;ta:remtrst;w:dacl
+
 SetACL.exe -on "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\SecurityHealthService" -ot reg -actn setowner -ownr "n:Administrators"
 SetACL.exe -on "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\SecurityHealthService" -ot reg -actn ace -ace "n:Administrators;p:full"
 
