b96604e6d58074bcc058d9c2a9a7d6032effb5b8
Vincent Grande <shoober420@gmail.com>
2023-10-31 13:43:19 -0700

    Update disablewindowsdefender.bat

--- disablewindowsdefender.bat
+++ disablewindowsdefender.bat
@@ -10,3 +10,17 @@ reg add "HKLM\System\CurrentControlSet\Services\WdFilter" /v "Start" /t REG_DWOR
 reg add "HKLM\System\CurrentControlSet\Services\WdNisDrv" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\System\CurrentControlSet\Services\WdNisSvc" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\System\CurrentControlSet\Services\WinDefend" /v "Start" /t REG_DWORD /d "4" /f
+
+rem Disable Anti-malware Service Executable (to restore run "sfc /scannow")
+takeown /s %computername% /u %username% /f "C:\ProgramData\Microsoft\Windows Defender\Platform\4.18.23090.2008-0\MsMpEng.exe"
+icacls "C:\ProgramData\Microsoft\Windows Defender\Platform\4.18.23090.2008-0\MsMpEng.exe" /grant:r %username%:F
+taskkill /im MsMpEng.exe /f
+rem del "C:\ProgramData\Microsoft\Windows Defender\Platform\4.18.23090.2008-0\MsMpEng.exe" /s /f /q
+ren "C:\ProgramData\Microsoft\Windows Defender\Platform\4.18.23090.2008-0\MsMpEng.exe" "MsMpEng.exe.bak"
+
+rem Disable Microsoft Network Realtime Inspection Service (to restore run "sfc /scannow")
+takeown /s %computername% /u %username% /f "C:\ProgramData\Microsoft\Windows Defender\Platform\4.18.23090.2008-0\NisSrv.exe"
+icacls "C:\ProgramData\Microsoft\Windows Defender\Platform\4.18.23090.2008-0\NisSrv.exe" /grant:r %username%:F
+taskkill /im NisSrv.exe /f
+rem del "C:\ProgramData\Microsoft\Windows Defender\Platform\4.18.23090.2008-0\NisSrv.exe" /s /f /q
+ren "C:\ProgramData\Microsoft\Windows Defender\Platform\4.18.23090.2008-0\NisSrv.exe" "NisSrv.exe.bak"
