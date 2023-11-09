89f3151035900e1ad218df88de308c67a9fc0fa6
Vincent Grande <shoober420@gmail.com>
2023-11-08 22:14:59 -0800

    Update Win11Tweaks.bat

--- Win11Tweaks.bat
+++ Win11Tweaks.bat
@@ -2391,20 +2391,20 @@ rem https://smallvoid.com/article/winnt-process-scheduler-priority.html
 rem https://forums.blurbusters.com/viewtopic.php?t=8535
 rem 2A Hex = Short, Fixed, High foreground boost
 rem 29 Hex = Short, Fixed, Medium foreground boost
-rem 28 Hex = Short, Fixed, No foreground boost
+rem 28 Hex = Short, Fixed, No foreground boost (lag spikes)
 rem 26 Hex = Short, Variable, High foreground boost
 rem 25 Hex = Short, Variable, Medium foreground boost
-rem 24 Hex = Short, Variable, No foreground boost
+rem 24 Hex = Short, Variable, No foreground boost (lag spikes)
 rem 1A Hex = Long, Fixed, High foreground boost
 rem 19 Hex = Long, Fixed, Medium foreground boost
 rem 18 Hex = Long, Fixed, No foreground boost
 rem 16 Hex = Long, Variable, High foreground boost
 rem 15 Hex = Long, Variable, Medium foreground boost
 rem 14 Hex = Long, Variable, No foreground boost
-rem Best input: Short, Fixed, No boost (28) (causes big lag spikes)
+rem Best input: Short, Fixed, No boost (28)
 rem Best performance: Long, Variable, High Boost (16)
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\PriorityControl" /v "Win32PrioritySeparation" /t REG_DWORD /d "0x00000024" /f
-reg add "HKLM\SYSTEM\ControlSet001\Control\PriorityControl" /v "Win32PrioritySeparation" /t REG_DWORD /d "0x00000024" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\PriorityControl" /v "Win32PrioritySeparation" /t REG_DWORD /d "0x00000026" /f
+reg add "HKLM\SYSTEM\ControlSet001\Control\PriorityControl" /v "Win32PrioritySeparation" /t REG_DWORD /d "0x00000026" /f
 
 rem NVIDIA video card tweaks, last key may vary from 0000 to 0001 if dual NVIDIA/AMD machine is used
 rem Second to last key is the "Class Guid"
