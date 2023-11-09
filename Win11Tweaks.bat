80aafa6bef513586e74fa82ab64394a631c79d94
Vincent Grande <shoober420@gmail.com>
2023-11-08 23:50:01 -0800

    Update Win11Tweaks.bat

--- Win11Tweaks.bat
+++ Win11Tweaks.bat
@@ -2392,13 +2392,13 @@ rem https://forums.blurbusters.com/viewtopic.php?t=8535
 rem 2A Hex = Short, Fixed, High foreground boost
 rem 29 Hex = Short, Fixed, Medium foreground boost
 rem 28 Hex = Short, Fixed, No foreground boost (stuttering)
-rem 26 Hex = Short, Variable, High foreground boost
+rem 26 Hex = Short, Variable, High foreground boost (very good)
 rem 25 Hex = Short, Variable, Medium foreground boost
 rem 24 Hex = Short, Variable, No foreground boost (lag spikes)
 rem 1A Hex = Long, Fixed, High foreground boost
 rem 19 Hex = Long, Fixed, Medium foreground boost
 rem 18 Hex = Long, Fixed, No foreground boost
-rem 16 Hex = Long, Variable, High foreground boost
+rem 16 Hex = Long, Variable, High foreground boost (good)
 rem 15 Hex = Long, Variable, Medium foreground boost
 rem 14 Hex = Long, Variable, No foreground boost
 rem Best input: Short and Fixed
