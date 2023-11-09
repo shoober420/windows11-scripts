1f8c8330f38f14e35b88d334b911851566173de2
Vincent Grande <shoober420@gmail.com>
2023-11-08 17:46:57 -0800

    Update Win11Tweaks.bat

--- Win11Tweaks.bat
+++ Win11Tweaks.bat
@@ -2389,22 +2389,22 @@ ECHO Yes | xcopy "%USERPROFILE%\Downloads\windows11-batch-scripts-main\SetTimerR
 rem Thread Quantum
 rem https://smallvoid.com/article/winnt-process-scheduler-priority.html
 rem https://forums.blurbusters.com/viewtopic.php?t=8535
-rem 2A Hex = Short, Fixed , High foreground boost.
-rem 29 Hex = Short, Fixed , Medium foreground boost.
-rem 28 Hex = Short, Fixed , No foreground boost.
-rem 26 Hex = Short, Variable , High foreground boost.
-rem 25 Hex = Short, Variable , Medium foreground boost.
-rem 24 Hex = Short, Variable , No foreground boost
-rem 1A Hex = Long, Fixed, High foreground boost.
-rem 19 Hex = Long, Fixed, Medium foreground boost.
-rem 18 Hex = Long, Fixed, No foreground boost.
-rem 16 Hex = Long, Variable, High foreground boost.
-rem 15 Hex = Long, Variable, Medium foreground boost.
-rem 14 Hex = Long, Variable, No foreground boost.
-rem Best input: Short, Fixed, No boost (28)
+rem 2A Hex = Short, Fixed, High foreground boost
+rem 29 Hex = Short, Fixed, Medium foreground boost
+rem 28 Hex = Short, Fixed, No foreground boost
+rem 26 Hex = Short, Variable, High foreground boost
+rem 25 Hex = Short, Variable, Medium foreground boost
+rem 24 Hex = Short, Variable, No foreground boost
+rem 1A Hex = Long, Fixed, High foreground boost
+rem 19 Hex = Long, Fixed, Medium foreground boost
+rem 18 Hex = Long, Fixed, No foreground boost
+rem 16 Hex = Long, Variable, High foreground boost
+rem 15 Hex = Long, Variable, Medium foreground boost
+rem 14 Hex = Long, Variable, No foreground boost
+rem Best input: Short, Fixed, No boost (28) (causes big lag spikes)
 rem Best performance: Long, Variable, High Boost (16)
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\PriorityControl" /v "Win32PrioritySeparation" /t REG_DWORD /d "0x00000028" /f
-reg add "HKLM\SYSTEM\ControlSet001\Control\PriorityControl" /v "Win32PrioritySeparation" /t REG_DWORD /d "0x00000028" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\PriorityControl" /v "Win32PrioritySeparation" /t REG_DWORD /d "0x0000002A" /f
+reg add "HKLM\SYSTEM\ControlSet001\Control\PriorityControl" /v "Win32PrioritySeparation" /t REG_DWORD /d "0x0000002A" /f
 
 rem NVIDIA video card tweaks, last key may vary from 0000 to 0001 if dual NVIDIA/AMD machine is used
 rem Second to last key is the "Class Guid"
