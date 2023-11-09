3a8c9b0547b56a80a1a3af701d739ad7d6ed0d8e
Vincent Grande <shoober420@gmail.com>
2023-11-09 00:35:38 -0800

    Update Win11Tweaks.bat

--- Win11Tweaks.bat
+++ Win11Tweaks.bat
@@ -2389,9 +2389,9 @@ ECHO Yes | xcopy "%USERPROFILE%\Downloads\windows11-batch-scripts-main\SetTimerR
 rem Thread Quantum
 rem https://smallvoid.com/article/winnt-process-scheduler-priority.html
 rem https://forums.blurbusters.com/viewtopic.php?t=8535
-rem 2A Hex = Short, Fixed, High foreground boost (best input)
+rem 2A Hex = Short, Fixed, High foreground boost (very good input)
 rem 29 Hex = Short, Fixed, Medium foreground boost
-rem 28 Hex = Short, Fixed, No foreground boost (lag spikes)
+rem 28 Hex = Short, Fixed, No foreground boost (best input, lag spikes)
 rem 26 Hex = Short, Variable, High foreground boost (good input)
 rem 25 Hex = Short, Variable, Medium foreground boost
 rem 24 Hex = Short, Variable, No foreground boost (lag spikes)
