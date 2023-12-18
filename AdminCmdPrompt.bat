605d5d53b7ae234bb275a2b6a875b0712a592288
shoober420 <shoober420@gmail.com>
2023-12-17 20:24:54 -0800

    Update AdminCmdPrompt.bat

--- AdminCmdPrompt.bat
+++ AdminCmdPrompt.bat
@@ -1,6 +1,6 @@
 REM Launch Admin Command Prompt
 REM Will NOT work under SAFE MODE
-REM requires EnableRunasAdmin.bat (Secondary Logon) (will NOT launch under SAFE MODE)
+REM requires EnableRunasAdmin.bat (Secondary Logon) (will NOT work under SAFE MODE)
 
 REM runas /user:<adminaccount> cmd
 
