47fb0f378876172b735333ed6d4ae282d7085f05
shoober420 <shoober420@gmail.com>
2023-12-17 20:04:04 -0800

    Update DisableIntelDSA.bat

--- DisableIntelDSA.bat
+++ DisableIntelDSA.bat
@@ -6,11 +6,13 @@ net stop Appinfo
 net stop Winmgmt
 net stop DSAService
 net stop DSAUpdateService
+net stop SystemUsageReportSvc_QUEENCREEK
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\TrustedInstaller" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Appinfo" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Winmgmt" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\DSAService" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\DSAUpdateService" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\SystemUsageReportSvc_QUEENCREEK" /v "Start" /t REG_DWORD /d "4" /f
 
 PAUSE
