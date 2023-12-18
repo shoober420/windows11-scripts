bbd4b75bffb21a90725dac21ac337a1c96e8105f
shoober420 <shoober420@gmail.com>
2023-12-17 20:45:41 -0800

    Update DisableIntelDSA.bat

--- DisableIntelDSA.bat
+++ DisableIntelDSA.bat
@@ -8,6 +8,8 @@ net stop DSAService
 net stop DSAUpdateService
 net stop SystemUsageReportSvc_QUEENCREEK
 net stop "Intel(R) SUR QC SAM"
+net stop ESRV_SVC_QUEENCREEK
+net stop USER_ESRV_SVC_QUEENCREEK
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\TrustedInstaller" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Appinfo" /v "Start" /t REG_DWORD /d "4" /f
@@ -16,5 +18,7 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Services\DSAService" /v "Start" /t REG_DW
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\DSAUpdateService" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\SystemUsageReportSvc_QUEENCREEK" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Intel(R) SUR QC SAM" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\ESRV_SVC_QUEENCREEK" /t REG_DWORD /d "4" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\USER_ESRV_SVC_QUEENCREEK" /t REG_DWORD /d "4" /f
 
 PAUSE
