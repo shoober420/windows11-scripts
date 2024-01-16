a896da542fee7253ac963dea370843329d990354
shoober420 <shoober420@gmail.com>
2024-01-15 18:14:13 -0800

    Create MinimalGUI.bat

---
+++ MinimalGUI.bat
@@ -0,0 +1,17 @@
+REM Enable Minimal GUI (no taskbar, no icons, no background)
+
+net stop StateRepository
+net stop TimeBrokerSvc
+net stop SystemEventsBroker
+net stop Schedule
+net stop DispBrokerDesktopSvc
+net stop BrokerInfrastructure
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\StateRepository" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\TimeBrokerSvc" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\SystemEventsBroker" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Schedule" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\DispBrokerDesktopSvc" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\BrokerInfrastructure" /v "Start" /t REG_DWORD /d "4" /f
+
+PAUSE
