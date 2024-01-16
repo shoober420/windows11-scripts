7cf814a254de364a2a71d91012fa3ed2b2e5cc17
shoober420 <shoober420@gmail.com>
2024-01-15 18:22:40 -0800

    Create EnableGUI.bat

---
+++ EnableGUI.bat
@@ -0,0 +1,17 @@
+REM Enable GUI (taskbar, icons, and background)
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\StateRepository" /v "Start" /t REG_DWORD /d "2" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\TimeBrokerSvc" /v "Start" /t REG_DWORD /d "2" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\SystemEventsBroker" /v "Start" /t REG_DWORD /d "2" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Schedule" /v "Start" /t REG_DWORD /d "2" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\DispBrokerDesktopSvc" /v "Start" /t REG_DWORD /d "2" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\BrokerInfrastructure" /v "Start" /t REG_DWORD /d "2" /f
+
+net start StateRepository
+net start TimeBrokerSvc
+net start SystemEventsBroker
+net start Schedule
+net start DispBrokerDesktopSvc
+net start BrokerInfrastructure
+
+PAUSE
