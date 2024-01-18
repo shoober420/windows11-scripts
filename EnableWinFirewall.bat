1fed37c87460664726048bac9c001805014fdd4b
shoober420 <shoober420@gmail.com>
2024-01-18 15:32:50 -0800

    Update EnableWinFirewall.bat

--- EnableWinFirewall.bat
+++ EnableWinFirewall.bat
@@ -2,8 +2,10 @@ REM Enable Windows Firewall
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\mpssvc" /v "Start" /t REG_DWORD /d "2" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\BFE" /v "Start" /t REG_DWORD /d "2" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\wtd" /v "Start" /t REG_DWORD /d "2" /f
 
 net start mpssvc
 net start BFE
+net start wtd
 
 PAUSE
