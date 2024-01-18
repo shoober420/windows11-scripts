063cb1038ffeb63c212266a9bea07d64c90d446b
shoober420 <shoober420@gmail.com>
2024-01-18 15:33:16 -0800

    Update DisableWinFirewall.bat

--- DisableWinFirewall.bat
+++ DisableWinFirewall.bat
@@ -2,8 +2,10 @@ REM Disable Windows Firewall
 
 net stop mpssvc
 net stop BFE
+net stop wtd
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\mpssvc" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\BFE" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\wtd" /v "Start" /t REG_DWORD /d "4" /f
 
 PAUSE
