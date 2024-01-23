1e2702879e3ca83b86371faa502c8466d70ab3ec
shoober420 <shoober420@gmail.com>
2024-01-22 16:47:28 -0800

    Update StartBATs.bat

--- StartBATs.bat
+++ StartBATs.bat
@@ -2,9 +2,9 @@ REM Start Batch scripts with one batch file
 
 cd %USERPROFILE%\Downloads\windows11-batch-scripts-main
 
-start SetTimerResolution.bat
-start StartGraphicsPerfSvc.bat
-start DisableServices.bat
-start DisableWinUpdate.bat
+call SetTimerResolution.bat
+call StartGraphicsPerfSvc.bat
+call DisableServices.bat
+call DisableWinUpdate.bat
 
 PAUSE
