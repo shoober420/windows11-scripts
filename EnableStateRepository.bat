90b520b063ad89016c427c998eeea3e142efd10d
shoober420 <shoober420@gmail.com>
2024-01-27 20:57:02 -0800

    Update EnableStateRepository.bat

--- EnableStateRepository.bat
+++ EnableStateRepository.bat
@@ -1,5 +1,6 @@
 rem Enable State Repository Service
-REM WARNING: break system configuration settings, like power options
+REM WARNING: Breaks system configuration settings, like power options
+REM WARNING: Breaks GUI, like taskbar, icons, and background
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\StateRepository" /v "Start" /t REG_DWORD /d "2" /f
 
