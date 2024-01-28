a529c528658279ebe1cd052e6cade6ca61edaade
shoober420 <shoober420@gmail.com>
2024-01-27 19:41:04 -0800

    Update DisableGUI.bat

--- DisableGUI.bat
+++ DisableGUI.bat
@@ -1,13 +1,11 @@
 REM Disable GUI (no taskbar, no icons, no background)
 
-net stop Themes
 net stop TimeBrokerSvc
 net stop SystemEventsBroker
 net stop Schedule
 net stop DispBrokerDesktopSvc
 net stop BrokerInfrastructure
 
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\Themes" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\TimeBrokerSvc" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\SystemEventsBroker" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Schedule" /v "Start" /t REG_DWORD /d "4" /f
