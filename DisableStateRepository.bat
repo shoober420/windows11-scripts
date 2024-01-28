a5a643a53b424b1344fc2a97877c807098092bd7
shoober420 <shoober420@gmail.com>
2024-01-27 19:05:57 -0800

    Update DisableStateRepository.bat

--- DisableStateRepository.bat
+++ DisableStateRepository.bat
@@ -1,4 +1,6 @@
 rem Disable State Repository Service
+REM WARNING: break system configuration settings, like power options
+
 net stop StateRepository
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\StateRepository" /v "Start" /t REG_DWORD /d "4" /f
