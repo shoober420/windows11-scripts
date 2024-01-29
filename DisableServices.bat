765e9f1259ae3aa6e1881ea4f1f88a02388c8ad3
shoober420 <shoober420@gmail.com>
2024-01-28 20:48:49 -0800

    Update DisableServices.bat

--- DisableServices.bat
+++ DisableServices.bat
@@ -187,7 +187,6 @@ sc config CdRomArbiterService start= disabled
 sc config ESRV_SVC_QUEENCREEK start= disabled
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\TimeBrokerSvc" /v "Start" /t REG_DWORD /d "4" /f
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\SystemEventsBroker" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\DispBrokerDesktopSvc" /v "Start" /t REG_DWORD /d "4" /f
 
 rem Internet connection services
