f2c68a9eaa4cb63919825b9ad406e4ff20aaf87e
shoober420 <shoober420@gmail.com>
2024-01-30 11:41:08 -0800

    Update DeleteFonts.bat

--- DeleteFonts.bat
+++ DeleteFonts.bat
@@ -1,6 +1,8 @@
 REM Delete Fonts
-
 rmdir /s /q "%USERPROFILE%\AppData\Local\Microsoft\Windows\Fonts"
+
+takeown /s %computername% /u %username% /f "C:\Windows\Fonts"
+icacls "C:\Windows\Fonts" /grant:r %username%:F
 rmdir /s /q "C:\Windows\Fonts"
 
 PAUSE
