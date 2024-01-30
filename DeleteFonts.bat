33bdb77783d8e3dcb110386e29f15bf65785433b
shoober420 <shoober420@gmail.com>
2024-01-30 12:41:12 -0800

    Update DeleteFonts.bat

--- DeleteFonts.bat
+++ DeleteFonts.bat
@@ -1,8 +1,11 @@
 REM Delete Fonts
+REM Windows 11 requires Segou font family, or will black screen at login
+
 rmdir /s /q "%USERPROFILE%\AppData\Local\Microsoft\Windows\Fonts"
 
 takeown /s %computername% /u %username% /f "C:\Windows\Fonts"
 icacls "C:\Windows\Fonts" /grant:r %username%:F
-rmdir /s /q "C:\Windows\Fonts"
+rename "C:\Windows\Fonts" "Fonts.bak"
+REM rmdir /s /q "C:\Windows\Fonts"
 
 PAUSE
