4ac226c8c465b15b421c08ce50e0a7ddc18a7967
shoober420 <shoober420@gmail.com>
2024-01-30 13:32:06 -0800

    Update DeleteFonts.bat

--- DeleteFonts.bat
+++ DeleteFonts.bat
@@ -1,10 +1,15 @@
 REM Rename Fonts directory
 REM Debloat extra fonts
+
 REM Windows 95/98 uses Microsoft Sans Serif
 REM Windows XP/7 uses Tahoma
 REM Windows 10 uses Segoe UI
 REM Windows 11 uses Segoe UI Variable
+
 REM GUI font required or will black screen at login
+
+REM If font folder is deleted, boot into WinRE and open cmd prompt
+REM copy X:\Windows\Fonts C:\Windows\Fonts
 
 rmdir /s /q "%USERPROFILE%\AppData\Local\Microsoft\Windows\Fonts"
 
