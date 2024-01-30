e56740a8a7cf759df3aa4430b2fb52d27ad17129
shoober420 <shoober420@gmail.com>
2024-01-30 14:24:08 -0800

    Update DeleteWindowsAppDir.bat

--- DeleteWindowsAppDir.bat
+++ DeleteWindowsAppDir.bat
@@ -1,7 +1,7 @@
 REM Rename WindowsApps directory
 takeown /s %computername% /u %username% /f "C:\Program Files\WindowsApps"
 icacls "C:\Program Files\WindowsApps" /grant:r %username%:F
-rename "C:\Program Files\WindowsApps" "WindowsApps.bak"
+ren "C:\Program Files\WindowsApps" "WindowsApps.bak"
 REM rmdir /s /q "C:\Program Files\WindowsApps"
 
 PAUSE
