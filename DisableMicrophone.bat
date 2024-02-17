9e565aa36a53d515880c6329b8d82d7ff9b54232
shoober420 <shoober420@gmail.com>
2024-02-16 21:12:18 -0800

    Update DisableMicrophone.bat

--- DisableMicrophone.bat
+++ DisableMicrophone.bat
@@ -5,8 +5,8 @@ rem ---------------------------------- Privacy & security ----------------------
 rem ...................................... Microphone ......................................
 
 REM camsvc is required for microphone to work
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\camsvc" /v "Start" /t REG_DWORD /d "4" /f
 net stop camsvc
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\camsvc" /v "Start" /t REG_DWORD /d "4" /f
 
 rem Allow/Deny - Microphone access
 reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\microphone" /v "Value" /t REG_SZ /d "Deny" /f
