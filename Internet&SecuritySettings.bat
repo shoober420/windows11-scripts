247ddc6e73551746479924be96a58a72271d4024
shoober420 <shoober420@gmail.com>
2024-04-01 21:56:06 -0700

    Update Internet&SecuritySettings.bat

--- Internet&SecuritySettings.bat
+++ Internet&SecuritySettings.bat
@@ -50,6 +50,12 @@ powershell.exe Set-Processmitigation -System -Disable AuditStoreSigned
 powershell.exe Set-Processmitigation -System -Disable AuditSystemCall
 powershell.exe Set-Processmitigation -System -Disable AuditChildProcess
 
+powershell.exe Set-SmbClientConfiguration -EnableSMBQUIC $false
+powershell.exe Set-SmbClientConfiguration -AuditServerDoesNotSupportEncryption $true
+powershell.exe Set-SmbClientConfiguration -AuditServerDoesNotSupportSigning $true
+powershell.exe Set-SmbServerConfiguration -AuditClientDoesNotSupportEncryption $true
+powershell.exe Set-SmbServerConfiguration -AuditClientDoesNotSupportSigning $true
+
 rem deleting all inbound firewall rules may break internet for certain Wi-Fi cards
 powershell.exe Remove-NetFirewallRule -All
 
