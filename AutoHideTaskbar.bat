0e1ab402e20aad15588cbf112fe4d8a049013f3f
shoober420 <shoober420@gmail.com>
2024-04-19 16:20:56 -0700

    Create AutoHideTaskbar.bat

---
+++ AutoHideTaskbar.bat
@@ -0,0 +1,2 @@
+rem Autohide Taskbar
+powershell -command "&{$p='HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3';$v=(Get-ItemProperty -Path $p).Settings;$v[8]=3;&Set-ItemProperty -Path $p -Name Settings -Value $v;&Stop-Process -f -ProcessName explorer}"
