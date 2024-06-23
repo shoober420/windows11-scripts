7f8cd7ad24ae41df88fc4b803d2b6adf1aca7cfa
shoober420 <shoober420@gmail.com>
2024-06-23 05:26:31 -0700

    Update DeleteCache.bat

--- DeleteCache.bat
+++ DeleteCache.bat
@@ -1,8 +1,8 @@
 rem # Deletes tmp, cache, prefetch, last known good configuration files, live kernel reports, and Windows.old
 rem # Dont delete "rescache" folder
 
-rmdir /f /s /q "C:\Windows\Temp"
-rmdir /f /s /q "%USERPROFILE%\AppData\Local\Temp"
+rmdir /s /q "C:\Windows\Temp"
+rmdir /s /q "%USERPROFILE%\AppData\Local\Temp"
 del "%USERPROFILE%\AppData\Local\OneDrive\cache" /f /q /s
 del "%USERPROFILE%\AppData\Local\CrashDumps" /f /q /s
 del "%APPDATA%\Microsoft\Windows\Recent" /f /q /s
@@ -18,14 +18,14 @@ del "%USERPROFILE%\AppData\Local\Chromium\User Data\Default\Cache" /f /q /s
 del "%USERPROFILE%\AppData\Local\Chromium\User Data\Default\GPUCache" /f /q /s
 DEL /F /S /Q /A %LocalAppData%\Microsoft\Windows\Explorer\thumbcache_*.db
 
-rmdir /f /s /q "C:\Windows\LastGood"
-rmdir /f /s /q "C:\Windows\LastGood.tmp"
-rmdir /f /s /q "C:\Windows\LiveKernelReports"
-rmdir /f /s /q "C:\Windows\Prefetch"
+rmdir /s /q "C:\Windows\LastGood"
+rmdir /s /q "C:\Windows\LastGood.tmp"
+rmdir /s /q "C:\Windows\LiveKernelReports"
+rmdir /s /q "C:\Windows\Prefetch"
 
 takeown /s %computername% /u %username% /f "C:\Windows.old"
 icacls "C:\Windows.old" /grant:r %username%:F
-rmdir /f /s /q "C:\Windows.old"
+rmdir /s /q "C:\Windows.old"
 
 DEL /F /S /Q /A "C:\pagefile.sys"
 DEL /F /S /Q /A "C:\swapfile.sys"
