be3f5197d249263ce46b3373757964d07a545f01
shoober420 <shoober420@gmail.com>
2024-06-23 05:19:00 -0700

    Update DeleteTmpCache.bat

--- DeleteTmpCache.bat
+++ DeleteTmpCache.bat
@@ -1,20 +1,35 @@
 REM Deletes tmp, cache, prefetch, last known good configuration files, live kernel reports, and Windows.old
 REM Dont delete "rescache" folder
 
-rmdir /s /q "C:\Windows\Temp"
-rmdir /s /q "%USERPROFILE%\AppData\Local\Temp"
+rmdir /f /s /q "C:\Windows\Temp"
+rmdir /f /s /q "%USERPROFILE%\AppData\Local\Temp"
+del "%USERPROFILE%\AppData\Local\OneDrive\cache" /f /q /s
+del "%USERPROFILE%\AppData\Local\CrashDumps" /f /q /s
+del "%APPDATA%\Microsoft\Windows\Recent" /f /q /s
+del "C:\Windows\Logs\MeasuredBoot" /f /q /s
+del "C:\ProgramData\Microsoft\Windows\WER" /f /q /s
+del "%USERPROFILE%\AppData\Local\Microsoft\Edge\User Data\Default\Cache" /f /q /s
+del "%USERPROFILE%\AppData\Local\Microsoft\Edge\User Data\Default\GPUCache" /f /q /s
+del "%USERPROFILE%\AppData\Local\Google\Chrome\User Data\Default\Cache" /f /q /s
+del "%USERPROFILE%\AppData\Local\Google\Chrome\User Data\Default\GPUCache" /f /q /s
+del "%USERPROFILE%\AppData\Local\Google\Chrome SxS\User Data\Default\Cache" /f /q /s
+del "%USERPROFILE%\AppData\Local\Google\Chrome SxS\User Data\Default\GPUCache" /f /q /s
+del "%USERPROFILE%\AppData\Local\Chromium\User Data\Default\Cache" /f /q /s
+del "%USERPROFILE%\AppData\Local\Chromium\User Data\Default\GPUCache" /f /q /s
 DEL /F /S /Q /A %LocalAppData%\Microsoft\Windows\Explorer\thumbcache_*.db
 
-rmdir /s /q "C:\Windows\LastGood"
-rmdir /s /q "C:\Windows\LastGood.tmp"
-rmdir /s /q "C:\Windows\LiveKernelReports"
-rmdir /s /q "C:\Windows\Prefetch"
+rmdir /f /s /q "C:\Windows\LastGood"
+rmdir /f /s /q "C:\Windows\LastGood.tmp"
+rmdir /f /s /q "C:\Windows\LiveKernelReports"
+rmdir /f /s /q "C:\Windows\Prefetch"
 
 takeown /s %computername% /u %username% /f "C:\Windows.old"
 icacls "C:\Windows.old" /grant:r %username%:F
-rmdir /s /q "C:\Windows.old"
+rmdir /f /s /q "C:\Windows.old"
 
 DEL /F /S /Q /A "C:\pagefile.sys"
 DEL /F /S /Q /A "C:\swapfile.sys"
+
+
 
 PAUSE
