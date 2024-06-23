15f526912f71c209eaec4b1ccbd40e6874822fd5
shoober420 <shoober420@gmail.com>
2024-06-22 18:32:56 -0700

    Update Windows11Tweaks.bat

--- Windows11Tweaks.bat
+++ Windows11Tweaks.bat
@@ -2822,15 +2822,4 @@ rem # Device Manager > Display Adapters > "GPUNAME" > Properties > Details > Pro
 rem # HKLM\SYSTEM\CurrentControlSet\Enum\PCI\"Device instant path"
 reg add "HKLM\SYSTEM\CurrentControlSet\Enum\PCI\VEN_10DE&DEV_2684&SUBSYS_89321043&REV_A1\4&8bd6e8d&0&0008\Device Parameters\Interrupt Management\MessageSignaledInterruptProperties" /v "MSISupported" /t REG_DWORD /d "1" /f
 
-rem # SvcHostSplitThresholdInKB value is amount of RAM in KiloBytes (KB)
-rem # Example math formula: 8 GB = 8x1024 MB = 8x1024x1024 KB = 8388608 KB
-rem # 8GB = 8388608 (800000) / 16GB = 16777216 (1000000) / 32GB = 33554432 (2000000) / 64GB = 67108864 (4000000)
-reg add "HKLM\SYSTEM\ControlSet001\Control" /v "SvcHostSplitThresholdInKB" /t REG_DWORD /d "2000000" /f
-reg add "HKLM\SYSTEM\CurrentControlSet\Control" /v "SvcHostSplitThresholdInKB" /t REG_DWORD /d "2000000" /f
-
-rem # Limits RAM to use certain tasks
-rem # Example math formula: 8 GB = 8x1024 MB = 8x1024x1024 KB = 8388608 KB
-rem # 8GB = 8388608 (800000) / 16GB = 16777216 (1000000) / 32GB = 33554432 (2000000) / 64GB = 67108864 (4000000)
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v "IoPageLockLimit" /t REG_DWORD /d "2000000" /f
-
 PAUSE
