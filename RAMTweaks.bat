629df5ffd7fb2839075aea7c07215fd0b38d5e3d
shoober420 <shoober420@gmail.com>
2024-06-23 02:03:19 -0700

    Update RAMTweaks.bat

--- RAMTweaks.bat
+++ RAMTweaks.bat
@@ -1,5 +1,11 @@
 rem # ADJUST VALUES ACCORDING TO RAM SIZE
 
+rem # Pagefile Size in MB (16GB = 16384) (32GB = 32768) (64GB = 65536)
+rem # InitialSize=65536,MaximumSize=65536
+wmic computersystem where name="%computername%" set AutomaticManagedPagefile=False
+wmic pagefileset where name="C:\\pagefile.sys" set InitialSize=32768,MaximumSize=32768
+rem reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v "PagingFiles" /t REG_MULTI_SZ /d "c:\pagefile.sys 0 0" /f
+
 rem # SvcHostSplitThresholdInKB value is amount of RAM in KiloBytes (KB)
 rem # Example math formula: 8 GB = 8x1024 MB = 8x1024x1024 KB = 8388608 KB
 rem # 8GB = 8388608 (800000) / 16GB = 16777216 (1000000) / 32GB = 33554432 (2000000) / 64GB = 67108864 (4000000)
