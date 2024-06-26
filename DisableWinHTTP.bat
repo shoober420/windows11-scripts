ca1f71df058eebb64deb652a35e3d6d5ec97ea4b
shoober420 <shoober420@gmail.com>
2024-06-25 21:26:49 -0800

    Update DisableWinHTTP.bat

--- DisableWinHTTP.bat
+++ DisableWinHTTP.bat
@@ -1,4 +1,12 @@
-rem Disable WinHttpAutoProxySvc
+rem # Disable WinHttpAutoProxySvc
+rem # !!! WARNING !!!
+rem # Breaks internet when disabled
+rem # !!! CAUTION !!!
+rem # DO NOT USE 
+rem # FOR TESTING PURPOSES ONLY
+
+PAUSE
+
 net stop WinHttpAutoProxySvc
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\WinHttpAutoProxySvc" /v "Start" /t REG_DWORD /d "4" /f
