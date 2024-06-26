1e56ff5f8e101cb77398a5d5fd97c22e67a9726f
shoober420 <shoober420@gmail.com>
2024-06-25 21:20:52 -0800

    Update DisableAppX.bat

--- DisableAppX.bat
+++ DisableAppX.bat
@@ -1,4 +1,11 @@
-rem Disable AppXSvc
+rem # Disable AppXSvc
+rem # !!! WARNING !!!
+rem # Disabling AppXSvc breaks File Explorer features, like zip functionality and right click menu
+rem # Causes File Explorer to crash frequently
+rem # !!! CAUTION !!!
+
+PAUSE
+
 net stop AppXSvc
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\AppXSvc" /v "Start" /t REG_DWORD /d "4" /f
