3aea2bba933c4770bb7c3cc7e0485a20f70b9bca
shoober420 <shoober420@gmail.com>
2024-06-25 22:48:35 -0800

    Update Windows11Tweaks.bat

--- Windows11Tweaks.bat
+++ Windows11Tweaks.bat
@@ -1318,13 +1318,6 @@ taskkill /im msedge.exe /f
 rem del "C:\Windows\System32\GigabyteUpdateService.exe" /s /f /q
 ren "C:\Windows\System32\GigabyteUpdateService.exe" "GigabyteUpdateService.exe.bak"
 
-rem # Disable Shell Experience Host (to restore run "sfc /scannow")
-takeown /s %computername% /u %username% /f "C:\Windows\SystemApps\ShellExperienceHost_cw5n1h2txyewy\ShellExperienceHost.exe"
-icacls "C:\Windows\SystemApps\ShellExperienceHost_cw5n1h2txyewy\ShellExperienceHost.exe" /grant:r %username%:F
-taskkill /im ShellExperienceHost.exe /f
-rem del "C:\Windows\SystemApps\ShellExperienceHost_cw5n1h2txyewy\ShellExperienceHost.exe" /s /f /q
-ren "C:\Windows\SystemApps\ShellExperienceHost_cw5n1h2txyewy\ShellExperienceHost.exe" "ShellExperienceHost.exe.bak"
-
 rem # Disable Spatial Audio License Service (to restore run "sfc /scannow")
 takeown /s %computername% /u %username% /f "%WinDir%\System32\SpatialAudioLicenseSrv.exe"
 icacls "%WinDir%\System32\SpatialAudioLicenseSrv.exe" /grant:r %username%:F
