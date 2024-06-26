06f6aa088c8aef34e7e16df5fd53ddb7af0f091b
shoober420 <shoober420@gmail.com>
2024-06-25 23:03:47 -0800

    Update EnableStartMenu.bat

--- EnableStartMenu.bat
+++ EnableStartMenu.bat
@@ -1,10 +1,17 @@
 rem # Enable Start Menu
 
-rem # Disable Shell Experience Host (to restore run "sfc /scannow")
+rem # Enable Shell Experience Host (to restore run "sfc /scannow")
 takeown /s %computername% /u %username% /f "C:\Windows\SystemApps\ShellExperienceHost_cw5n1h2txyewy\ShellExperienceHost.exe"
 icacls "C:\Windows\SystemApps\ShellExperienceHost_cw5n1h2txyewy\ShellExperienceHost.exe" /grant:r %username%:F
 taskkill /im ShellExperienceHost.exe /f
 rem del "C:\Windows\SystemApps\ShellExperienceHost_cw5n1h2txyewy\ShellExperienceHost.exe" /s /f /q
 ren "C:\Windows\SystemApps\ShellExperienceHost_cw5n1h2txyewy\ShellExperienceHost.exe.bak" "ShellExperienceHost.exe"
+
+rem # Enable Start (to restore run SFC scan)
+takeown /s %computername% /u %username% /f "%WINDIR%\SystemApps\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\StartMenuExperienceHost.exe"
+icacls "%WINDIR%\SystemApps\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\StartMenuExperienceHost.exe" /inheritance:r /grant:r %username%:F
+taskkill /im StartMenuExperienceHost.exe /f
+rem del "%WINDIR%\SystemApps\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\StartMenuExperienceHost.exe" /s /f /q
+ren "C:\Windows\SystemApps\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\StartMenuExperienceHost.exe.bak" "StartMenuExperienceHost.exe"
 
 PAUSE
