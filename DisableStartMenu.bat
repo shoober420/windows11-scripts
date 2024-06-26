7d9af6fbf7b1c3e93185da8f433e6a372d12740c
shoober420 <shoober420@gmail.com>
2024-06-25 22:45:42 -0800

    Create DisableStartMenu.bat

---
+++ DisableStartMenu.bat
@@ -0,0 +1,11 @@
+rem # Disable Start Menu
+rem # Right click start menu still works
+
+rem # Disable Shell Experience Host (to restore run "sfc /scannow")
+takeown /s %computername% /u %username% /f "C:\Windows\SystemApps\ShellExperienceHost_cw5n1h2txyewy\ShellExperienceHost.exe"
+icacls "C:\Windows\SystemApps\ShellExperienceHost_cw5n1h2txyewy\ShellExperienceHost.exe" /grant:r %username%:F
+taskkill /im ShellExperienceHost.exe /f
+rem del "C:\Windows\SystemApps\ShellExperienceHost_cw5n1h2txyewy\ShellExperienceHost.exe" /s /f /q
+ren "C:\Windows\SystemApps\ShellExperienceHost_cw5n1h2txyewy\ShellExperienceHost.exe" "ShellExperienceHost.exe.bak"
+
+PAUSE
