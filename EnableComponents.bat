f8e8f50096d3eb4e98c41a41569ea6ecf445a2d0
shoober420 <shoober420@gmail.com>
2024-06-26 16:11:21 -0800

    Update EnableComponents.bat

--- EnableComponents.bat
+++ EnableComponents.bat
@@ -1,79 +1,91 @@
-rem Enable Components
+rem # Enable Components
 
-rem Enable Smartscreen (to restore run "sfc /scannow")
+rem # Enable Smartscreen (to restore run "sfc /scannow")
 takeown /s %computername% /u %username% /f "%WinDir%\System32\smartscreen.exe"
 icacls "%WinDir%\System32\smartscreen.exe" /grant:r %username%:F
 ren "%WinDir%\System32\smartscreen.exe.bak" "smartscreen.exe"
 
-rem Enable GameBarPresenceWriter.exe (to restore run SFC scan)
+rem # Enable GameBarPresenceWriter.exe (to restore run SFC scan)
 takeown /s %computername% /u %username% /f "%WINDIR%\System32\GameBarPresenceWriter.exe"
 icacls "%WINDIR%\System32\GameBarPresenceWriter.exe" /inheritance:r /grant:r %username%:F
 ren "%WinDir%\System32\GameBarPresenceWriter.exe.bak" "GameBarPresenceWriter.exe"
 
-rem Enable Start (to restore run SFC scan)
+rem # Enable Start (to restore run SFC scan)
 takeown /s %computername% /u %username% /f "%WINDIR%\SystemApps\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\StartMenuExperienceHost.exe"
 icacls "%WINDIR%\SystemApps\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\StartMenuExperienceHost.exe" /inheritance:r /grant:r %username%:F
 ren "%WINDIR%\SystemApps\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\StartMenuExperienceHost.exe.bak" "StartMenuExperienceHost.exe"
 
-rem Enable Search (Cortana/to restore run SFC scan)
+rem # Enable Search (Cortana/to restore run SFC scan)
 rem Manual installation of Cortana through Microsoft Store preferred, very undesirable software
 rem winget install "cortana"
 takeown /s %computername% /u %username% /f "%WINDIR%\SystemApps\MicrosoftWindows.Client.CBS_cw5n1h2txyewy\SearchHost.exe"
 icacls "%WINDIR%\SystemApps\MicrosoftWindows.Client.CBS_cw5n1h2txyewy\SearchHost.exe" /inheritance:r /grant:r %username%:F
 ren "%WINDIR%\SystemApps\MicrosoftWindows.Client.CBS_cw5n1h2txyewy\SearchHost.exe.bak" "SearchHost.exe"
 
-rem Enable Shell Experience Host (to restore run "sfc /scannow")
+rem # Enable Shell Experience Host (to restore run "sfc /scannow")
 takeown /s %computername% /u %username% /f "C:\Windows\SystemApps\ShellExperienceHost_cw5n1h2txyewy\ShellExperienceHost.exe"
 icacls "C:\Windows\SystemApps\ShellExperienceHost_cw5n1h2txyewy\ShellExperienceHost.exe" /grant:r %username%:F
 ren "C:\Windows\SystemApps\ShellExperienceHost_cw5n1h2txyewy\ShellExperienceHost.exe.bak" "ShellExperienceHost.exe"
 
-rem Enable Application Frame Host (to restore run "sfc /scannow")
+rem # Enable Application Frame Host (to restore run "sfc /scannow")
 takeown /s %computername% /u %username% /f "%WinDir%\System32\ApplicationFrameHost.exe"
 icacls "%WinDir%\System32\ApplicationFrameHost.exe" /grant:r %username%:F
 taskkill /im ApplicationFrameHost.exe /f
 rem del "%WinDir%\System32\ApplicationFrameHost.exe" /s /f /q
 ren "%WinDir%\System32\ApplicationFrameHost.exe.bak" "ApplicationFrameHost.exe"
 
-rem Enable Spatial Audio License Service (to restore run "sfc /scannow")
+rem # Enable Spatial Audio License Service (to restore run "sfc /scannow")
 takeown /s %computername% /u %username% /f "%WinDir%\System32\SpatialAudioLicenseSrv.exe"
 icacls "%WinDir%\System32\SpatialAudioLicenseSrv.exe" /grant:r %username%:F
 taskkill /im SpatialAudioLicenseSrv.exe /f
 rem del "%WinDir%\System32\SpatialAudioLicenseSrv.exe" /s /f /q
 ren "%WinDir%\System32\SpatialAudioLicenseSrv.exe.bak" "SpatialAudioLicenseSrv.exe"
 
-rem Enable Spatial Audio License Service 64 (to restore run "sfc /scannow")
+rem # Enable Spatial Audio License Service 64 (to restore run "sfc /scannow")
 takeown /s %computername% /u %username% /f "%WinDir%\SysWOW64\SpatialAudioLicenseSrv.exe"
 icacls "%WinDir%\SysWOW64\SpatialAudioLicenseSrv.exe" /grant:r %username%:F
 taskkill /im SpatialAudioLicenseSrv.exe /f
 rem del "%WinDir%\SysWOW64\SpatialAudioLicenseSrv.exe" /s /f /q
 ren "%WinDir%\SysWOW64\SpatialAudioLicenseSrv.exe.bak" "SpatialAudioLicenseSrv.exe"
 
-rem Enable AMD User Experience Program Master
+rem # Enable AMD User Experience Program Master
 takeown /s %computername% /u %username% /f "C:\Program Files\AMD\Performance Profile Client\AUEPMaster.exe"
 icacls "C:\Program Files\AMD\Performance Profile Client\AUEPMaster.exe" /grant:r %username%:F
 taskkill /im AUEPMaster.exe /f
 rem del "C:\Program Files\AMD\Performance Profile Client\AUEPMaster.exe" /s /f /q
 ren "C:\Program Files\AMD\Performance Profile Client\AUEPMaster.exe.bak" "AUEPMaster.exe"
 
-rem Enable Armoury Socket Server
+rem # Enable Armoury Socket Server
 takeown /s %computername% /u %username% /f "C:\Program Files (x86)\ASUS\ArmouryDevice\dll\ArmourySocketServer.exe"
 icacls "C:\Program Files (x86)\ASUS\ArmouryDevice\dll\ArmourySocketServer.exe" /grant:r %username%:F
 taskkill /im ArmourySocketServer.exe /f
 rem del "C:\Program Files (x86)\ASUS\ArmouryDevice\dll\ArmourySocketServer.exe" /s /f /q
 ren "C:\Program Files (x86)\ASUS\ArmouryDevice\dll\ArmourySocketServer.exe.bak" "ArmourySocketServer.exe"
 
-rem Enable P508PowerAgent
+rem # Enable P508PowerAgent
 takeown /s %computername% /u %username% /f "C:\Program Files (x86)\ASUS\ArmouryDevice\dll\ShareFromArmouryIII\Mouse\ROG STRIX CARRY\P508PowerAgent.exe"
 icacls "C:\Program Files (x86)\ASUS\ArmouryDevice\dll\ShareFromArmouryIII\Mouse\ROG STRIX CARRY\P508PowerAgent.exe" /grant:r %username%:F
 taskkill /im P508PowerAgent.exe /f
 rem del "C:\Program Files (x86)\ASUS\ArmouryDevice\dll\ShareFromArmouryIII\Mouse\ROG STRIX CARRY\P508PowerAgent.exe" /s /f /q
 ren "C:\Program Files (x86)\ASUS\ArmouryDevice\dll\ShareFromArmouryIII\Mouse\ROG STRIX CARRY\P508PowerAgent.exe.bak" "P508PowerAgent.exe"
 
-rem Disable Anti-malware Service Executable (to restore run "sfc /scannow") 
- takeown /s %computername% /u %username% /f "C:\ProgramData\Microsoft\Windows Defender\Platform\4.18.23070.1004-0\MsMpEng.exe" 
- icacls "C:\ProgramData\Microsoft\Windows Defender\Platform\4.18.23070.1004-0\MsMpEng.exe" /grant:r %username%:F 
- taskkill /im MsMpEng.exe /f 
- rem del "C:\ProgramData\Microsoft\Windows Defender\Platform\4.18.23070.1004-0\MsMpEng.exe" /s /f /q 
- ren "C:\ProgramData\Microsoft\Windows Defender\Platform\4.18.23070.1004-0\MsMpEng.exe.bak" "MsMpEng.exe"
+rem # Enable Anti-malware Service Executable (to restore run "sfc /scannow") 
+takeown /s %computername% /u %username% /f "C:\ProgramData\Microsoft\Windows Defender\Platform\4.18.23070.1004-0\MsMpEng.exe" 
+icacls "C:\ProgramData\Microsoft\Windows Defender\Platform\4.18.23070.1004-0\MsMpEng.exe" /grant:r %username%:F 
+taskkill /im MsMpEng.exe /f 
+rem del "C:\ProgramData\Microsoft\Windows Defender\Platform\4.18.23070.1004-0\MsMpEng.exe" /s /f /q 
+ren "C:\ProgramData\Microsoft\Windows Defender\Platform\4.18.23070.1004-0\MsMpEng.exe.bak" "MsMpEng.exe"
+
+rem # Enable INTEL Energy Server Service
+takeown /s %computername% /u %username% /f "C:\Program Files\Intel\SUR\QUEENCREEK\x64\esrv_svc.exe"
+icacls "C:\Program Files\Intel\SUR\QUEENCREEK\x64\esrv_svc.exe" /grant:r %username%:F
+taskkill /im esrv_svc.exe /f
+rem del "C:\Program Files\Intel\SUR\QUEENCREEK\x64\esrv_svc.exe" /s /f /q
+ren "C:\Program Files\Intel\SUR\QUEENCREEK\x64\esrv_svc.exe.bak" "esrv_svc.exe"
+
+rem # Enable WWAHost.exe (to restore run "sfc /scannow")
+takeown /s %computername% /u %username% /f "%WinDir%\System32\WWAHost.exe"
+icacls "%WinDir%\System32\WWAHost.exe" /grant:r %username%:F
+ren "%WinDir%\System32\WWAHost.exe.bak" "WWAHost.exe"
 
 PAUSE
