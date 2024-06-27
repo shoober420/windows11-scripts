b93da7f62363457db0772b7833d7ed8d86ce400a
shoober420 <shoober420@gmail.com>
2024-06-26 23:21:25 -0800

    Update InstallChocolatey.bat

--- InstallChocolatey.bat
+++ InstallChocolatey.bat
@@ -1,7 +1,7 @@
 rem # Installs chocolatey
 
 rem # Enable PowerShell
-Enable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2Root
+powershell.exe Enable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2Root
 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
