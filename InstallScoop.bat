17bebf5ff7c2d0796545994452250b8a3818f22d
shoober420 <shoober420@gmail.com>
2024-06-27 05:21:16 -0800

    Update InstallScoop.bat

--- InstallScoop.bat
+++ InstallScoop.bat
@@ -1,6 +1,9 @@
 # Install Scoop (as admin)
 
 rem # Enable PowerShell
+
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\DisallowRun" /v "powershell.exe" /t REG_DWORD /d "0" /f
+
 powershell.exe Enable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2Root
 
 powershell iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
@@ -8,5 +11,7 @@ powershell scoop update
 
 rem # Disable PowerShell
 powershell.exe Disable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2Root
+
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\DisallowRun" /v "powershell.exe" /t REG_DWORD /d "1" /f
 
 PAUSE
