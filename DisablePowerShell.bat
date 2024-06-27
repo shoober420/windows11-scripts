29db74138dcf0c05c6e21fc27f3ffc63d34e6214
shoober420 <shoober420@gmail.com>
2024-06-27 05:14:45 -0800

    Update DisablePowerShell.bat

--- DisablePowerShell.bat
+++ DisablePowerShell.bat
@@ -2,6 +2,9 @@ rem # Disable PowerShell
 
 powershell.exe Disable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2Root
 
+rem # Disable Remote PowerShell
+powershell.exe Disable-PSRemoting -Force
+
 rem # Disable and stop WMI
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Winmgmt" /v "Start" /t REG_DWORD /d "4" /f
