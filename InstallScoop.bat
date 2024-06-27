f3441683df6bc13aaad8eed746e143020e9345a8
shoober420 <shoober420@gmail.com>
2024-06-26 23:24:40 -0800

    Update InstallScoop.bat

--- InstallScoop.bat
+++ InstallScoop.bat
@@ -1,5 +1,8 @@
 # Install Scoop (as admin)
 
+rem # Enable PowerShell
+powershell.exe Enable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2Root
+
 powershell iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
 powershell scoop update
 
