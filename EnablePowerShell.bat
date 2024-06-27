9e0a4f61b44f367ffb3aa49ef213ce2ee5ce351b
shoober420 <shoober420@gmail.com>
2024-06-26 23:38:12 -0800

    Update EnablePowerShell.bat

--- EnablePowerShell.bat
+++ EnablePowerShell.bat
@@ -1,4 +1,11 @@
 rem # Enable PowerShell
+
+rem Enable and start WMI (required for powershell)
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Winmgmt" /v "Start" /t REG_DWORD /d "2" /f
+sc config winmgmt start= auto
+net start winmgmt
+
 powershell.exe Enable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2Root
 
 PAUSE
