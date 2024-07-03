cb045bf518aa64a0d67dce190836815060d65342
shoober420 <shoober420@gmail.com>
2024-07-02 18:54:36 -0800

    Delete EnableCCleaner.bat

--- EnableCCleaner.bat
+++
@@ -1,6 +0,0 @@
-rem # Enable CCleaner services
-rem # Sets to "Manual", the default value
-
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\CCleanerPerformanceOptimizerService" /v "Start" /t REG_DWORD /d "3" /f
-
-PAUSE
