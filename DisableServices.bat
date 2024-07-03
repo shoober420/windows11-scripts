0e794d25c60c6a3386a5d22092bfc749f07e5abe
shoober420 <shoober420@gmail.com>
2024-07-02 18:33:21 -0800

    Update DisableServices.bat

--- DisableServices.bat
+++ DisableServices.bat
@@ -307,6 +307,9 @@ rem Disable for constant CPU max frequency
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\intelppm" /v "Start" /t REG_DWORD /d "4" /f
 rem sc config intelppm start= demand
 
+rem # Disable CCleaner Services
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\CCleanerPerformanceOptimizerService" /v "Start" /t REG_DWORD /d "4" /f
+
 rem GraphicsPerfSvc enable, improves performance
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\GraphicsPerfSvc" /v "Start" /t REG_DWORD /d "2" /f
 
