66c9dda7284f3e9aa6c1aa96169d698edabf12c8
shoober420 <shoober420@gmail.com>
2024-07-21 20:52:30 -0700

    Update MouseAccelFix.bat

--- MouseAccelFix.bat
+++ MouseAccelFix.bat
@@ -17,7 +17,7 @@ reg add "HKU\.DEFAULT\Control Panel\Mouse" /v "MouseThreshold2" /t REG_SZ /d "0"
 
 rem # Binary form of MarkC patch
 rem # Windows_10+8.x_MouseFix_ItemsSize=100%_Scale=1-to-1_@6-of-11.reg
-rem # Items Size must be 100%
+rem # System > Display > Scale & layout > Scale 100% (Recommended)
 rem # MouseSensitivity (10 = Normal, 20 = Max)
 reg add "HKCU\Control Panel\Mouse" /f /v "SmoothMouseXCurve" /t REG_BINARY /d 0000000000000000C0CC0C0000000000809919000000000040662600000000000033330000000000
 reg add "HKCU\Control Panel\Mouse" /f /v "SmoothMouseYCurve" /t REG_BINARY /d 0000000000000000000038000000000000007000000000000000A800000000000000E00000000000
