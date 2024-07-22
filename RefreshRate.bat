c19d1402ce9b63cb66c7f98d6dc8ed4091ba0f2c
shoober420 <shoober420@gmail.com>
2024-07-21 18:34:43 -0700

    Update and rename RefreshRate240Hz.bat to RefreshRate.bat

---
+++ RefreshRate.bat
@@ -0,0 +1,15 @@
+rem # Updates monitor refresh rate
+rem # Requires Refresh Rate Switcher (https://github.com/sryze/RefreshRateSwitcher)
+
+rem # Tweak last setting to desired value before launching
+rem # RefreshRateSwitcher.exe 0 360
+
+rem # !!!WARNING!!!
+rem # Black screen may occur if unsupported refresh rate is chosen]
+rem # !!!WARNING!!!
+
+PAUSE
+
+%USERPROFILE%\Downloads\RefreshRateSwitcher-0.1.1-win64\RefreshRateSwitcher.exe 0 240
+
+TIMEOUT /T 5
