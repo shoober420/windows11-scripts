5125ebd72f1278b1012c51dc131c677cdc59cf0a
shoober420 <shoober420@gmail.com>
2024-07-22 02:37:59 -0700

    Update DisablePowerShell.bat

--- DisablePowerShell.bat
+++ DisablePowerShell.bat
@@ -1,5 +1,8 @@
 rem # Disable PowerShell
 
+rem # Disable PowerShell scripts
+Set-ExecutionPolicy Default
+
 powershell.exe Disable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2Root
 
 rem # Disable Remote PowerShell
@@ -14,6 +17,5 @@ net stop winmgmt
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "DisallowRun" /t REG_DWORD /d "1" /f
 
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\DisallowRun" /v "powershell.exe" /t REG_DWORD /d "1" /f
-
 
 PAUSE
