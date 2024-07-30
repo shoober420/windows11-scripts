5d13a182ccf2ada062f2b45553ea086d50abe6f0
shoober420 <shoober420@gmail.com>
2024-07-29 18:04:23 -0700

    Update Windows11Tweaks.bat

--- Windows11Tweaks.bat
+++ Windows11Tweaks.bat
@@ -520,9 +520,11 @@ reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\GameDVR" /v "HistoricalC
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\GameDVR" /v "HistoricalCaptureOnWirelessDisplayAllowed" /t REG_DWORD /d "0" /f
 
 
-rem 0 - Disable Fullscreen Optimizations for Current User / 0 - Enabled / 2 - Disabled
+rem # Disable Fullscreen Optimizations for Current User / 0 - Enabled / 2 - Disabled
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_FSEBehavior" /t REG_DWORD /d "2" /f
-reg add "HKCU\System\GameConfigStore" /v "GameDVR_HonorUserFSEBehaviorMode" /t REG_DWORD /d "2" /f
+
+rem # Honor User adjusted FSE value
+reg add "HKCU\System\GameConfigStore" /v "GameDVR_HonorUserFSEBehaviorMode" /t REG_DWORD /d "1" /f
 
 rem 0 - Disable Game DVR / "Press Win + G to record a clip"
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_Enabled" /t REG_DWORD /d "0" /f
@@ -2713,8 +2715,16 @@ reg add "HKLM\SOFTWARE\Microsoft\Windows\Dwm" /v "OverlayTestMode" /t REG_DWORD 
 rem # Disable DPI Scaling
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\SideBySide" /v "PreferExternalManifest" /t REG_DWORD /d "1" /f
 
-rem # Disable Fullscreen Optimizations
+rem # Disable Fullscreen Optimizations (FSO)
 rem # 0 = Enabled / 2 = Disabled
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_DSEBehavior" /t REG_DWORD /d "2" /f
+reg add "HKCU\System\GameConfigStore" /v "GameDVR_DSEBehaviorMode" /t REG_DWORD /d "2" /f
+
+rem # Disable more FSO, removes Color Management
+rem # 0 = Enable FSO / 1 = Disable FSO
+reg add "HKCU\System\GameConfigStore" /v "GameDVR_DXGIHonorFSEWindowsCompatible" /t REG_DWORD /d "1" /f
+
+rem # Disable GameDVR features
+reg add "HKCU\System\GameConfigStore" /v "GameDVR_EFSEFeatureFlags" /t REG_DWORD /d "0" /f
 
 PAUSE
