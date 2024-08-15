91ae612c6a80a7d9d08f386c5f6c95ddde0757aa
shoober420 <shoober420@gmail.com>
2024-08-15 16:04:29 -0700

    Update RemoveActivateWindowsWatermark.bat

--- RemoveActivateWindowsWatermark.bat
+++ RemoveActivateWindowsWatermark.bat
@@ -2,7 +2,7 @@ rem # Remove Activate Windows watermark on the bottom right of the screen
 
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform\Activation" /v "Manual" /t REG_DWORD /d "0" /f
 
-rem reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform\Activation" /v "ActivationInterval" /t REG_DWORD /d "0" /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform\Activation" /v "ActivationInterval" /t REG_DWORD /d "0" /f
 
 reg add "HKCU\Control Panel\Desktop" /v "PaintDesktopVersion" /t REG_DWORD /d "0" /f
 
