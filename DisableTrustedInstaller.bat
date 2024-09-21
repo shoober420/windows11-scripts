ef241f81135f8756fdce69a21d97e30088df4985
shoober420 <shoober420@gmail.com>
2024-09-21 02:35:47 -0700

    Update DisableTrustedInstaller.bat

--- DisableTrustedInstaller.bat
+++ DisableTrustedInstaller.bat
@@ -1,4 +1,9 @@
 rem # Disable Trusted Installer (Windows Modules Installer)
+rem # requires SAFE MODE
+rem # requires SetACL.bat script to be run first
+
+PAUSE
+
 net stop TrustedInstaller
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\TrustedInstaller" /v "Start" /t REG_DWORD /d "4" /f
 
