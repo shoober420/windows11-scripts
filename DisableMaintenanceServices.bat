d8d7843b14a5c5ac5cf0ef5a482b726225a8e659
shoober420 <shoober420@gmail.com>
2024-09-21 03:09:34 -0700

    Update DisableMaintenanceServices.bat

--- DisableMaintenanceServices.bat
+++ DisableMaintenanceServices.bat
@@ -22,13 +22,11 @@ net stop UsoSvc
 net stop wisvc
 net stop wuauserv
 net stop BITS
-net stop TrustedInstaller
 net stop WaaSMedicSvc
 net stop cryptsvc
 net stop InventorySvc
 net stop DoSvc
 net stop DiagTrack
-net stop NetSetupSvc
 net stop DsmSvc
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\ClipSVC" /v "Start" /t REG_DWORD /d "4" /f
@@ -43,12 +41,10 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Services\UsoSvc" /v "Start" /t REG_DWORD 
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\wisvc" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\wuauserv" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\BITS" /v "Start" /t REG_DWORD /d "4" /f
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\TrustedInstaller" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\WaaSMedicSvc" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\InventorySvc" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\DoSvc" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\DiagTrack" /v "Start" /t REG_DWORD /d "4" /f
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\NetSetupSvc" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\DsmSvc" /v "Start" /t REG_DWORD /d "4" /f
 
 rem TLS 1.2 or lower is required for Windows Update to work, TLS 1.3 not supported yet
