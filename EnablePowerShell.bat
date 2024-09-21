f5159c4fc81a5aca913856280e6c54b4132bf6a2
shoober420 <shoober420@gmail.com>
2024-09-20 19:48:19 -0700

    Update EnablePowerShell.bat

--- EnablePowerShell.bat
+++ EnablePowerShell.bat
@@ -1,6 +1,6 @@
 rem # Enable PowerShell
 
-rem Enable and start WMI (required for powershell)
+rem # Enable and start WMI (required for powershell)
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Winmgmt" /v "Start" /t REG_DWORD /d "2" /f
 sc config winmgmt start= auto
@@ -10,7 +10,7 @@ reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\Disall
 
 powershell.exe Enable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2Root
 
-# Enable PowerShell scripts
+rem # Enable PowerShell scripts
 powershell.exe Set-ExecutionPolicy RemoteSigned
 
 PAUSE
