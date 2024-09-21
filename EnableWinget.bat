e7d88d9cf058356dce8018a82619bdedb676178f
shoober420 <shoober420@gmail.com>
2024-09-20 23:11:12 -0700

    Update EnableWinget.bat

--- EnableWinget.bat
+++ EnableWinget.bat
@@ -1,7 +1,10 @@
 rem # Enable Winget
 
-rem # Enable and start WMI (required for winget)
+rem # winget requires "Application Information" service to be running
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Appinfo" /v "Start" /t REG_DWORD /d "2" /f
+net start Appinfo
 
+rem # Enable and start WMI (required for winget)
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Winmgmt" /v "Start" /t REG_DWORD /d "2" /f
 sc config winmgmt start= auto
 net start winmgmt
