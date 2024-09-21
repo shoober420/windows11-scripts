b9a94fb250b50a0060b8466b0e0a0b759bf55f63
shoober420 <shoober420@gmail.com>
2024-09-21 00:44:02 -0700

    Update

---
+++ DisableSGRMBroker.bat
@@ -0,0 +1,7 @@
+rem # Disable System Guard Runtime Monitor Broker
+net stop SgrmBroker
+net stop SgrmAgent
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\SgrmBroker" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\SgrmAgent" /v "Start" /t REG_DWORD /d "4" /f
+
+PAUSE
