a50584cc2f38371124a81a7e8125ed1bbc99cc75
shoober420 <shoober420@gmail.com>
2024-09-21 04:32:39 -0700

    Update

---
+++ DisableApplicationFrameHost.bat
@@ -0,0 +1,8 @@
+rem # Disable Application Frame Host (to restore run "sfc /scannow")
+takeown /s %computername% /u %username% /f "%WinDir%\System32\ApplicationFrameHost.exe"
+icacls "%WinDir%\System32\ApplicationFrameHost.exe" /grant:r %username%:F
+taskkill /im ApplicationFrameHost.exe /f
+rem del "%WinDir%\System32\ApplicationFrameHost.exe" /s /f /q
+ren "%WinDir%\System32\ApplicationFrameHost.exe" "ApplicationFrameHost.exe.bak"
+
+PAUSE
