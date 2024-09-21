052c8c38f06b4012afbf3cd2b2c47050f164af1b
shoober420 <shoober420@gmail.com>
2024-09-21 01:10:38 -0700

    Update DisableSAM.bat

--- DisableSAM.bat
+++ DisableSAM.bat
@@ -1,5 +1,11 @@
 rem # Disable Microsoft SAM (Security Accounts Manager)
 rem # May cause black screen after login if disabled
+
+rem # Requires SAFE MODE
+rem # Requires SetACL.bat script to be run first
+
+PAUSE
+
 net stop SamSs
 reg add "HKLM\System\CurrentControlSet\Services\SamSs" /v "Start" /t REG_DWORD /d "4" /f
 
