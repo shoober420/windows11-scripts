be8ffdb2ef4d700190ddc7f28584262a8242ea45
shoober420 <shoober420@gmail.com>
2024-09-21 02:42:29 -0700

    Update DisableWindowsUpdate.bat

--- DisableWindowsUpdate.bat
+++ DisableWindowsUpdate.bat
@@ -4,7 +4,6 @@ net stop UsoSvc
 net stop wisvc
 net stop wuauserv
 net stop BITS
-net stop TrustedInstaller
 net stop WaaSMedicSvc
 net stop cryptsvc
 net stop InventorySvc
@@ -19,7 +18,6 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Services\UsoSvc" /v "Start" /t REG_DWORD 
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\wisvc" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\wuauserv" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\BITS" /v "Start" /t REG_DWORD /d "4" /f
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\TrustedInstaller" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\WaaSMedicSvc" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\cryptsvc" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\InventorySvc" /v "Start" /t REG_DWORD /d "4" /f
