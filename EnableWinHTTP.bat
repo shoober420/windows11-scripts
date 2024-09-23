2442ff5435ca97b001184b8660a97356e6bf4fb7
shoober420 <shoober420@gmail.com>
2024-09-23 01:53:39 -0700

    Update EnableWinHTTP.bat

--- EnableWinHTTP.bat
+++ EnableWinHTTP.bat
@@ -1,6 +1,9 @@
-rem Enable WinHttpAutoProxySvc
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\WinHttpAutoProxySvc" /v "Start" /t REG_DWORD /d "2" /f
+rem # Enable WinHTTP Web Proxy Auto-Discovery Service (WinHttpAutoProxySvc)
+rem # required for WiFi and wireless internet
 
+PAUSE
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\WinHttpAutoProxySvc" /v "Start" /t REG_DWORD /d "2" /f
 net start WinHttpAutoProxySvc
 
 PAUSE
