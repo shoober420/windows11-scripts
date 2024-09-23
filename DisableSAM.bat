5b4cfced5bdb9dcff7fccb3cc79ce94dacbce3ac
shoober420 <shoober420@gmail.com>
2024-09-22 21:41:01 -0700

    Update DisableSAM.bat

--- DisableSAM.bat
+++ DisableSAM.bat
@@ -10,6 +10,6 @@ net stop SamSs
 reg add "HKLM\System\CurrentControlSet\Services\SamSs" /v "Start" /t REG_DWORD /d "4" /f
 
 net stop LanmanServer
-reg add "HKLM\System\CurrentControlSet\Services\SamSs" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\System\CurrentControlSet\Services\LanmanServer" /v "Start" /t REG_DWORD /d "4" /f
 
 PAUSE
