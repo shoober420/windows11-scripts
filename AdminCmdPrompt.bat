427f2337aaa97881807f86fc2b72bb37d22c004c
shoober420 <shoober420@gmail.com>
2024-09-23 16:13:55 -0700

    Update AdminCmdPrompt.bat

--- AdminCmdPrompt.bat
+++ AdminCmdPrompt.bat
@@ -1,7 +1,9 @@
-REM Launch Admin Command Prompt
-REM Will NOT work under SAFE MODE
-REM requires EnableRunasAdmin.bat (Secondary Logon) (will NOT work under SAFE MODE)
+rem # Launch Powershell Admin Command Prompt
+rem # Will NOT work under SAFE MODE
+rem # requires EnableRunasAdmin.bat (Secondary Logon) (will NOT work under SAFE MODE)
 
 REM runas /user:<adminaccount> cmd
+
+powershell start-process powershell -verb runas
 
 PAUSE
