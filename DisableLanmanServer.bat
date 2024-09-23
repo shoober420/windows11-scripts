b5a2b7a374a40cbdeaee36e085ef05a95178fbe8
shoober420 <shoober420@gmail.com>
2024-09-22 20:22:33 -0700

    Update

---
+++ DisableLanmanServer.bat
@@ -0,0 +1,7 @@
+rem # Disables LanmanServer (Server service)
+rem # Fixes black screen when SecurityAccountManager is disabled
+
+net stop LanmanServer
+reg add "HKLM\System\CurrentControlSet\Services\SamSs" /v "Start" /t REG_DWORD /d "4" /f
+
+PAUSE
