49ad4876b84b8b50500c19439148ea6fc406217c
shoober420 <shoober420@gmail.com>
2024-09-22 21:41:47 -0700

    Update EnableLanmanServer.bat

--- EnableLanmanServer.bat
+++ EnableLanmanServer.bat
@@ -1,6 +1,6 @@
 rem # Enables LanmanServer (Server service)
 
-reg add "HKLM\System\CurrentControlSet\Services\SamSs" /v "Start" /t REG_DWORD /d "2" /f
+reg add "HKLM\System\CurrentControlSet\Services\LanmanServer" /v "Start" /t REG_DWORD /d "2" /f
 net start LanmanServer
 
 PAUSE
