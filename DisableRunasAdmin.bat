cb50df8240d9e3801bdd8f0f60dfb6e3abf8a19b
shoober420 <shoober420@gmail.com>
2024-09-23 01:17:57 -0700

    Update DisableRunasAdmin.bat

--- DisableRunasAdmin.bat
+++ DisableRunasAdmin.bat
@@ -1,9 +1,7 @@
-rem Disables required services for "Run as administrator"
+rem # Disables Secondary Logon service for "Run as administrator"
 
-net stop Appinfo
 net stop seclogon
 
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\Appinfo" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\seclogon" /v "Start" /t REG_DWORD /d "4" /f
 
 PAUSE
