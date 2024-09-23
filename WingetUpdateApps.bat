acbfdb296bf110171a84feaa69d574fd63771c1e
shoober420 <shoober420@gmail.com>
2024-09-23 01:45:06 -0700

    Update WingetUpdateApps.bat

--- WingetUpdateApps.bat
+++ WingetUpdateApps.bat
@@ -1,8 +1,17 @@
-rem Update winget apps
+rem # Update winget apps
 
-rem winget requires "Application Information" service to be running
+rem # winget requires "Application Information" service
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Appinfo" /v "Start" /t REG_DWORD /d "2" /f
 net start Appinfo
+
+rem # Enable and start WMI (required for winget)
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Winmgmt" /v "Start" /t REG_DWORD /d "2" /f
+sc config winmgmt start= auto
+net start winmgmt
+
+rem # Enable AppXSvc (required for winget)
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\AppXSvc" /v "Start" /t REG_DWORD /d "2" /f
+net start AppXSvc
 
 winget upgrade --all --include-unknown
 
