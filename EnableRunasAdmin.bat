0ad6df0cc8228a6a9b4867a3b6da12a9d070dac3
shoober420 <shoober420@gmail.com>
2024-09-23 01:20:47 -0700

    Update EnableRunasAdmin.bat

--- EnableRunasAdmin.bat
+++ EnableRunasAdmin.bat
@@ -1,6 +1,6 @@
 rem Enables requires services for "Run as administrator"
 
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\Appinfo" /v "Start" /t REG_DWORD /d "2" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Appinfo" /v "Start" /t REG_DWORD /d "3" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\seclogon" /v "Start" /t REG_DWORD /d "2" /f
 
 net start Appinfo
