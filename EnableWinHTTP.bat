040fe583a11775ea19bd94cd39629deec3de2f2c
shoober420 <shoober420@gmail.com>
2024-09-23 18:07:01 -0700

    Update EnableWinHTTP.bat

--- EnableWinHTTP.bat
+++ EnableWinHTTP.bat
@@ -6,4 +6,6 @@ PAUSE
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\WinHttpAutoProxySvc" /v "Start" /t REG_DWORD /d "2" /f
 net start WinHttpAutoProxySvc
 
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\WinHttp" /v "DisableWpad" /t REG_DWORD /d "0" /f
+
 PAUSE
