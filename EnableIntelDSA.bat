c081c072a6e76d2c4394ede2821fdfa07294a14e
shoober420 <shoober420@gmail.com>
2024-09-23 21:03:56 -0700

    Update EnableIntelDSA.bat

--- EnableIntelDSA.bat
+++ EnableIntelDSA.bat
@@ -1,8 +1,8 @@
 REM Enable Intel Driver & Support Assistant (Intel DSA)
 REM Execute EnableWinUpdate.bat recommended
 
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\TrustedInstaller" /v "Start" /t REG_DWORD /d "2" /f
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\Appinfo" /v "Start" /t REG_DWORD /d "2" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\TrustedInstaller" /v "Start" /t REG_DWORD /d "3" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Appinfo" /v "Start" /t REG_DWORD /d "3" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Winmgmt" /v "Start" /t REG_DWORD /d "2" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\DSAService" /v "Start" /t REG_DWORD /d "2" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\DSAUpdateService" /v "Start" /t REG_DWORD /d "2" /f
