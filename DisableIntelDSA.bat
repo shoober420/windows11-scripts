4f9f9fdaca5ba441fcd03f3a0518365a416bc224
shoober420 <shoober420@gmail.com>
2024-09-23 21:03:21 -0700

    Update DisableIntelDSA.bat

--- DisableIntelDSA.bat
+++ DisableIntelDSA.bat
@@ -1,9 +1,6 @@
-REM Disable Intel Driver & Support Assistant (Intel DSA)
-REM Execute DisableWinUpdate.bat recommended
+rem # Disable Intel Driver & Support Assistant (Intel DSA)
+rem # Execute DisableWinUpdate.bat recommended
 
-net stop TrustedInstaller
-net stop Appinfo
-net stop Winmgmt
 net stop DSAService
 net stop DSAUpdateService
 net stop SystemUsageReportSvc_QUEENCREEK
@@ -11,9 +8,6 @@ net stop "Intel(R) SUR QC SAM"
 net stop ESRV_SVC_QUEENCREEK
 net stop USER_ESRV_SVC_QUEENCREEK
 
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\TrustedInstaller" /v "Start" /t REG_DWORD /d "4" /f
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\Appinfo" /v "Start" /t REG_DWORD /d "4" /f
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\Winmgmt" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\DSAService" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\DSAUpdateService" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\SystemUsageReportSvc_QUEENCREEK" /v "Start" /t REG_DWORD /d "4" /f
