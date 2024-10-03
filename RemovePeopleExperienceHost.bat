16f17c62254cb3fbd3fba25954af1a88c59f1c1b
shoober420 <shoober420@gmail.com>
2024-10-03 04:13:35 -0700

    Update RemovePeopleExperienceHost.bat

--- RemovePeopleExperienceHost.bat
+++ RemovePeopleExperienceHost.bat
@@ -1,7 +1,4 @@
 rem # Uninstall Windows People Experience Host
-rem # Requires SAFE MODE
-
-PAUSE
 
 powershell -command "Get-AppxPackage *Microsoft.Windows.PeopleExperienceHost* | Remove-AppxPackage"
 
