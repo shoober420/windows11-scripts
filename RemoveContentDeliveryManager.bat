09728f7013e77bc1dafdc9584d32665cba44ad78
shoober420 <shoober420@gmail.com>
2024-10-03 04:12:32 -0700

    Update RemoveContentDeliveryManager.bat

--- RemoveContentDeliveryManager.bat
+++ RemoveContentDeliveryManager.bat
@@ -1,7 +1,4 @@
 rem # Uninstall Microsoft Windows Content Delivery Manager
-rem # Requires SAFE MODE
-
-PAUSE
 
 powershell -command "Get-AppxPackage *Microsoft.Windows.ContentDeliveryManager* | Remove-AppxPackage"
 
