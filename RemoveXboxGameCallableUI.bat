12f00626e2e97ac75117f5e4356d038e1b08eb59
shoober420 <shoober420@gmail.com>
2024-10-03 00:17:52 -0700

    Update RemoveXboxGameCallableUI.bat

--- RemoveXboxGameCallableUI.bat
+++ RemoveXboxGameCallableUI.bat
@@ -2,3 +2,7 @@ rem # Uninstall Microsoft Xbox Game Callable UI
 rem # Requires SAFE MODE
 
 PAUSE
+
+powershell -command "Get-AppxPackage *Microsoft.XboxGameCallableUI* | Remove-AppxPackage"
+
+PAUSE
