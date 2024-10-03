634813d4c6b93b41b469275cd2d3c2e997c1956a
shoober420 <shoober420@gmail.com>
2024-10-03 00:25:13 -0700

    Update RemoveShellExperienceHost.bat

--- RemoveShellExperienceHost.bat
+++ RemoveShellExperienceHost.bat
@@ -2,3 +2,7 @@ rem # Uninstall Windows Shell Experience Host
 rem # Requires SAFE MODE
 
 PAUSE
+
+powershell -command "Get-AppxPackage *Microsoft.Windows.ShellExperienceHost* | Remove-AppxPackage"
+
+PAUSE
