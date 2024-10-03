a87f1adae75e0cdaca219a041be1cfc613262f8a
shoober420 <shoober420@gmail.com>
2024-10-03 04:14:36 -0700

    Update RemoveStartMenuExperienceHost.bat

--- RemoveStartMenuExperienceHost.bat
+++ RemoveStartMenuExperienceHost.bat
@@ -1,7 +1,4 @@
 rem # Uninstall Windows Start Menu Experience Host
-rem # Requires SAFE MODE
-
-PAUSE
 
 powershell -command "Get-AppxPackage *Microsoft.Windows.StartMenuExperienceHost* | Remove-AppxPackage"
 
