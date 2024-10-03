ba67011654dd124a4584c3622e3fee9c7c3ff699
shoober420 <shoober420@gmail.com>
2024-10-03 04:13:18 -0700

    Update RemoveNarratorQuickStart.bat

--- RemoveNarratorQuickStart.bat
+++ RemoveNarratorQuickStart.bat
@@ -1,7 +1,4 @@
 rem # Uninstall Microsoft Windows Narrator Quick Start
-rem # Requires SAFE MODE
-
-PAUSE
 
 powershell -command "Get-AppxPackage *Microsoft.Windows.NarratorQuickStart* | Remove-AppxPackage"
 
