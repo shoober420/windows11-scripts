18a61eb23a4e764c1ffb60b049a40aab5ee5c35f
shoober420 <shoober420@gmail.com>
2024-10-06 10:11:17 -0700

    Update

---
+++ BlockRazerSynapse.bat
@@ -0,0 +1,10 @@
+rem # Prevent Razer Synapse auto install (rubber ducky attack)
+
+del C:\Windows\Installer\Razer
+mkdir C:\Windows\Installer\Razer
+
+icacls C:\Windows\Installer\Razer /deny Everyone:(OI)(CI)F /t /c
+icacls C:\Windows\Installer\Razer /deny SYSTEM:(OI)(CI)F /t /c
+icacls C:\Windows\Installer\Razer /deny Administrators:(OI)(CI)F /t /c
+
+PAUSE
