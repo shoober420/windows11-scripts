b183753a8b2246128bd230428d035e39c8e56d1c
shoober420 <shoober420@gmail.com>
2024-10-08 08:12:37 -0700

    Update BlockRazerSynapse.bat

--- BlockRazerSynapse.bat
+++ BlockRazerSynapse.bat
@@ -1,9 +1,14 @@
 rem # Prevent Razer Synapse auto install (rubber ducky attack)
 
-rem # OpenRGB (buggy)
-rem # if OpenRGB borks lighting and shuts it off even after rebooting, you will need to download the company software (NZXT CAM) and tweak it again to get it to turn on
+rem # NEVER INSTALL RGB SOFTWARE, LEAVE AT DEFAULTS
+
+rem # OpenRGB (DANGEROUSLY BUGGY)
+rem # OpenRGB can bork your RAM RGB by itself, and even fan rgb, along with led strips if used in conjuction with other RGB software
+
+rem # if OpenRGB borks fan lighting and shuts it off even after rebooting, you will need to download the company software (NZXT CAM) and tweak it again to get it to turn on
 
 rem # SignalRGB (bloat)
+rem # SignalRGB needs to be running jn the background for RGB effects to work
 
 del C:\Windows\Installer\Razer
 mkdir C:\Windows\Installer\Razer
