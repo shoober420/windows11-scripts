9fadd777c7f0e922654f0771c5c388a86a1726cd
shoober420 <shoober420@gmail.com>
2024-10-08 07:00:05 -0700

    Update BlockRazerSynapse.bat

--- BlockRazerSynapse.bat
+++ BlockRazerSynapse.bat
@@ -1,5 +1,9 @@
 rem # Prevent Razer Synapse auto install (rubber ducky attack)
-rem # use SignalRGB instead
+
+rem # OpenRGB (buggy)
+rem # if OpenRGB borks lighting and shuts it off even after rebooting, you will need to download the company software (NZXT CAM) and tweak it again to get it to turn on
+
+rem # SignalRGB (bloat)
 
 del C:\Windows\Installer\Razer
 mkdir C:\Windows\Installer\Razer
