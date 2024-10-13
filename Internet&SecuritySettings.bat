3c9805be938a4f24eec40458393cb63a460d9193
shoober420 <shoober420@gmail.com>
2024-10-13 15:06:47 -0700

    Update Internet&SecuritySettings.bat

--- Internet&SecuritySettings.bat
+++ Internet&SecuritySettings.bat
@@ -181,7 +181,7 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "DisableIPS
 netsh int isatap set state disable
 netsh int tcp set global ecn=disabled
 
-rem # enable BBR2 
+rem # enable BBR2 Congestion Control Provider
 netsh int tcp set supplemental Template=Internet CongestionProvider=bbr2
 netsh int tcp set supplemental Template=Datacenter CongestionProvider=bbr2
 netsh int tcp set supplemental Template=Compat CongestionProvider=bbr2
