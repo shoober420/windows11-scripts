2fd64231dec37e7faacd4f6509d018da4f2ead10
shoober420 <shoober420@gmail.com>
2024-10-22 23:35:51 -0700

    Update NetworkTweaks.bat

--- NetworkTweaks.bat
+++ NetworkTweaks.bat
@@ -115,6 +115,7 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\0001" /v "TxAbsIntDelay" /t REG_SZ /d "0" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\0001" /v "RxIntDelay" /t REG_SZ /d "0" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\0001" /v "RxAbsIntDelay" /t REG_SZ /d "0" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\0001" /v "FlowControlCap" /t REG_SZ /d "0" /f
 
 rem # Interrupt Moderation Rate: Interrupt Throttling Rate (ITR)
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\0001" /v "ITR" /t REG_SZ /d "0" /f
