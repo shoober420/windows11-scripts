62851ca74f5a2cc83d9e096ad22b030e38596508
shoober420 <shoober420@gmail.com>
2024-10-28 15:52:24 -0700

    Update DataQueueSize.bat

--- DataQueueSize.bat
+++ DataQueueSize.bat
@@ -6,4 +6,16 @@ rem # https://www.clickspeedtester.com/keyboard-latency-test/
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\mouclass\Parameters" /v "MouseDataQueueSize" /t REG_DWORD /d "0x00000010" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\kbdclass\Parameters" /v "KeyboardDataQueueSize" /t REG_DWORD /d "0x00000010" /f
 
+rem # Repeat Rate
+rem # May increase latency if 1+
+reg add "HKCU\Control Panel\Keyboard" /v "KeyboardSpeed" /t REG_DWORD /d "0" /f
+reg add "HKCU\Control Panel\Desktop" /v "KeyboardSpeed" /t REG_DWORD /d "0" /f
+
+rem # Repeat Delay
+rem # May increase latency if 1+
+reg add "HKCU\Control Panel\Keyboard" /v "KeyboardDelay" /t REG_DWORD /d "0" /f
+reg add "HKCU\Control Panel\Keyboard" /v "TypematicDelay" /t REG_DWORD /d "0" /f
+
+
+
 PAUSE
