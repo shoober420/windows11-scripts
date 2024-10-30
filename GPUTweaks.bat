2765778a7899b050912ca299820ec41be8f1e2ae
shoober420 <shoober420@gmail.com>
2024-10-30 16:40:42 -0700

    Update GPUTweaks.bat

--- GPUTweaks.bat
+++ GPUTweaks.bat
@@ -8,6 +8,7 @@ rem # HighPerfAdapter must match Hardware ID in Device Manager for GPU
 rem # Device Manager > Display Adapters > "GPUNAME" > Properties > Details > Property > Hardware Ids
 rem # HighPerfAdapter="VEN&DEV&SUBSYS" / PCI\VEN_10DE&DEV_2684&SUBSYS_89321043 / HighPerfAdapter=10DE&2684&89321043
 rem reg add "HKCU\Software\Microsoft\DirectX\UserGpuPreferences" /v "DirectXUserGlobalSettings" /t REG_SZ /d "HighPerfAdapter=10DE&2684&89321043;VRROptimizeEnable=0;AutoHDREnable=1;SwapEffectUpgradeEnable=1" /f
+
 rem # garlin: https://www.elevenforum.com/members/garlin.5387/
 rem # https://www.elevenforum.com/t/find-gpu-ven-dev-subsys-values-for-highperfadapter-option-using-wmic-or-other-method.30127/post-521164
 
