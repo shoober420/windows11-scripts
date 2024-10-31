e503ac715ce6fd56d0a6087783bfebfcea11f15d
shoober420 <shoober420@gmail.com>
2024-10-31 00:01:51 -0700

    Update

---
+++ DHCP.bat
@@ -0,0 +1,7 @@
+rem # Enable DHCP Connection
+netsh interface ip set address name="Ethernet" dhcp
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Dhcp" /v "Start" /t REG_DWORD /d "2" /f
+
+sc start dhcp
+
+PAUSE
