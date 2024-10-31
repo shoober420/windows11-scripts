288d37e4cc84f1a55464c10cd6b1598390ffbcf1
shoober420 <shoober420@gmail.com>
2024-10-31 14:15:52 -0700

    Update RAMTweaks.bat

--- RAMTweaks.bat
+++ RAMTweaks.bat
@@ -3,6 +3,9 @@ rem # ADJUST VALUES ACCORDING TO RAM SIZE BEFORE RUNNING
 
 PAUSE
 
+rem # https://www.elevenforum.com/members/garlin.5387/
+rem # https://www.elevenforum.com/t/gpu-tweaks-batch-script.30134/post-521530
+
 if not exist C:\Windows\System32\wbem\WMIC.exe (
     echo Installing WMIC...
     DISM /Online /Add-Capability /CapabilityName:WMIC~~~~
