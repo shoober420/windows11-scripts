8881c6cb7f3f098bfd997c700cbd3d3e481f20e6
shoober420 <shoober420@gmail.com>
2024-11-01 16:58:02 -0700

    Update GPUTweaks.bat

--- GPUTweaks.bat
+++ GPUTweaks.bat
@@ -2,6 +2,8 @@ rem # AMD USERS: if screen flickering occurs, remove the "DalDramClockChangeLate
 
 PAUSE
 
+rem # https://www.elevenforum.com/members/garlin.5387/
+rem # https://www.elevenforum.com/t/gpu-tweaks-batch-script.30134/post-521530
 if not exist C:\Windows\System32\wbem\WMIC.exe (
     echo Installing WMIC...
     DISM /Online /Add-Capability /CapabilityName:WMIC~~~~
