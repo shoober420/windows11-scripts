a379aff60a3dc73ba13850de37bc3241c1aa93d1
shoober420 <shoober420@gmail.com>
2024-11-01 16:58:31 -0700

    Update NetworkTweaks.bat

--- NetworkTweaks.bat
+++ NetworkTweaks.bat
@@ -2,6 +2,10 @@ rem !!! SETTINGS REQUIRE ADJUSTING BEFORE BEING EXECUTED !!!
 
 PAUSE
 
+
+rem # https://www.elevenforum.com/members/garlin.5387/
+rem # https://www.elevenforum.com/t/gpu-tweaks-batch-script.30134/post-521530
+
 if not exist C:\Windows\System32\wbem\WMIC.exe (
     echo Installing WMIC...
     DISM /Online /Add-Capability /CapabilityName:WMIC~~~~
