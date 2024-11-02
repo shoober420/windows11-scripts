59dbb2b58e7fd9c988881c3fc7d0ae6e53e7b602
shoober420 <shoober420@gmail.com>
2024-11-02 11:50:04 -0700

    Update DisablePowerManagement.ps1

--- DisablePowerManagement.ps1
+++ DisablePowerManagement.ps1
@@ -2,6 +2,9 @@
 # Disable "Allow the computer to turn off this device to save power"
 # Disable "Allow this device to wake the computer"
 
+# https://www.elevenforum.com/members/pseymour.23912/
+# https://www.elevenforum.com/t/disable-waking-up-from-hibernation-for-good.27700/#post-487462
+
 Set-CimInstance -Query 'SELECT InstanceName FROM MSPower_DeviceWakeEnable WHERE (Enable = True)' -Namespace 'root\WMI' -Property @{Enable = $false}
 
 Set-CimInstance -Query 'SELECT InstanceName FROM MSPower_DeviceEnable WHERE (Enable = True)' -Namespace 'root\WMI' -Property @{Enable = $false}
