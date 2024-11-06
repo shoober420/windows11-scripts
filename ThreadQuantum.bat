caa5b0ba0b492faadd1a0edb08c8c34b9718b49b
shoober420 <shoober420@gmail.com>
2024-11-06 03:21:28 -0800

    Update ThreadQuantum.bat

--- ThreadQuantum.bat
+++ ThreadQuantum.bat
@@ -2,23 +2,23 @@ rem # Thread Quantum
 rem # https://smallvoid.com/article/winnt-process-scheduler-priority.html
 rem # https://forums.blurbusters.com/viewtopic.php?t=8535
 
-rem # 2A Hex = Short, Fixed, High foreground boost (very good input)
+rem # 2A Hex = Short, Fixed, High foreground boost (very good input/latency)
 rem # 29 Hex = Short, Fixed, Medium foreground boost
-rem # 28 Hex = Short, Fixed, No foreground boost (best input, possible lag spikes)
-rem # 26 Hex = Short, Variable, High foreground boost (good input)
+rem # 28 Hex = Short, Fixed, No foreground boost (best input/latency, possible lag spikes)
+rem # 26 Hex = Short, Variable, High foreground boost (good input/latency)
 rem # 25 Hex = Short, Variable, Medium foreground boost
 rem # 24 Hex = Short, Variable, No foreground boost (possible lag spikes)
 rem # 1A Hex = Long, Fixed, High foreground boost
 rem # 19 Hex = Long, Fixed, Medium foreground boost
 rem # 18 Hex = Long, Fixed, No foreground boost
-rem # 16 Hex = Long, Variable, High foreground boost (best performance)
+rem # 16 Hex = Long, Variable, High foreground boost (best performance/FPS)
 rem # 15 Hex = Long, Variable, Medium foreground boost
 rem # 14 Hex = Long, Variable, No foreground boost
 
 rem # "No foreground boost" causes possible lag spikes
 
-rem # Best input: Short, Fixed, No foreground boost (28, possible lag spikes)
-rem # Best performance: Long, Variable, High foreground boost (16)
+rem # Best Input/Latency - 28: Short, Fixed, No foreground boost (possible lag spikes)
+rem # Best Performance/FPS - 16: Long, Variable, High foreground boost
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\PriorityControl" /v "Win32PrioritySeparation" /t REG_DWORD /d "0x00000028" /f
 reg add "HKLM\SYSTEM\ControlSet001\Control\PriorityControl" /v "Win32PrioritySeparation" /t REG_DWORD /d "0x00000028" /f
