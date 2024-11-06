83a8b3f2e709d8287538ab3c72f302ac9e3e04aa
shoober420 <shoober420@gmail.com>
2024-11-05 22:00:16 -0800

    Update ThreadQuantum.bat

--- ThreadQuantum.bat
+++ ThreadQuantum.bat
@@ -1,6 +1,7 @@
 rem # Thread Quantum
 rem # https://smallvoid.com/article/winnt-process-scheduler-priority.html
 rem # https://forums.blurbusters.com/viewtopic.php?t=8535
+
 rem # 2A Hex = Short, Fixed, High foreground boost (very good input)
 rem # 29 Hex = Short, Fixed, Medium foreground boost
 rem # 28 Hex = Short, Fixed, No foreground boost (best input, possible lag spikes)
@@ -13,9 +14,12 @@ rem # 18 Hex = Long, Fixed, No foreground boost
 rem # 16 Hex = Long, Variable, High foreground boost (best performance)
 rem # 15 Hex = Long, Variable, Medium foreground boost
 rem # 14 Hex = Long, Variable, No foreground boost
+
+rem # "No foreground boost" causes possible lag spikes
+
 rem # Best input: Short, Fixed, No foreground boost (28, possible lag spikes)
 rem # Best performance: Long, Variable, High foreground boost (16)
-rem # "No foreground boost" causes lag spikes
+
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\PriorityControl" /v "Win32PrioritySeparation" /t REG_DWORD /d "0x00000028" /f
 reg add "HKLM\SYSTEM\ControlSet001\Control\PriorityControl" /v "Win32PrioritySeparation" /t REG_DWORD /d "0x00000028" /f
 
