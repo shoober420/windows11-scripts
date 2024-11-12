65fa7997c12463c5975da105f7ba8ceec38017d6
shoober420 <shoober420@gmail.com>
2024-11-12 13:36:47 -0500

    Update GPUTweaks.bat

--- GPUTweaks.bat
+++ GPUTweaks.bat
@@ -12,6 +12,20 @@ if not exist C:\Windows\System32\wbem\WMIC.exe (
 
 rem # GPU tweaks for any graphics card
 
+rem # SwapEffectUpgradeEnable = Optimizations for windowed and borderless windowed games
+rem # HighPerfAdapter must match Hardware ID in Device Manager for GPU
+rem # Device Manager > Display Adapters > "GPUNAME" > Properties > Details > Property > Hardware Ids
+rem # HighPerfAdapter="VEN&DEV&SUBSYS" / PCI\VEN_10DE&DEV_2684&SUBSYS_89321043 / HighPerfAdapter=10DE&2684&89321043
+
+rem reg add "HKCU\Software\Microsoft\DirectX\UserGpuPreferences" /v "DirectXUserGlobalSettings" /t REG_SZ /d "HighPerfAdapter=10DE&2684&89321043;VRROptimizeEnable=0;AutoHDREnable=1;SwapEffectUpgradeEnable=1" /f
+
+rem # garlin: https://www.elevenforum.com/members/garlin.5387/
+rem # https://www.elevenforum.com/t/find-gpu-ven-dev-subsys-values-for-highperfadapter-option-using-wmic-or-other-method.30127/post-521164
+
+for /f "tokens=1-6 delims=_&" %%a in ('wmic path win32_videocontroller get PNPDeviceID ^| findstr PCI') do (
+    reg add "HKCU\Software\Microsoft\DirectX\UserGpuPreferences" /v "DirectXUserGlobalSettings" /t REG_SZ /d "HighPerfAdapter=%%b&%%d&%%f;VRROptimizeEnable=0;AutoHDREnable=1;SwapEffectUpgradeEnable=1" /f 
+)
+
 rem # Affinity tweak
 rem # garlin: https://www.elevenforum.com/members/garlin.5387/
 rem # https://www.elevenforum.com/t/wmic-query-cpu-core-count-to-execute-setting.30155/post-521450
