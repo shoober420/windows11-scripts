b78a166e7abef326707d0944b8f6cf9a66f69173
shoober420 <shoober420@gmail.com>
2024-11-27 15:57:38 -0500

    Update AMDGPUTweaks.bat

--- AMDGPUTweaks.bat
+++ AMDGPUTweaks.bat
@@ -4,6 +4,10 @@ rem # AMD USERS: if screen flickering occurs, remove the "DalDramClockChangeLate
 
 PAUSE
 
+rem # Sources:
+rem # https://sites.google.com/view/melodystweaks/basictweaks
+rem # https://github.com/sherifmagdy32/gaming_os_tweaker
+
 rem # https://www.elevenforum.com/members/garlin.5387/
 rem # https://www.elevenforum.com/t/gpu-tweaks-batch-script.30134/post-521530
 if not exist C:\Windows\System32\wbem\WMIC.exe (
