e10d53db121b4799cf75a35e9e38b557a00e98ac
shoober420 <shoober420@gmail.com>
2024-12-05 10:47:28 -0500

    Update NVGPUTweaks.bat

--- NVGPUTweaks.bat
+++ NVGPUTweaks.bat
@@ -246,9 +246,6 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "TdrTestMode"
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "TdrDodPresentDelay" /t REG_DWORD /d 0 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "TdrDodVSyncDelay" /t REG_DWORD /d 0 /f
 			 
-rem # IGPU Dedicated Segment Size
-reg add "HKLM\SOFTWARE\Intel\GMM" /v "DedicatedSegmentSize" /t REG_DWORD /d "512"
-
 rem # Disable NVIDIA tray icon
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\NVIDIA Corporation\NvTray" /v "StartOnLogin" /t REG_DWORD /d 0 /f
 
