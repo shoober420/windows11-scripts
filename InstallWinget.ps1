d90dfccf7fa2bd03ab698f668f48df2189ae58fa
shoober420 <shoober420@gmail.com>
2025-01-14 02:29:54 -0500

    Update

---
+++ InstallWinget.ps1
@@ -0,0 +1,20 @@
+rem # Installs Winget with Powershell
+
+rem # https://stackoverflow.com/users/586898/vladan
+rem # https://stackoverflow.com/questions/74166150/install-winget-by-the-command-line-powershell/75334942#75334942
+
+# get latest download url
+$URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
+$URL = (Invoke-WebRequest -Uri $URL).Content | ConvertFrom-Json |
+        Select-Object -ExpandProperty "assets" |
+        Where-Object "browser_download_url" -Match '.msixbundle' |
+        Select-Object -ExpandProperty "browser_download_url"
+
+# download
+Invoke-WebRequest -Uri $URL -OutFile "Setup.msix" -UseBasicParsing
+
+# install
+Add-AppxPackage -Path "Setup.msix"
+
+# delete file
+Remove-Item "Setup.msix"
