1227ecc9263402ae53bc5217b032c908c12af7f1
shoober420 <shoober420@gmail.com>
2025-01-14 04:36:56 -0500

    Update DownloadFile.ps1

--- DownloadFile.ps1
+++ DownloadFile.ps1
@@ -2,8 +2,6 @@ rem # Download file with PowerShell
  
 rem # https://raw.githubusercontent.com/shoober420/windows11-scripts/refs/heads/main/
 
-rem # https://commondatastorage.googleapis.com/chromium-browser-snapshots/index.html?prefix=Win_x64/LAST_CHANGE/chrome_win.zip
-
 rem # Change $Path to desired file name
  
 Invoke-WebRequest -URI $URL -OutFile $Path
