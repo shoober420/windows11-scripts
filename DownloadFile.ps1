37529f0a3829827ea0ae2fa8b857e3bfd48ad76c
shoober420 <shoober420@gmail.com>
2025-01-14 04:08:07 -0500

    Update DownloadFile.ps1

--- DownloadFile.ps1
+++ DownloadFile.ps1
@@ -1,5 +1,7 @@
 rem # Download file with PowerShell
  
 rem # https://raw.githubusercontent.com/shoober420/windows11-scripts/refs/heads/main/
+
+rem # Change $Path to desired file name
  
 Invoke-WebRequest -URI $URL -OutFile $Path
