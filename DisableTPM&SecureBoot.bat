e734b10ae9e6255dc87625f49f60273075c5aea7
shoober420 <shoober420@gmail.com>
2025-01-15 16:48:46 -0500

    Update

---
+++ DisableTPM&SecureBoot.bat
@@ -0,0 +1,14 @@
+rem # Disable TPM and SecureBoot
+
+rem # MUST BE LAUNCHED BEFORE WINDOWS 11 INSTALLATION TO DISABLE TPM AND SECUREBOOT
+
+rem # TPM AND SECURE BOOT MUST BE DISABLED TO DO A WINRE "RESET THIS PC" WITHOUT A RECOVERY KEY BEING NEEDED
+
+PAUSE
+
+reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassTPMCheck" /t REG_DWORD /d "1" /f
+reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassSecureBootCheck " /t REG_DWORD /d "1" /f
+reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassRAMCheck" /t REG_DWORD /d "1" /f
+reg add "HKLM\SYSTEM\Setup\MoSetup" /v "AllowUpgradesWithUnsupportedTPMOrCPU" /t REG_DWORD /d "1" /f
+
+PAUSE
