51dfb7dd0ccd08d5af31c530671d52d65066e749
shoober420 <shoober420@gmail.com>
2025-01-15 13:14:10 -0500

    Update EnableTLS1.2.bat

--- EnableTLS1.2.bat
+++ EnableTLS1.2.bat
@@ -1,6 +1,8 @@
 rem # Enables TLS 1.2
 
-rem # TLS 1.2 or lower is required for Windows Update to work, TLS 1.3 not supported yet
+rem # TLS 1.2 or lower is required for Windows Update and DISM to work, TLS 1.3 not supported yet
+
+PAUSE
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client" /v "Enabled" /t REG_DWORD /d "1" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client" /v "DisabledByDefault" /t REG_DWORD /d "0" /f
