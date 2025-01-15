fdd1f8d937b92dc2d293e01555a2297da2de397f
shoober420 <shoober420@gmail.com>
2025-01-15 18:47:44 -0500

    Update DisableTPM&SecureBoot.bat

--- DisableTPM&SecureBoot.bat
+++ DisableTPM&SecureBoot.bat
@@ -19,5 +19,6 @@ reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassTPMCheck" /t REG_DWORD /d "1" /f
 reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassSecureBootCheck " /t REG_DWORD /d "1" /f
 reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassRAMCheck" /t REG_DWORD /d "1" /f
 reg add "HKLM\SYSTEM\Setup\MoSetup" /v "AllowUpgradesWithUnsupportedTPMOrCPU" /t REG_DWORD /d "1" /f
+reg add "HKLM\System\CurrentControlSet\Control\BitLocker" /v "PreventDeviceEncryption" /t REG_DWORD /d "1" /f
 
 PAUSE
