1af20abe4d3556596a4bf1d201dc3d7d1d6a817a
shoober420 <shoober420@gmail.com>
2025-01-15 15:10:49 -0500

    Update EnablePowerShell.bat

--- EnablePowerShell.bat
+++ EnablePowerShell.bat
@@ -14,6 +14,10 @@ rem # Powershell requires "Application Information" service to be running
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Appinfo" /v "Start" /t REG_DWORD /d "3" /f
 net start Appinfo
 
+rem # Csmc: https://www.elevenforum.com/members/csmc.38355/
+rem # Launches PowerShell scripts with admin privileges
+reg add "HKCR\Applications\powershell.exe\shell\open\command" /ve /t REG_SZ /d "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoLogo -ExecutionPolicy unrestricted -File \"%%1\"" /f >nul 2>&1
+
 reg del "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\DisallowRun\powershell.exe"
 
 powershell.exe Enable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2Root
