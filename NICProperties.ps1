fe135517f7648f955d0365ed2b6b0ecb533cd8e8
shoober420 <shoober420@gmail.com>
2025-01-15 15:47:24 -0500

    Update NICProperties.ps1

--- NICProperties.ps1
+++ NICProperties.ps1
@@ -1,11 +1,12 @@
 # Disable bloat NIC protocols and drivers
-Disable-NetAdapterBinding -Name * -DisplayName "Client for Microsoft Networks"
-Disable-NetAdapterBinding -Name * -DisplayName "File and Printer Sharing for Microsoft Networks"
-Disable-NetAdapterQos -Name *
-Disable-NetAdapterBinding -Name * -DisplayName "Microsoft Network Adapter Multiplexor Protocol"
-Disable-NetAdapterBinding -Name * -DisplayName "Microsoft LLDP Protocol Driver"
-Disable-NetAdapterBinding -Name * -ComponentID ms_tcpip6
-Disable-NetAdapterBinding -Name * -DisplayName "Link-Layer Topology Discovery Responder"
-Disable-NetAdapterBinding -Name * -DisplayName "Link-Layer Topology Discovery Mapper I/O Driver"
+
+powershell Disable-NetAdapterBinding -Name * -DisplayName 'Client for Microsoft Networks'
+powershell Disable-NetAdapterBinding -Name * -DisplayName 'File and Printer Sharing for Microsoft Networks'
+powershell Disable-NetAdapterQos -Name *
+powershell Disable-NetAdapterBinding -Name * -DisplayName 'Microsoft Network Adapter Multiplexor Protocol'
+powershell Disable-NetAdapterBinding -Name * -DisplayName 'Microsoft LLDP Protocol Driver'
+powershell Disable-NetAdapterBinding -Name * -ComponentID ms_tcpip6
+powershell Disable-NetAdapterBinding -Name * -DisplayName 'Link-Layer Topology Discovery Responder'
+powershell Disable-NetAdapterBinding -Name * -DisplayName 'Link-Layer Topology Discovery Mapper I/O Driver'
 
 cmd /c 'pause'
