59bd6f0052b665fa4b1b7795130cde586c19560c
shoober420 <shoober420@gmail.com>
2025-01-15 14:02:33 -0500

    Update DisableBitLocker.bat

--- DisableBitLocker.bat
+++ DisableBitLocker.bat
@@ -1,5 +1,7 @@
 rem # Disables BitLocker
 
+rem # If asked for a recovery key when doing a WinRE "Refresh my PC", run this script
+
 manage-bde -off C:
 powershell.exe Disable-BitLocker -MountPoint "C:"
 
