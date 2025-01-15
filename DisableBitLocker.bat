dcc4b509ca4aaf0a459825bb3b464f33447bf058
shoober420 <shoober420@gmail.com>
2025-01-15 16:56:17 -0500

    Update DisableBitLocker.bat

--- DisableBitLocker.bat
+++ DisableBitLocker.bat
@@ -2,6 +2,8 @@ rem # Disables BitLocker
 
 rem # Installing Windows 11 without TPM and SecureBoot in Rufus is required to fully disable BitLocker
 
+rem # TPM AND SECURE BOOT MUST BE DISABLED TO DO A WINRE "RESET THIS PC" WITHOUT A RECOVERY KEY BEING NEEDED
+
 manage-bde -off C:
 powershell.exe Disable-BitLocker -MountPoint "C:"
 
