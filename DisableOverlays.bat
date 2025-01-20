8f257b5c07c4e30b840583c494dd683646868102
shoober420 <shoober420@gmail.com>
2025-01-20 03:27:48 -0800

    Update

---
+++ DisableOverlays.bat
@@ -0,0 +1,11 @@
+rem # Disable Overlays for GPU
+
+rem # !!! WARNING !!!
+rem # MAY CAUSE CRASHES AND LOW FPS, USE EnableOverlays.bat TO FIX
+rem # !!! WARNING !!!
+
+PAUSE
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "DisableOverlays" /t REG_DWORD /d 1 /f
+
+PAUSE
