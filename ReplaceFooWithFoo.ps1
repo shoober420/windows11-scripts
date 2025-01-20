0d1f2a631ea2983b46ec7c8183cc87d2f122179e
shoober420 <shoober420@gmail.com>
2025-01-19 23:31:42 -0800

    Update ReplaceFooWithFoo.ps1

--- ReplaceFooWithFoo.ps1
+++ ReplaceFooWithFoo.ps1
@@ -8,6 +8,6 @@
 # For registry tweaks
 Copy-Item C:\Users\$env:username\Downloads\windows11-scripts-main\foo.bat -Destination C:\Users\$env:username\Downloads\windows11-scripts-main\foo.batBACKUP
 $content = Get-Content -Path C:\Users\$env:username\Downloads\windows11-scripts-main\foo.bat
-$newcontent = $content -replace 'add', 'delete' | Set-Content C:\Users\$env:username\Downloads\windows11-scripts-main\foo.bat
+$newcontent = $content -replace 'reg add', 'ECHO Yes | reg delete' -replace '/t REG_DWORD /d "0" /f', '' -replace '/t REG_DWORD /d "1" /f', '' -replace ' /t REG_DWORD /d 0 /f', '' -replace ' /t REG_DWORD /d 1 /f', '' -replace '" /v "', '\' -replace '" /v ', '\' | Set-Content C:\Users\$env:username\Downloads\windows11-scripts-main\foo.bat
 
 PAUSE
