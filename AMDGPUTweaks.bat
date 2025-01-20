c45d7e8221b83e7a9980e9667951be85ed54bfc9
shoober420 <shoober420@gmail.com>
2025-01-19 19:09:32 -0800

    Update AMDGPUTweaks.bat

--- AMDGPUTweaks.bat
+++ AMDGPUTweaks.bat
@@ -30,6 +30,8 @@ for /f %%i in ('wmic path Win32_VideoController get PNPDeviceID^| findstr /L "PC
 
 rem # DalDramClockChangeLatencyNs below can cause screen flickering, delete reg key if flickering occurs
 
+rem reg delete "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i\DalDramClockChangeLatencyNs"
+
                          reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i" /v "DalDramClockChangeLatencyNs" /t REG_DWORD /d "1" /f
 
                      reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i" /v "LTRSnoopL1Latency" /t REG_DWORD /d "1" /f
