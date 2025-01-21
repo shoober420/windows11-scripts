2fff796871f38ba36bc4c6c14e26e73da4acd56f
shoober420 <shoober420@gmail.com>
2025-01-21 02:38:36 -0500

    Update DisableHighPerfAdapter.bat

--- DisableHighPerfAdapter.bat
+++ DisableHighPerfAdapter.bat
@@ -1,5 +1,7 @@
 rem # Disable HighPerfAdapter
 
+rem # HighPerfAdapter decreases performance and should NOT be enabled
+
 rem # SwapEffectUpgradeEnable = Optimizations for windowed and borderless windowed games
 
 reg add "HKCU\Software\Microsoft\DirectX\UserGpuPreferences" /v "DirectXUserGlobalSettings" /t REG_SZ /d "VRROptimizeEnable=0;AutoHDREnable=1;SwapEffectUpgradeEnable=1" /f 
