d582bf9df6453ae9041d43f3fd606228954d0076
shoober420 <shoober420@gmail.com>
2025-01-21 05:47:24 -0500

    Update

---
+++ DisableClearPageFileAtShutdown.bat
@@ -0,0 +1,5 @@
+rem # Disable Delete page file on shutdown
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v "ClearPageFileAtShutdown" /t REG_DWORD /d "0" /f
+
+PAUSE
