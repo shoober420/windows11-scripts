584f9a61f985f511099c4a21843feeeb2f803725
shoober420 <shoober420@gmail.com>
2025-01-23 03:06:21 -0500

    Update RemoveAllWindowsApps.bat

--- RemoveAllWindowsApps.bat
+++ RemoveAllWindowsApps.bat
@@ -4,8 +4,15 @@ rem # powershell "Get-AppxPackage | Remove-AppxPackage"
 
 rem # DONT UNINSTALL "Microsoft.UI.Xaml.CBS", causes constant desktop refresh flicker
 
+rem # "winget list" shows installed apps
+
+rem # powershell -command "Get-AppxPackage | Select Name, PackageFullName" shows installed apps
+
+rem # Add-AppxPackage installs apps
+
 rem # !!! WARNING !!!
-rem # Removes certain user installed apps (not Steam)
+rem # Removes certain user installed apps if not specified below
+rem # You can add to the list of Apps to keep in the string
 rem # Do a backup incase anything of value is lost
 rem # !!! WARNING !!!
 
@@ -21,9 +28,10 @@ rem # Remove all other apps except Microsoft Store
 rem powershell -command "Get-AppXPackage | where-object {$_.name -notlike '*store*'} | Remove-AppxPackage"
 
 rem # Remove all apps except "Microsoft.UI.Xaml.CBS"
-powershell -command "Get-AppXPackage | where-object {$_.name -notlike '*Microsoft.UI.Xaml.CBS*'} | Remove-AppxPackage"
+rem powershell -command "Get-AppXPackage | where-object {$_.name -notlike '*Microsoft.UI.Xaml.CBS*'} | Remove-AppxPackage"
 
 rem # Remove all apps for every user but keep "Microsoft.UI.Xaml.CBS"
+
 powershell -command "Get-AppXPackage -AllUsers | where-object {$_.name -notlike '*Microsoft.UI.Xaml.CBS*'} | Remove-AppxPackage"
 
 PAUSE
