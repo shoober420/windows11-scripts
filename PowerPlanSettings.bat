4d514ef1ad1108c4341934733b400d401333ea96
shoober420 <shoober420@gmail.com>
2025-01-23 02:38:14 -0500

    Update PowerPlanSettings.bat

--- PowerPlanSettings.bat
+++ PowerPlanSettings.bat
@@ -1,4 +1,10 @@
-rem Power Scheme GUID is random, set and check GUID matches using "powercfg -L"
+rem # Power Plan Settings
+
+rem # Power Scheme GUID is random, set and check GUID matches using "powercfg -L"
+
+rem # Recommended to run PowerPlanUltra.bat first before running this script
+
+PAUSE
 
 rem Disable CPU Parking (fixes stuttering)
 reg add "HKLM\SYSTEM\ControlSet001\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\0cc5b647-c1df-4637-891a-dec35c318583" /v "ValueMax" /t REG_DWORD /d "0" /f
