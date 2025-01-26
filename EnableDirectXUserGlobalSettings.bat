ae4d51f544ecbe01e7c0f2b9e7f80c3035851004
shoober420 <shoober420@gmail.com>
2025-01-26 07:58:59 -0500

    Update EnableDirectXUserGlobalSettings.bat

--- EnableDirectXUserGlobalSettings.bat
+++ EnableDirectXUserGlobalSettings.bat
@@ -1,7 +1,5 @@
 rem # Enable DirectXUserGlobalSettings
 
-rem # HighPerfAdapter decreases performance and should NOT be enabled
-
 rem # SwapEffectUpgradeEnable = Optimizations for windowed and borderless windowed games
 
 reg add "HKCU\Software\Microsoft\DirectX\UserGpuPreferences" /v "DirectXUserGlobalSettings" /t REG_SZ /d "VRROptimizeEnable=0;AutoHDREnable=1;SwapEffectUpgradeEnable=1" /f 
