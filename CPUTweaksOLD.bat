032669927abe665dca23f1e7448d4cab38e2bd6b
shoober420 <shoober420@gmail.com>
2025-01-26 09:50:12 -0500

    Update CPUTweaksOLD.bat

--- CPUTweaksOLD.bat
+++ CPUTweaksOLD.bat
@@ -9,12 +9,15 @@ rem # https://martin77s.wordpress.com/2010/04/05/performance-tuning-your-windows
 rem # https://www.thewindowsclub.com/improve-network-speed-using-smb-compression
 rem # https://james-rankin.com/features/the-ultimate-guide-to-windows-logon-optimizations-part-5/
 rem # https://www.wilderssecurity.com/threads/increase-number-of-threads-per-process.317532/
+rem # rem # https://notes.ponderworthy.com/additional-critical-worker-threads-in-windows-speed-tweak
 
 rem # AdditionalCriticalWorkerThreads value increases the number of critical worker threads created for a specified work queue
 
 rem # By increasing the value of this one, you can get more additional worker threads which will allow for more queued I/O in the storage subsystem
 
 rem # Allow more I/O to queue in the storage subsystem
+
+rem # Value is determined by RAM size NOT thread or core count
 
 rem # OLD METHOD: ONLY FOR TESTING
 rem # CPUTweaks.bat IS RECOMMENDED
@@ -83,7 +86,8 @@ goto :end
 
 
 
-rem # Delayed Worker Threads - Threads in this queue have a lower priority and therefore a higher latency because they must compete with other processing for CPU time
+rem # Delayed Worker Threads
+rem # Threads in this queue have a lower priority and therefore a higher latency because they must compete with other processing for CPU time
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Executive" /v "AdditionalDelayedWorkerThreads" /t REG_DWORD /d "0" /f
 
