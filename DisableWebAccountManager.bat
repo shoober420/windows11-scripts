a2811746829620a3de9d4b02e4fc39b6c1109094
shoober420 <shoober420@gmail.com>
2025-01-27 02:14:09 -0500

    Update DisableWebAccountManager.bat

--- DisableWebAccountManager.bat
+++ DisableWebAccountManager.bat
@@ -1,6 +1,9 @@
 rem # Disable Web Account Manager (TokenBroker)
+
 rem # may cause black screens after login
+
 net stop TokenBroker
+
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\TokenBroker" /v "Start" /t REG_DWORD /d "4" /f
 
 PAUSE
