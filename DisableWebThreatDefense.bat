035a1f930f632cc6d8060125e2b4be2266080db2
shoober420 <shoober420@gmail.com>
2025-01-27 02:13:43 -0500

    Update DisableWebThreatDefense.bat

--- DisableWebThreatDefense.bat
+++ DisableWebThreatDefense.bat
@@ -1,6 +1,8 @@
 rem # Disable Web Threat Defense services
+
 net stop webthreatdefsvc
 net stop webthreatdefusersvc
+
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\webthreatdefsvc" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\webthreatdefusersvc" /v "Start" /t REG_DWORD /d "4" /f
 
