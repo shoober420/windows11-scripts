89cf6cb63eb7772233f84a448f1e84f9b3e4f872
shoober420 <shoober420@gmail.com>
2025-01-27 02:12:14 -0500

    Update DisableWindowsSecurityCenter.bat

--- DisableWindowsSecurityCenter.bat
+++ DisableWindowsSecurityCenter.bat
@@ -1,6 +1,9 @@
 rem # Disable Windows Security Center
-rem # requires SAFE MODE
-rem # must run SetACL.bat first
+
+rem # Recommended to run in SAFE MODE
+
+rem # Requires SetACL.bat to be run first
+rem # Gives ownership to reg keys
 
 PAUSE
 
@@ -8,5 +11,8 @@ net stop SecurityHealthService
 net stop wscsvc
 reg add "HKLM\System\CurrentControlSet\Services\SecurityHealthService" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\System\CurrentControlSet\Services\wscsvc" /v "Start" /t REG_DWORD /d "4" /f
+
+rem # Requires SetACL.bat to be run first
+rem # Gives ownership to reg keys
 
 PAUSE
