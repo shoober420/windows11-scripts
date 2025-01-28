229e2f2ecee788eafd62b95f435935a02bacf4a7
shoober420 <shoober420@gmail.com>
2025-01-28 09:48:30 -0500

    Update DisableWindowsSecurityCenter.bat

--- DisableWindowsSecurityCenter.bat
+++ DisableWindowsSecurityCenter.bat
@@ -5,6 +5,8 @@ rem # Recommended to run in SAFE MODE
 rem # Requires SetACL.bat to be run first
 rem # Gives ownership to reg keys
 
+PAUSE
+
 rem # Launch SetACL.bat to take overship of reg keys
 cd "%~dp0"
 ECHO R | powershell.exe ./SetACL.bat
@@ -26,11 +28,11 @@ cd "%USERPROFILE%\Downloads\windows11-scripts"
 ECHO R | powershell.exe ./SetACL.bat
 cd "%USERPROFILE%\Downloads"
 
-PAUSE
-
 net stop SecurityHealthService
 net stop wscsvc
+
 reg add "HKLM\System\CurrentControlSet\Services\SecurityHealthService" /v "Start" /t REG_DWORD /d "4" /f
+
 reg add "HKLM\System\CurrentControlSet\Services\wscsvc" /v "Start" /t REG_DWORD /d "4" /f
 
 rem # Requires SetACL.bat to be run first
