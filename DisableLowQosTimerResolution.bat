f26c494d2ff931983c7bab291393dc68cebaeec6
shoober420 <shoober420@gmail.com>
2025-01-29 06:47:36 -0500

    Update

---
+++ DisableLowQosTimerResolution.bat
@@ -0,0 +1,13 @@
+rem # Disable Kernel LowQosTimerResolution
+
+rem # !!! WARNING !!!
+rem # MAY CAUSE BLUE/GREEN SCREEN OF DEATH ON BOOT
+rem # !!! WARNING !!!
+
+rem # TESTING ONLY
+
+PAUSE
+
+rem reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "DisableLowQosTimerResolution" /t REG_DWORD /d "1" /f
+
+PAUSE
