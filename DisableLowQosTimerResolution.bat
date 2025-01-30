a83a5ab8338265642b9607648ce80849b37f552b
shoober420 <shoober420@gmail.com>
2025-01-30 02:57:27 -0800

    Update DisableLowQosTimerResolution.bat

--- DisableLowQosTimerResolution.bat
+++ DisableLowQosTimerResolution.bat
@@ -2,6 +2,7 @@ rem # Disable Kernel LowQosTimerResolution
 
 rem # !!! WARNING !!!
 rem # MAY CAUSE BLUE/GREEN SCREEN OF DEATH ON BOOT
+rem # ERROR: KERNAL SECIRITY CHECK FEATURE
 rem # !!! WARNING !!!
 
 rem # TESTING ONLY
