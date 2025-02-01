ce70bacb98af0e76975570557a58d2fe08a31b19
shoober420 <shoober420@gmail.com>
2025-02-01 00:59:29 -0800

    Update StartUpBATs.bat

--- StartUpBATs.bat
+++ StartUpBATs.bat
@@ -3,16 +3,16 @@ REM Highly recommended to run after login
 REM Executes SetTimerResolution.exe
 REM Disables services launched after signing in
 
-cd %USERPROFILE%\Downloads\windows11-batch-scripts-main
+cd %USERPROFILE%\Downloads\windows11-scripts-main
 call SetTimerResolution.bat
 
-cd %USERPROFILE%\Downloads\windows11-batch-scripts-main
+cd %USERPROFILE%\Downloads\windows11-scripts-main
 call StartGraphicsPerfSvc.bat
 
-cd %USERPROFILE%\Downloads\windows11-batch-scripts-main
+cd %USERPROFILE%\Downloads\windows11-scripts-main
 call DisableServices.bat
 
-cd %USERPROFILE%\Downloads\windows11-batch-scripts-main
+cd %USERPROFILE%\Downloads\windows11-scripts-main
 call DisableWindowsUpdate.bat
 
 PAUSE
