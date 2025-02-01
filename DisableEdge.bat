103236da0617124e82f0cd27479cf7b930929cbf
shoober420 <shoober420@gmail.com>
2025-02-01 00:43:12 -0800

    Update

---
+++ DisableEdge.bat
@@ -0,0 +1,9 @@
+rem # Disable Microsoft Edge (to restore run "sfc /scannow")
+
+takeown /s %computername% /u %username% /f "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+icacls "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" /grant:r %username%:F
+taskkill /im msedge.exe /f
+rem del "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" /s /f /q
+ren "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" "msedge.exe.bak"
+
+PAUSE
