0bfaa449a04c46de03cf40c4ba1dde3f799d30ea
shoober420 <shoober420@gmail.com>
2025-02-01 03:16:08 -0800

    Update DisableWMI.bat

--- DisableWMI.bat
+++ DisableWMI.bat
@@ -1,6 +1,8 @@
 rem # Disable WMI (Windows Management Instrumentation)
 
 net stop Winmgmt
+sc config Winmgmt start= disabled
+
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Winmgmt" /v "Start" /t REG_DWORD /d "4" /f
 
 PAUSE
