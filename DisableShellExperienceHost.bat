3e2c96973a87ac65b62218dd5457ab29db155858
shoober420 <shoober420@gmail.com>
2025-02-01 00:38:01 -0800

    Update DisableShellExperienceHost.bat

--- DisableShellExperienceHost.bat
+++ DisableShellExperienceHost.bat
@@ -1,7 +1,10 @@
 rem # Disable Shell Experience Host (to restore run "sfc /scannow")
 rem # Disable Shell Infrastructure Host
+
 takeown /s %computername% /u %username% /f "C:\Windows\System32\ShellHost.exe"
 icacls "C:\Windows\System32\ShellHost.exe" /grant:r %username%:F
 taskkill /im ShellHost.exe /f
 rem del "C:\Windows\System32\ShellHost.exe" /s /f /q
 ren "C:\Windows\System32\ShellHost.exe" "ShellHost.exe.bak"
+
+PAUSE
