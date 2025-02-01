294d41f7d9afac164f179429ebe5baf239218d8d
shoober420 <shoober420@gmail.com>
2025-02-01 01:10:39 -0800

    Update DisableThemes.bat

--- DisableThemes.bat
+++ DisableThemes.bat
@@ -1,5 +1,10 @@
-rem Disables Themes service
-REM Breaks User Theme settings when disabled
+rem # Disables Themes service
+
+rem # !!! WARNING !!!
+rem # Breaks User Theme settings when disabled
+rem # !!! WARNING !!!
+
+PAUSE
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Themes" /v "Start" /t REG_DWORD /d "4" /f
 
