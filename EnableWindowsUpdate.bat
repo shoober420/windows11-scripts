0e86b204822e5fade52248d3981b2290de62b0a1
shoober420 <shoober420@gmail.com>
2025-02-02 06:04:49 -0800

    Update EnableWindowsUpdate.bat

--- EnableWindowsUpdate.bat
+++ EnableWindowsUpdate.bat
@@ -34,7 +34,7 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Services\DsmSvc" /v "Start" /t REG_DWORD 
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\DeviceInstall" /v "Start" /t REG_DWORD /d "3" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\PushToInstall" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\LicenseManager" /v "Start" /t REG_DWORD /d "4" /f
-
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\sppsvc" /v "Start" /t REG_DWORD /d "4" /f
 
 net start AppXSvc
 net start StateRepository
@@ -64,6 +64,7 @@ net start DsmSvc
 net start DeviceInstall
 net start PushToInstall
 net start LicenseManager
+net start sppsvc
 
 rem # TLS 1.2 or lower is required for Windows Update to work, TLS 1.3 not supported yet
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client" /v "Enabled" /t REG_DWORD /d "1" /f
