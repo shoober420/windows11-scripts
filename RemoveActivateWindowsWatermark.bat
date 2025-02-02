a50c377c8924da0fa0fb975c66d20f06f0759fde
shoober420 <shoober420@gmail.com>
2025-02-02 07:00:35 -0800

    Update RemoveActivateWindowsWatermark.bat

--- RemoveActivateWindowsWatermark.bat
+++ RemoveActivateWindowsWatermark.bat
@@ -1,5 +1,7 @@
 rem # Remove Activate Windows watermark on the bottom right of the screen
 
+rem # LicenseManager, sppsvc, svsvc may need to be enabled for watermark to disappear
+
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform\Activation" /v "Manual" /t REG_DWORD /d "1" /f
 
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform\Activation" /v "ActivationInterval" /t REG_DWORD /d "0" /f
