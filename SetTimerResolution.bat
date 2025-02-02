706b6cc3ceaadf7527bf942e26d0bbb4478e4d57
shoober420 <shoober420@gmail.com>
2025-02-02 03:30:28 -0800

    Update SetTimerResolution.bat

--- SetTimerResolution.bat
+++ SetTimerResolution.bat
@@ -1,2 +1,7 @@
+rem # Launches SetTimerResolution.exe
+rem # https://github.com/valleyofdoom/TimerResolution
+
 cd "%USERPROFILE%\Downloads"
 start SetTimerResolution.exe --resolution 5000 --no-console
+
+PAUSE
