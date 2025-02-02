b8dadb0840d2d567bbce974ba6d167f5681e5ed7
shoober420 <shoober420@gmail.com>
2025-02-02 18:49:08 -0500

    Update DisableAppX.bat

--- DisableAppX.bat
+++ DisableAppX.bat
@@ -2,7 +2,7 @@ rem # Disable AppXSvc
 
 rem # !!! WARNING !!!
 rem # Disabling AppXSvc breaks File Explorer features, like zip functionality and right click context menu
-rem # !!! CAUTION !!!
+rem # !!! WARNING !!!
 
 PAUSE
 
