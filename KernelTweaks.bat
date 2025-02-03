0b1f27777d5270f51c07bd45b1157604ace222e0
shoober420 <shoober420@gmail.com>
2025-02-03 05:56:59 -0500

    Update KernelTweaks.bat

--- KernelTweaks.bat
+++ KernelTweaks.bat
@@ -60,4 +60,9 @@ reg add "HKLM\SYSTEM\ControlSet001\Control\Session Manager\kernel" /v "Interrupt
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "InterruptSteeringDisabled" /t REG_DWORD /d "1" /f
 reg add "HKLM\SYSTEM" /v "InterruptSteeringDisabled" /t REG_DWORD /d "1" /f
 
+rem # Disable Kernel security features
+rem # Causes stuttering in games
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "DisableExceptionChainValidation" /t REG_DWORD /d "1" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "KernelSEHOPEnabled" /t REG_DWORD /d "0" /f
+
 PAUSE
