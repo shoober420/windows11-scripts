79fc03ee6e7003fd6d70ed29da5a28a4414c38f5
shoober420 <shoober420@gmail.com>
2025-02-04 10:56:22 -0500

    Update TimerTweaks.bat

--- TimerTweaks.bat
+++ TimerTweaks.bat
@@ -1,6 +1,8 @@
 rem # Timer Tweaks
 
+rem # useplatformclock should be missing by default, not present unless enabled prior
 bcdedit /deletevalue useplatformclock
+
 bcdedit /set useplatformtick yes
 bcdedit /set disabledynamictick yes
 bcdedit /set tscsyncpolicy enhanced
