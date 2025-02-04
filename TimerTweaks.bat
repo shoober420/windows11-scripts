1455b51a56d32a3bb2dcc11d7ebecd2c37d1402c
shoober420 <shoober420@gmail.com>
2025-02-04 11:23:50 -0500

    Update TimerTweaks.bat

--- TimerTweaks.bat
+++ TimerTweaks.bat
@@ -8,7 +8,9 @@ rem # Disable HPET in Windows
 rem # useplatformclock not found by default, setting not present unless enabled prior
 bcdedit /deletevalue useplatformclock
 
+rem # Disable synthetic timers
 bcdedit /set useplatformtick yes
+
 bcdedit /set disabledynamictick yes
 bcdedit /set tscsyncpolicy enhanced
 
