fad68716cca61a17d3ea770659332e8a6f73b861
shoober420 <shoober420@gmail.com>
2025-02-04 11:22:07 -0500

    Update TimerTweaks.bat

--- TimerTweaks.bat
+++ TimerTweaks.bat
@@ -2,6 +2,9 @@ rem # Timer Tweaks
 
 rem # https://sites.google.com/view/melodystweaks/misconceptions-about-timers-hpet-tsc-pmt
 
+rem # Leave HPET enabled in BIOS
+
+rem # Disable HPET
 rem # useplatformclock not found by default, setting not present unless enabled prior
 bcdedit /deletevalue useplatformclock
 
