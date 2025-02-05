e566651f9bd40aae93ccda5c87747000e84db355
shoober420 <shoober420@gmail.com>
2025-02-05 03:21:25 -0500

    Update PowerPlanUltra.bat

--- PowerPlanUltra.bat
+++ PowerPlanUltra.bat
@@ -8,7 +8,7 @@ rem # !!! ATTENTION !!!
 rem # New GUID is generated after "powercfg -duplicatescheme" command is executed, you must manually set randomly generated GUID using the "powercfg /setactive" command each time
 rem # !!! ATTENTION !!!
 
-rem # Using Ultimate Performance plan causes CPU Utilization to be at 100% at all times, this is normal
+rem # Using Ultimate Performance plan causes CPU Utilization to be at 100% percent at all times, this is normal
 
 rem # powercfg /setactive *GUID*
 
