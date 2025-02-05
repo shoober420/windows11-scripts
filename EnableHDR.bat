1a45e813622fbfbe3258b4082e6d662816fd452f
shoober420 <shoober420@gmail.com>
2025-02-05 06:02:35 -0500

    Update EnableHDR.bat

--- EnableHDR.bat
+++ EnableHDR.bat
@@ -1,6 +1,14 @@
 rem # Enable HDR
 rem # Decreases FPS when enabled
 
-reg add "HKCU\Software\Microsoft\DirectX\UserGpuPreferences" /v "DirectXUserGlobalSettings" /t REG_SZ /d "AutoHDREnable=1"
-
 PAUSE
+
+$RegistryPath = "HKCU:\Software\Microsoft\DirectX\UserGpuPreferences"
+$Name = "DirectXUserGlobalSettings"
+$Value = "AutoHDREnable=1"
+
+if (!(Test-Path -Path $RegistryPath -Name $Name)) {
+    New-ItemProperty -Path $RegistryPath -Name $Name -Value $Value -PropertyType String -Force
+}
+
+cmd /c 'pause'
