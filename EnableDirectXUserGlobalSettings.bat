a0aebaa69ee8fc37374b0072ce21d6f1b807112a
shoober420 <shoober420@gmail.com>
2025-02-05 05:35:25 -0500

    Update EnableDirectXUserGlobalSettings.bat

--- EnableDirectXUserGlobalSettings.bat
+++ EnableDirectXUserGlobalSettings.bat
@@ -3,6 +3,6 @@ rem # Enable DirectXUserGlobalSettings
 rem # SwapEffectUpgradeEnable = "Optimizations" for windowed and borderless windowed games
 rem # Only enable when playing in windowed mode
 
-reg add "HKCU\Software\Microsoft\DirectX\UserGpuPreferences" /v "DirectXUserGlobalSettings" /t REG_SZ /d "VRROptimizeEnable=0;SwapEffectUpgradeEnable=0" /f 
+reg add "HKCU\Software\Microsoft\DirectX\UserGpuPreferences" /v "DirectXUserGlobalSettings" /t REG_SZ /d "VRROptimizeEnable=0;SwapEffectUpgradeEnable=0"
 
 PAUSE
