8193bfcc49c8d76dc2a8a83f6fe5530d6be514e3
shoober420 <shoober420@gmail.com>
2025-02-06 06:56:37 -0500

    Update PowerPlanUltra.bat

--- PowerPlanUltra.bat
+++ PowerPlanUltra.bat
@@ -1,12 +1,8 @@
 rem # Ultimate Performance Plan
 
-rem # Use "powercfg /list" to show all power config plans
+rem # Use "powercfg /l" to show all power config plans
 
 rem # Use "powercfg -delete" to remove old and unused power config plans
-
-rem # !!! ATTENTION !!!
-rem # New GUID is generated after "powercfg -duplicatescheme" command is executed, you must manually set randomly generated GUID using the "powercfg /setactive" command each time
-rem # !!! ATTENTION !!!
 
 rem # Using Ultimate Performance plan causes CPU Utilization to be at 100% percent at all times, this is normal
 
@@ -14,6 +10,11 @@ rem # powercfg /setactive *GUID*
 
 PAUSE
 
-powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
+rem # https://www.elevenforum.com/members/garlin.5387/
+rem # https://www.elevenforum.com/t/select-ultimate-performance-power-plan-guid-automatically.33089/
+
+powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 00000000-0000-0000-0000-000000001234
+
+powercfg /setactive 00000000-0000-0000-0000-000000001234
 
 PAUSE
