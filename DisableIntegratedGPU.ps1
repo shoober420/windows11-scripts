fa85a99e1f8325ab1f0654daa14a5ecbfbf1df8d
shoober420 <shoober420@gmail.com>
2025-02-07 12:06:25 -0500

    Update DisableIntegratedGPU.ps1

--- DisableIntegratedGPU.ps1
+++ DisableIntegratedGPU.ps1
@@ -1,7 +1,11 @@
 # Disable Integrated GPU
 
-foreach ($dev in (Get-PnpDevice -Class Display | Where-Object {$_.Name -like "*Intel*"})) {
-    &"pnputil" /disable-device $dev.InstanceId;
+rem # foreach ($dev in (Get-PnpDevice -Class Display | Where-Object {$_.Name -like "*Intel*"})) {
+rem #    &"pnputil" /disable-device $dev.InstanceId;
+rem # }
+
+foreach ($dev in (Get-CimInstance win32_VideoController | Where-Object {$_.AdapterDACType -like "Internal"})) {
+    &"pnputil" /disable-device $dev.PNPDeviceID;
 }
 
 cmd /k 'pause'
