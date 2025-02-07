ad9a3f548c01000fbf429a358afe88afcf64cf42
shoober420 <shoober420@gmail.com>
2025-02-07 12:27:32 -0500

    Update DisableIntegratedGPU.ps1

--- DisableIntegratedGPU.ps1
+++ DisableIntegratedGPU.ps1
@@ -4,8 +4,19 @@ rem # foreach ($dev in (Get-PnpDevice -Class Display | Where-Object {$_.Name -li
 rem #    &"pnputil" /disable-device $dev.InstanceId;
 rem # }
 
-foreach ($dev in (Get-CimInstance win32_VideoController | Where-Object {$_.AdapterDACType -like "Internal"})) {
-    &"pnputil" /disable-device $dev.PNPDeviceID;
+rem # foreach ($dev in (Get-CimInstance win32_VideoController | Where-Object {$_.AdapterDACType -like "Internal"})) {
+rem #    &"pnputil" /disable-device $dev.PNPDeviceID;
+rem # }
+
+rem # https://www.elevenforum.com/members/garlin.5387/
+rem # https://www.elevenforum.com/t/pnputil-uninstall-integrated-gpu.33072/#post-559670
+
+$Adapters = @(Get-CimInstance win32_VideoController | Where-Object {$_.AdapterDACType -notlike "Internal"})
+
+if ($Adapters.Count -ge 1) {
+    foreach ($dev in (Get-CimInstance win32_VideoController | Where-Object {$_.AdapterDACType -like "Internal"})) {
+        &"pnputil" /disable-device $dev.PNPDeviceID
+    }
 }
 
 cmd /k 'pause'
