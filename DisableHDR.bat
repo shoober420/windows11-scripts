08e1749aefb154c4c98fca72f18babf9c569bb45
shoober420 <shoober420@gmail.com>
2025-02-07 09:35:51 -0500

    Update DisableHDR.bat

--- DisableHDR.bat
+++ DisableHDR.bat
@@ -2,13 +2,13 @@ rem # Disable HDR
 
 rem # !!! WARNING !!!
 
-rem # MUST DISABLE ONBOARD INTEGRATED GPU IN BIOS AND DEVICE MANAGER TO RUN PROPERLY
-
-rem # You will need to manually set values below if onboard integrated GPU is enabled
+rem # RECOMMENDED TO DISABLE ONBOARD INTEGRATED GPU IN BIOS IF POSSIBLE
 
 rem # Double check to make sure the correct dedicated GPU hardware ID was selected or performance will suffer
 
 rem # !!! WARNING !!!
+
+rem # wmic path win32_videocontroller get PNPDeviceID,Name
 
 PAUSE
 
@@ -18,40 +18,21 @@ rem # Device Manager > Display Adapters > "GPUNAME" > Properties > Details > Pro
 
 rem # HighPerfAdapter="VEN&DEV&SUBSYS" / PCI\VEN_10DE&DEV_2684&SUBSYS_89321043 / HighPerfAdapter=10DE&2684&89321043
 
-rem reg add "HKCU\Software\Microsoft\DirectX\UserGpuPreferences" /v "DirectXUserGlobalSettings" /t REG_SZ /d "HighPerfAdapter=10DE&2684&89321043"
+rem # reg add "HKCU\Software\Microsoft\DirectX\UserGpuPreferences" /v "DirectXUserGlobalSettings" /t REG_SZ /d "HighPerfAdapter=10DE&2684&89321043"
 
 rem # SwapEffectUpgradeEnable = "Optimizations" for windowed and borderless windowed games
 rem # Only enable when playing in windowed mode
 
 PAUSE
 
-rem # Launch DisableIntegratedGPU.ps1
-
-cd "%~dp0"
-ECHO R | powershell.exe ./DisableIntegratedGPU.ps1
-cd "%USERPROFILE%\Downloads"
-
-cd "%USERPROFILE%\Downloads\windows11-scripts-main"
-ECHO R | powershell.exe ./DisableIntegratedGPU.ps1
-cd "%USERPROFILE%\Downloads"
-
-cd "%USERPROFILE%\Downloads\windows11-scripts-main\windows11-scripts-main"
-ECHO R | powershell.exe ./DisableIntegratedGPU.ps1
-cd "%USERPROFILE%\Downloads"
-
-cd "%USERPROFILE%\Downloads\windows11-scripts\windows11-scripts"
-ECHO R | powershell.exe ./DisableIntegratedGPU.ps1
-cd "%USERPROFILE%\Downloads"
-
-cd "%USERPROFILE%\Downloads\windows11-scripts"
-ECHO R | powershell.exe ./DisableIntegratedGPU.ps1
-cd "%USERPROFILE%\Downloads"
-
 rem # garlin: https://www.elevenforum.com/members/garlin.5387/
 rem # https://www.elevenforum.com/t/find-gpu-ven-dev-subsys-values-for-highperfadapter-option-using-wmic-or-other-method.30127/post-521164
+rem # https://www.elevenforum.com/t/pnputil-uninstall-integrated-gpu.33072/
 
-for /f "tokens=1-6 delims=_&" %%a in ('wmic path win32_videocontroller get PNPDeviceID ^| findstr PCI') do (
+for /f "tokens=1-6 delims=_&" %%a in ('wmic path win32_videocontroller where "not AdapterDACType like 'Internal'" get PNPDeviceID ^| find "PCI"') do (
     reg add "HKCU\Software\Microsoft\DirectX\UserGpuPreferences" /v "DirectXUserGlobalSettings" /t REG_SZ /d "HighPerfAdapter=%%b&%%d&%%f;AutoHDREnable=0;VRROptimizeEnable=0;SwapEffectUpgradeEnable=0"
 )
+
+wmic path win32_videocontroller get PNPDeviceID,Name
 
 PAUSE
