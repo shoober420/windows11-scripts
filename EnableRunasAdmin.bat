9ff15e52b3d92aa8a34d67ad37989e51c893969a
shoober420 <shoober420@gmail.com>
2025-02-08 07:18:53 -0500

    Delete EnableRunasAdmin.bat

--- EnableRunasAdmin.bat
+++
@@ -1,9 +0,0 @@
-rem Enables requires services for "Run as administrator"
-
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\Appinfo" /v "Start" /t REG_DWORD /d "3" /f
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\seclogon" /v "Start" /t REG_DWORD /d "2" /f
-
-net start Appinfo
-net start seclogon
-
-PAUSE
