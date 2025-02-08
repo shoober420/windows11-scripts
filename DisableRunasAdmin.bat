90ef534a3a982ecd7a938b126fcf5ef43f05be70
shoober420 <shoober420@gmail.com>
2025-02-08 07:23:03 -0500

    Delete DisableRunasAdmin.bat

--- DisableRunasAdmin.bat
+++
@@ -1,7 +0,0 @@
-rem # Disables Secondary Logon service for "Run as administrator"
-
-net stop seclogon
-
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\seclogon" /v "Start" /t REG_DWORD /d "4" /f
-
-PAUSE
