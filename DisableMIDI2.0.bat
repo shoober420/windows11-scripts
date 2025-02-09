3f235b01fa05efc0b32bcd8fb6563007052079f2
shoober420 <shoober420@gmail.com>
2025-02-09 11:41:20 -0500

    Update

---
+++ DisableMIDI2.0.bat
@@ -0,0 +1,28 @@
+rem # Disable MIDI 2.0 services
+
+rem # !!! WARNING !!!
+
+rem # Breaks Steam when disabled
+
+rem # !!! WARNING !!!
+
+rem # TESTING ONLY
+
+PAUSE
+
+rem # MIDI 2.0 Virtual Devices
+pnputil /disable-device "SWD\MIDISRV\MIDIU_APP_TRANSPORT"
+
+rem # Diagnostics Ping (Internal)
+pnputil /disable-device "SWD\MIDISRV\MIDIU_DIAG_PING"
+
+rem # Diagnostics Loopback B
+pnputil /disable-device "SWD\MIDISRV\MIDIU_DIAG_LOOPBACK_B"
+
+rem # Diagnostics Loopback A
+pnputil /disable-device "SWD\MIDISRV\MIDIU_DIAG_LOOPBACK_A"
+
+rem # MIDI 2.0 Diagnostics Devices
+pnputil /disable-device "SWD\MIDISRV\MIDIU_DIAG_TRANSPORT"
+
+PAUSE
