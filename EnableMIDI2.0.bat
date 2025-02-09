c960d58cc74232c0729e937fbbfa48be312437b5
shoober420 <shoober420@gmail.com>
2025-02-09 11:42:27 -0500

    Update

---
+++ EnableMIDI2.0.bat
@@ -0,0 +1,28 @@
+rem # Enable MIDI 2.0 services
+
+rem # !!! WARNING !!!
+
+rem # Breaks Steam when disabled
+
+rem # !!! WARNING !!!
+
+rem # TESTING ONLY
+
+PAUSE
+
+rem # MIDI 2.0 Virtual Devices
+pnputil /enable-device "SWD\MIDISRV\MIDIU_APP_TRANSPORT"
+
+rem # Diagnostics Ping (Internal)
+pnputil /enable-device "SWD\MIDISRV\MIDIU_DIAG_PING"
+
+rem # Diagnostics Loopback B
+pnputil /enable-device "SWD\MIDISRV\MIDIU_DIAG_LOOPBACK_B"
+
+rem # Diagnostics Loopback A
+pnputil /enable-device "SWD\MIDISRV\MIDIU_DIAG_LOOPBACK_A"
+
+rem # MIDI 2.0 Diagnostics Devices
+pnputil /enable-device "SWD\MIDISRV\MIDIU_DIAG_TRANSPORT"
+
+PAUSE
