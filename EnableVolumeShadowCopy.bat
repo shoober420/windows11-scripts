bf4d8c9b7d0d5ba30e28ebdff23d12f348eedc11
shoober420 <shoober420@gmail.com>
2025-02-09 15:47:53 -0500

    Update

---
+++ EnableVolumeShadowCopy.bat
@@ -0,0 +1,16 @@
+rem # Enable Volume Shadow Copy
+
+rem # !!! WARNING !!!
+
+rem # Glitches system when disabled
+
+rem # !!! WARNING !!!
+
+rem # TESTING ONLY
+
+PAUSE
+
+rem # Generic volume shadow copy
+pnputil /enable-device "STORAGE\VolumeSnapshot\HarddiskVolumeSnapshot1"
+
+PAUSE
