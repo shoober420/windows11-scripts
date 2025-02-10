d8328494af210356a7703fd471d91d8832e88794
shoober420 <shoober420@gmail.com>
2025-02-10 08:50:29 -0500

    Update

---
+++ EnableCommonLogFileSystem.bat
@@ -0,0 +1,15 @@
+rem # Enable Common Log File System (CLFS)
+
+rem # !!! WARNING !!!
+
+rem # Breaks PowerShell and DISM when disabled
+
+rem # !!! WARNING !!!
+
+rem # TESTING ONLY
+
+PAUSE
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\CLFS" /v "Start" /t REG_DWORD /d "2" /f
+
+PAUSE
