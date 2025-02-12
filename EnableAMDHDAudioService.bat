f0583dcb7dfaf86a25ca8c87fcd44774a5681d8b
shoober420 <shoober420@gmail.com>
2025-02-11 22:51:40 -0500

    Update

---
+++ EnableAMDHDAudioService.bat
@@ -0,0 +1,13 @@
+rem # Enable AMD HD Audio Service
+
+rem # !!! WARNING !!!
+
+rem # Breaks sound coming from GPU HDMI to monitor when disabled
+
+rem # !!! WARNING !!!
+
+PAUSE
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\AtiHDAudioService" /v "Start" /t REG_DWORD /d "2" /f
+
+PAUSE
