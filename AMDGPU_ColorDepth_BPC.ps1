a38de75f3e6d0a2c33389a76ebdc633445ab31d6
shoober420 <shoober420@gmail.com>
2025-02-27 00:27:14 -0500

    Update AMDGPU_ColorDepth_BPC.ps1

--- AMDGPU_ColorDepth_BPC.ps1
+++ AMDGPU_ColorDepth_BPC.ps1
@@ -1,6 +1,8 @@
 # AMD Radeon Display Color Depth
 # BPC (bits per component, bits per channel, bits per color)
 
+# Multi-Monitor setups not supported
+
 # https://www.elevenforum.com/members/garlin.5387/
 # https://www.elevenforum.com/t/query-monitor-edid.33440/
 
