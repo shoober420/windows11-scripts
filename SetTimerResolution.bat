2868f9c14da2b03d9270c2d7a76408bfe8a42697
shoober420 <shoober420@gmail.com>
2025-03-06 06:47:23 -0500

    Update SetTimerResolution.bat

--- SetTimerResolution.bat
+++ SetTimerResolution.bat
@@ -3,6 +3,9 @@ rem # https://github.com/valleyofdoom/TimerResolution
 
 rem # Requires VCRedist.bat packages
 
+rem # --resolution 5000 = 0.5ms
+rem # --resolution 10000 = 1.0ms
+
 PAUSE
 
 cd "%USERPROFILE%\Downloads"
