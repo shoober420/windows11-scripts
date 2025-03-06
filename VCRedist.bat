45a36c8aa3a7d668e7ab612c06cad3cfe5aafeeb
shoober420 <shoober420@gmail.com>
2025-03-06 05:29:22 -0500

    Update VCRedist.bat

--- VCRedist.bat
+++ VCRedist.bat
@@ -1,5 +1,9 @@
 rem # Installs all VCRedist packages
 
+rem # Requires EnableStore.bat script to run
+
+PAUSE
+
 cd C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_1.20.1081.0_x64__8wekyb3d8bbwe
 winget --info
 
