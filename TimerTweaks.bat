d38e8b7840ce326f43399003db4b90d59ba19c55
shoober420 <shoober420@gmail.com>
2025-03-08 09:59:28 -0500

    Update TimerTweaks.bat

--- TimerTweaks.bat
+++ TimerTweaks.bat
@@ -6,6 +6,9 @@ rem # https://www.xbitlabs.com/hpet-settings/
 
 rem # Leave HPET enabled in BIOS
 
+rem # Timer Resolution Tweak
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "GlobalTimerResolutionRequests" /t REG_DWORD /d "1" /f
+
 rem # Disable HPET in Windows
 rem # useplatformclock not found by default, setting not present unless enabled prior
 bcdedit /deletevalue useplatformclock
