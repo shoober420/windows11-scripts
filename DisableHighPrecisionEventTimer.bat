e819ba35b272f73bb1b62f6dde59c9c9be6db5ad
shoober420 <shoober420@gmail.com>
2025-03-07 23:44:04 -0500

    Update DisableHighPrecisionEventTimer.bat

--- DisableHighPrecisionEventTimer.bat
+++ DisableHighPrecisionEventTimer.bat
@@ -2,4 +2,7 @@ rem # Disable High Precision Event Timer
 
 pnputil /disable-device "ACPI\PNP0103\0"
 
+rem # Disable HPET (High Precision Event Timer)
+reg add "HKLM\SYSTEM\CurrentControlSet\Enum\HARDWARE\DEVICEMAP\ACPI\HPET" /v "Enable" /t REG_DWORD /d "0" /f
+
 PAUSE
