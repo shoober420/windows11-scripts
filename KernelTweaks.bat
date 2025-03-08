764075715fd1c8d378ec2f5c319c75c982dac94a
shoober420 <shoober420@gmail.com>
2025-03-08 12:27:38 -0500

    Update KernelTweaks.bat

--- KernelTweaks.bat
+++ KernelTweaks.bat
@@ -2,6 +2,8 @@ rem # Kernel Tweaks
 
 rem # https://forums.blurbusters.com/viewtopic.php?t=13323
 
+rem # https://www.tenforums.com/performance-maintenance/146774-cacheawarescheduling-what-how-does-windows-benefit.html
+
 rem # Disable ThreadDpcEnable for lower latency
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "ThreadDpcEnable" /t REG_DWORD /d "0" /f
 
