1016e6483ced37a0d41a77db704ccbcc44353f97
shoober420 <shoober420@gmail.com>
2025-03-09 16:35:00 -0400

    Create EnableCPUMitigations.bat

---
+++ EnableCPUMitigations.bat
@@ -0,0 +1,7 @@
+rem # Enable CPU Mitigations
+
+rem 0/3 - Enable / 3/3 - Disable mitigations for CVE-2017-5715 (Spectre Variant 2) and CVE-2017-5754 (Meltdown)
+reg add "HKLM\System\CurrentControlSet\Control\Session Manager\Memory Management" /v "FeatureSettingsOverride" /t REG_DWORD /d "0" /f
+reg add "HKLM\System\CurrentControlSet\Control\Session Manager\Memory Management" /v "FeatureSettingsOverrideMask" /t REG_DWORD /d "0" /f
+
+PAUSE
