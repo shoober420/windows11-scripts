60d6ef50fcff2404ae1f6a52cea89ffe33ce907f
shoober420 <shoober420@gmail.com>
2025-03-09 20:22:31 -0400

    Update Windows11Tweaks.bat

--- Windows11Tweaks.bat
+++ Windows11Tweaks.bat
@@ -1171,29 +1171,6 @@ reg add "HKLM\SOFTWARE\Microsoft\WindowsRuntime\ActivatableClassId\Windows.Gamin
 rem # Disable Widgets
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Dsh" /v "IsPrelaunchEnabled" /t REG_DWORD /d "0" /f
 
-rem # Remove Microsoft Edge
-takeown /s %computername% /u %username% /f "C:\Program Files (x86)\Microsoft\Edge"
-takeown /s %computername% /u %username% /f "C:\Program Files (x86)\Microsoft\EdgeCore"
-takeown /s %computername% /u %username% /f "C:\Program Files (x86)\Microsoft\EdgeUpdate"
-takeown /s %computername% /u %username% /f "C:\Program Files (x86)\Microsoft\EdgeWebView"
-icacls "C:\Program Files (x86)\Microsoft\Edge" /grant:r %username%:F
-icacls "C:\Program Files (x86)\Microsoft\EdgeCore" /grant:r %username%:F
-icacls "C:\Program Files (x86)\Microsoft\EdgeUpdate" /grant:r %username%:F
-icacls "C:\Program Files (x86)\Microsoft\EdgeWebView" /grant:r %username%:F
-taskkill /im MicrosoftEdgeUpdate.exe /f
-taskkill /im EdgeUpdate.exe /f
-ren "C:\Program Files (x86)\Microsoft\Edge" "Edge.bak"
-ren "C:\Program Files (x86)\Microsoft\EdgeCore" "EdgeCore.bak"
-ren "C:\Program Files (x86)\Microsoft\EdgeUpdate" "EdgeUpdate.bak"
-ren "C:\Program Files (x86)\Microsoft\EdgeWebView" "EdgeWebView.bak"
-
-rem # Disable Microsoft Edge
-takeown /s %computername% /u %username% /f "C:\Windows\SystemApps\Microsoft.MicrosoftEdgeDevToolsClient_8wekyb3d8bbwe"
-icacls "C:\Windows\SystemApps\Microsoft.MicrosoftEdgeDevToolsClient_8wekyb3d8bbwe" /grant:r %username%:F
-taskkill /im msedge.exe /f
-rem del "C:\Windows\SystemApps\Microsoft.MicrosoftEdgeDevToolsClient_8wekyb3d8bbwe" /s /f /q
-ren "C:\Windows\SystemApps\Microsoft.MicrosoftEdgeDevToolsClient_8wekyb3d8bbwe" "Microsoft.MicrosoftEdgeDevToolsClient_8wekyb3d8bbwe.bak"
-
 rem # Fastest cursor update rate
 reg add "HKLM\SOFTWARE\Microsoft\Input\Settings\ControllerProcessor\CursorSpeed" /v "CursorUpdateInterval" /t REG_DWORD /d "1" /f
 
