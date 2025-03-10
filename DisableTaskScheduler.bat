2ba818519e588c615056a1e4e6af460a03aafaa6
shoober420 <shoober420@gmail.com>
2025-03-09 23:13:31 -0400

    Update DisableTaskScheduler.bat

--- DisableTaskScheduler.bat
+++ DisableTaskScheduler.bat
@@ -1,9 +1,7 @@
-REM Disable Task Scheduler
+rem # Disable Task Scheduler
 
-net stop TimeBrokerSvc
 net stop Schedule
 
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\TimeBrokerSvc" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Schedule" /v "Start" /t REG_DWORD /d "4" /f
 
 PAUSE
