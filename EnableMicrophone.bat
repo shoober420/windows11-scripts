5e3443baa6864f91bacdb0fa28afbccad593398f
shoober420 <shoober420@gmail.com>
2025-03-09 23:05:23 -0400

    Update EnableMicrophone.bat

--- EnableMicrophone.bat
+++ EnableMicrophone.bat
@@ -1,17 +1,19 @@
-REM Enable Microphone
+rem # Enable Microphone
 
 rem =================================== Windows Settings ===================================
 rem ---------------------------------- Privacy & security ----------------------------------
 rem ...................................... Microphone ......................................
 
-REM Capability Access Manager Service (camsvc) is required for microphone to work
+rem # Capability Access Manager Service (camsvc) is required for microphone to work
+
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\camsvc" /v "Start" /t REG_DWORD /d "2" /f
+
 net start camsvc
 
-rem Allow/Deny - Microphone access
+rem # Allow/Deny - Microphone access
 reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\microphone" /v "Value" /t REG_SZ /d "Allow" /f
 
-rem Allow/Deny - Let apps access your microphone
+rem # Allow/Deny - Let apps access your microphone
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\microphone" /v "Value" /t REG_SZ /d "Allow" /f
 
 PAUSE
