5f3ba1e9f7f380989177d447d5b722fdd24dda01
shoober420 <shoober420@gmail.com>
2025-03-15 23:51:29 -0400

    Create DisableThreadDpcEnable.bat

---
+++ DisableThreadDpcEnable.bat
@@ -0,0 +1,12 @@
+rem # Disable ThreadDpcEnable
+rem # Deferred Procedure Call (DPC)
+rem # Allows high-priority tasks to defer required but lower-priority tasks for later execution
+
+rem # https://forums.blurbusters.com/viewtopic.php?t=13323
+
+rem # https://www.tenforums.com/performance-maintenance/146774-cacheawarescheduling-what-how-does-windows-benefit.html
+
+rem # Disable ThreadDpcEnable for lower latency
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "ThreadDpcEnable" /t REG_DWORD /d "0" /f
+
+PAUSE
