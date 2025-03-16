f0e84fd69eff0dcbc09982b99272f0a1b7358775
shoober420 <shoober420@gmail.com>
2025-03-16 00:25:15 -0400

    Update EnableSerializeTimerExpiration.bat

--- EnableSerializeTimerExpiration.bat
+++ EnableSerializeTimerExpiration.bat
@@ -3,8 +3,8 @@ rem # Enable Serialize Timer Expiration kernel default
 rem # Enable Serialization Standby
 rem # Allows the kernel to make the decision based on non-Modern Standby systems
 
-reg delete "HKLM\SYSTEM\ControlSet001\Control\Session Manager\kernel" /v "SerializeTimerExpiration"
-reg delete "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "SerializeTimerExpiration"
+ECHO Y | reg delete "HKLM\SYSTEM\ControlSet001\Control\Session Manager\kernel" /v "SerializeTimerExpiration"
+ECHO Y | reg delete "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "SerializeTimerExpiration"
 
 rem reg add "HKLM\SYSTEM\ControlSet001\Control\Session Manager\kernel" /v "SerializeTimerExpiration" /t REG_DWORD /d "0" /f
 rem reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "SerializeTimerExpiration" /t REG_DWORD /d "0" /f
