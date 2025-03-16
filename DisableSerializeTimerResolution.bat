2030638a70e695d3be4e6ae6c53d4eca8ac534e4
shoober420 <shoober420@gmail.com>
2025-03-15 21:46:00 -0400

    Update DisableSerializeTimerResolution.bat

--- DisableSerializeTimerResolution.bat
+++ DisableSerializeTimerResolution.bat
@@ -1,5 +1,6 @@
 rem # Disable Serialize Timer Expiration kernel tweak
 
+rem # Enable Serialization
 reg add "HKLM\SYSTEM\ControlSet001\Control\Session Manager\kernel" /v "SerializeTimerExpiration" /t REG_DWORD /d "1" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "SerializeTimerExpiration" /t REG_DWORD /d "1" /f
 
