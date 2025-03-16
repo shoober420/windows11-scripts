168a63e6f656e3cedd67535f8fedf46570052ca0
shoober420 <shoober420@gmail.com>
2025-03-15 21:50:18 -0400

    Update DisableSerializeTimerResolution.bat

--- DisableSerializeTimerResolution.bat
+++ DisableSerializeTimerResolution.bat
@@ -4,10 +4,13 @@ rem # Enable Serialization
 reg add "HKLM\SYSTEM\ControlSet001\Control\Session Manager\kernel" /v "SerializeTimerExpiration" /t REG_DWORD /d "1" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "SerializeTimerExpiration" /t REG_DWORD /d "1" /f
 
+
+
 rem # Disable Serialization
 rem # DISTRIBUTES TIMERS AMONG PROCESSORS
 rem # LOWERS FPS
-reg add "HKLM\SYSTEM\ControlSet001\Control\Session Manager\kernel" /v "SerializeTimerExpiration" /t REG_DWORD /d "2" /f
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "SerializeTimerExpiration" /t REG_DWORD /d "2" /f
+
+rem reg add "HKLM\SYSTEM\ControlSet001\Control\Session Manager\kernel" /v "SerializeTimerExpiration" /t REG_DWORD /d "2" /f
+rem reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "SerializeTimerExpiration" /t REG_DWORD /d "2" /f
 
 PAUSE
