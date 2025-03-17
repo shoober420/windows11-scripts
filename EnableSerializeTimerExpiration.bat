dd23761b4e8249f3659be571c64d9c38271226f3
shoober420 <shoober420@gmail.com>
2025-03-17 01:59:38 -0400

    Update EnableSerializeTimerExpiration.bat

--- EnableSerializeTimerExpiration.bat
+++ EnableSerializeTimerExpiration.bat
@@ -1,5 +1,7 @@
 rem # Enable Serialize Timer Expiration kernel tweak
 
+rem # https://www.youtube.com/watch?v=wil-09_5H0M
+
 rem # Enable Serialization
 reg add "HKLM\SYSTEM\ControlSet001\Control\Session Manager\kernel" /v "SerializeTimerExpiration" /t REG_DWORD /d "1" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "SerializeTimerExpiration" /t REG_DWORD /d "1" /f
