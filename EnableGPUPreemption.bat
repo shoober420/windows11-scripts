4e149679686a584e0dddc7885ce84976f90f932b
shoober420 <shoober420@gmail.com>
2025-03-18 01:01:37 -0400

    Update EnableGPUPreemption.bat

--- EnableGPUPreemption.bat
+++ EnableGPUPreemption.bat
@@ -1,5 +1,7 @@
 rem # Enable preemption requests from the GPU scheduler (default)
 
+rem # https://www.youtube.com/watch?v=B-3JgtuurhQ
+
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers\Scheduler" /v "EnablePreemption" /t REG_DWORD /d "1" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "EnablePreemption" /t REG_DWORD /d 1 /f
 
