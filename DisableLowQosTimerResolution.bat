6570b79cb9cef320ab0f9374bf32008a45bb7ff6
shoober420 <shoober420@gmail.com>
2025-03-18 17:24:58 -0400

    Update DisableLowQosTimerResolution.bat

--- DisableLowQosTimerResolution.bat
+++ DisableLowQosTimerResolution.bat
@@ -2,10 +2,14 @@ rem # Disable Kernel LowQosTimerResolution
 
 rem # !!! WARNING !!!
 
+rem # LOWERS FPS
+
 rem # MAY CAUSE BLUE/GREEN SCREEN OF DEATH ON BOOT
 rem # ERROR: KERNAL SECURITY CHECK FAILURE
 
 rem # !!! WARNING !!!
+
+rem # TESTING ONLY
 
 PAUSE
 
