1ed419034e41ffe07ce6c7bf225c5d457ddedf7b
shoober420 <shoober420@gmail.com>
2025-03-18 00:49:22 -0400

    Update EnableGPUPreemption.bat

--- EnableGPUPreemption.bat
+++ EnableGPUPreemption.bat
@@ -3,4 +3,10 @@ rem # Enable preemption requests from the GPU scheduler (default)
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers\Scheduler" /v "EnablePreemption" /t REG_DWORD /d "1" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "EnablePreemption" /t REG_DWORD /d 1 /f
 
+for /f %%i in ('wmic path Win32_VideoController get PNPDeviceID^| findstr /L "PCI\VEN_"') do (
+        for /f "tokens=3" %%a in ('reg query "HKLM\SYSTEM\ControlSet001\Enum\%%i" /v "Driver"') do (
+                for /f %%i in ('echo %%a ^| findstr "{"') do (
+
+
+
 PAUSE
