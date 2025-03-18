cec0aff6f7c33cf49d76c9aa2a70164ea9102f87
shoober420 <shoober420@gmail.com>
2025-03-18 00:52:02 -0400

    Update EnableGPUPreemption.bat

--- EnableGPUPreemption.bat
+++ EnableGPUPreemption.bat
@@ -7,6 +7,10 @@ for /f %%i in ('wmic path Win32_VideoController get PNPDeviceID^| findstr /L "PC
         for /f "tokens=3" %%a in ('reg query "HKLM\SYSTEM\ControlSet001\Enum\%%i" /v "Driver"') do (
                 for /f %%i in ('echo %%a ^| findstr "{"') do (
 
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i" /v "EnablePreemption" /t REG_DWORD /d "1" /f
 
+)
+)
+)
 
 PAUSE
