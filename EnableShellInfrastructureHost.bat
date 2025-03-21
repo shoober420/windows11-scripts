5d0168daef481dd9491aaa5f032fae3ae2a5b0e5
shoober420 <shoober420@gmail.com>
2025-03-20 20:10:47 -0400

    Update EnableShellInfrastructureHost.bat

--- EnableShellInfrastructureHost.bat
+++ EnableShellInfrastructureHost.bat
@@ -2,7 +2,7 @@ rem # Enable Shell Infrastructure Host
 
 takeown /s %computername% /u %username% /f "C:\Windows\System32\sihost.exe"
 icacls "C:\Windows\System32\sihost.exe" /grant:r %username%:F
-
+taskkill /im sihost.exe /f
 rem del "C:\Windows\System32\sihost.exe" /s /f /q
 ren "C:\Windows\System32\sihost.exe.bak" "sihost.exe"
 
