41c58b1689ab7b27a4d7cae635fd3dee3c8952f3
shoober420 <shoober420@gmail.com>
2025-03-21 09:56:07 -0400

    Update DWM_ExclusiveModeFramerateAveragingPeriodMs.bat

--- DWM_ExclusiveModeFramerateAveragingPeriodMs.bat
+++ DWM_ExclusiveModeFramerateAveragingPeriodMs.bat
@@ -1,5 +1,18 @@
-rem # Too low of a value can cause game freezing (default 1000)
+rem # DWM: ExclusiveModeFramerateAveragingPeriodMs tweak
+
+rem # Desktop Window Manager
+
+rem # !!! WARNING !!!
+
+rem # May cause game freezing if value too low
+
 rem # Can be set lower when using SetTimerResolution -5000 (0.5ms)
-reg add "HKLM\SOFTWARE\Microsoft\Windows\Dwm\ExtendedComposition" /v "ExclusiveModeFramerateAveragingPeriodMs" /t REG_DWORD /d "100" /f
+
+rem # !!! WARNING !!!
+
+PAUSE
+
+rem # Default 1000
+reg add "HKLM\SOFTWARE\Microsoft\Windows\Dwm\ExtendedComposition" /v "ExclusiveModeFramerateAveragingPeriodMs" /t REG_DWORD /d "1" /f
 
 PAUSE
