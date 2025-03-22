b1c5c38102ca12b366efc34cb197276442d0a9c5
shoober420 <shoober420@gmail.com>
2025-03-22 10:13:10 -0400

    Update WallpaperWindows98Teal.bat

--- WallpaperWindows98Teal.bat
+++ WallpaperWindows98Teal.bat
@@ -6,6 +6,8 @@ rem # DELETES CURRENT WALLPAPER FOR SOLID COLOR BACKGROUND
 
 rem # !!! WARNING !!!
 
+rem # Restart explorer, re-sign in, or reboot required
+
 PAUSE
 
 rem # BackgroundType: 0 = Picture / 1 = Solid color / 2 = Slideshow
@@ -13,10 +15,15 @@ reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Wallpapers" /v 
 
 reg add "HKCU\Control Panel\Desktop" /v "Wallpaper" /t REG_SZ /d "" /f
 
-rem # Teal Green
+rem # Windows 95/98 Teal Green
 rem # HEX: #008080
 rem # RGB: (0 128 128)
 reg add "HKCU\Control Panel\Colors" /v "Background" /t REG_SZ /d "0 128 128" /f
+
+rem # Windows 2000/XP Light Blue
+rem # HEX: #3A6EA5
+rem # RGB: (58 110 165)
+rem reg add "HKCU\Control Panel\Colors" /v "Background" /t REG_SZ /d "58 110 165" /f
 
 rem # Black (default)
 rem # HEX: #000000
