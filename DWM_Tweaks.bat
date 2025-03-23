cfdc23e3e152018fd347e84dc247f709b595430e
shoober420 <shoober420@gmail.com>
2025-03-22 20:05:43 -0400

    Update DWM_Tweaks.bat

--- DWM_Tweaks.bat
+++ DWM_Tweaks.bat
@@ -70,6 +70,7 @@ rem # https://sites.google.com/site/tweakradje/windows/windows-tweaking
 rem # Too low of a value can cause game freezing
 rem # Can be set lower when using SetTimerResolution -5000 (0.5ms)
 rem # Default 0x3e8 = 1000
+rem # 0x1f4 = 500
 rem # 0xfa = 250
 rem # 0x64 = 100
 reg add "HKLM\SOFTWARE\Microsoft\Windows\Dwm\ExtendedComposition" /v "ExclusiveModeFramerateAveragingPeriodMs" /t REG_DWORD /d "0x3e8" /f
