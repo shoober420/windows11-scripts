6281bac7d791df16e64521e62ae3529de5c8afe7
shoober420 <shoober420@gmail.com>
2025-03-26 01:07:23 -0400

    Update DWM_Tweaks.bat

--- DWM_Tweaks.bat
+++ DWM_Tweaks.bat
@@ -83,7 +83,7 @@ rem # https://sites.google.com/site/tweakradje/windows/windows-tweaking
 
 rem # Too low of a value can cause game freezing
 rem # Can be set lower when using SetTimerResolution -5000 (0.5ms)
-rem # Default (Hex / Decimal) 0x3e8 = 1000
+rem # Default (Hex / Decimal): 0x3e8 = 1000
 rem # 0x2ee = 750
 rem # 0x1f4 = 500
 rem # 0xfa = 250
