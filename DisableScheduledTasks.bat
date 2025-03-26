3362e00a0791157eed3d3364e04f6890da904a7d
shoober420 <shoober420@gmail.com>
2025-03-26 07:14:50 -0400

    Update DisableScheduledTasks.bat

--- DisableScheduledTasks.bat
+++ DisableScheduledTasks.bat
@@ -2,4 +2,8 @@ rem # Delete Task Scheduler tasks
 
 schtasks /delete /tn * /f
 
+schtasks /Change /TN "Microsoft\Windows\Wininet\CacheTask" /Disable
+
+schtasks /Change /TN "Microsoft\Windows\WlanSvc\CDSSync" /Disable
+
 PAUSE
