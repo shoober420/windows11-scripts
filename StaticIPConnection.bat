5a94b47bb98ee02d9dff2d829a2b6f319a23e5ab
shoober420 <shoober420@gmail.com>
2025-03-26 05:16:35 -0400

    Update StaticIPConnection.bat

--- StaticIPConnection.bat
+++ StaticIPConnection.bat
@@ -1,5 +1,11 @@
 rem # Enable Static IP Connection
 
-netsh interface ip set address name="Ethernet" static 192.168.50.73 255.255.255.0 192.168.50.1
+rem # Make sure the first 3 parts of the IP address and gateway match
+
+ipconfig
+
+netsh interface ip set address name="Ethernet" static 192.168.1.37 255.255.255.0 192.168.1.1
+
+rem # netsh interface ip set address name="Ethernet" static 192.168.50.37 255.255.255.0 192.168.50.1
 
 PAUSE
