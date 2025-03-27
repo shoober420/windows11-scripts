25e2aa7f8f84ee51ae4685587788d7f54c0e4968
shoober420 <shoober420@gmail.com>
2025-03-27 02:58:40 -0400

    Update Windows11Tweaks.bat

--- Windows11Tweaks.bat
+++ Windows11Tweaks.bat
@@ -2102,7 +2102,7 @@ reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v "DisableDia
 rem ================================ Windows Error Reporting ===============================
 rem https://docs.microsoft.com/en-us/windows/win32/wer/wer-settings
 rem # Disable Microsoft Support Diagnostic Tool MSDT
-reg add "HKLM\Software\Policies\Microsoft\Windows\ScriptedDiagnosticsProvider\Policy" /v "DisableQueryRemoteServer" /t REG_DWORD /d "0" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows\ScriptedDiagnosticsProvider\Policy" /v "DisableQueryRemoteServer" /t REG_DWORD /d "1" /f
 reg add "HKLM\Software\Policies\Microsoft\Windows\ScriptedDiagnosticsProvider\Policy" /v "EnableQueryRemoteServer" /t REG_DWORD /d "0" /f
 rem # Disable System Debugger (Dr. Watson)
 reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\AeDebug" /v "Auto" /t REG_SZ /d "0" /f
