f3afe53a8beabd0bf18c08f8db49297e10643427
shoober420 <shoober420@gmail.com>
2025-03-29 09:38:17 -0400

    Update Theme.bat

--- Theme.bat
+++ Theme.bat
@@ -16,8 +16,8 @@ reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" /v "empty" /t REG_SZ /d "%SystemRoot%\System32\SHELL32.dll,101" /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" /v "full" /t REG_SZ /d "%SystemRoot%\System32\SHELL32.dll,102" /f
 
-rem # Windows 98 Desktop Control Panel Icon
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{21EC2020-3AEA-1069-A2DD-08002B30309D}\DefaultIcon" /ve /d "%SystemRoot%\System32\SHELL32.dll,35" /f
+rem # Windows 95 similar My Computer icon
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{20D04FE0-3AEA-1069-A2D8-08002B30309D}\DefaultIcon" /ve /d "%SystemRoot%\System32\pifmgr.dll,11" /f
 
 rem # Enable Dark mode
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "AppsUseLightTheme" /t REG_DWORD /d "0" /f
