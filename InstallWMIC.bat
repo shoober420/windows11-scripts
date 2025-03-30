a2cc81894606e0fe914d02ef6fed98dceb9b0005
shoober420 <shoober420@gmail.com>
2025-03-30 01:58:55 -0400

    Create InstallWMIC.bat

---
+++ InstallWMIC.bat
@@ -0,0 +1,12 @@
+rem # Install WMIC
+rem # Windows Management Instrumentation Command line
+
+rem # https://www.elevenforum.com/members/garlin.5387/
+rem # https://www.elevenforum.com/t/gpu-tweaks-batch-script.30134/post-521530
+if not exist C:\Windows\System32\wbem\WMIC.exe (
+    echo Installing WMIC...
+    DISM /Online /Add-Capability /CapabilityName:WMIC~~~~
+    echo Done.
+)
+
+PAUSE
