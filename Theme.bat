10666327a792c15611592210f4d821b6d8afcf57
shoober420 <shoober420@gmail.com>
2025-03-30 00:12:19 -0400

    Update Theme.bat

--- Theme.bat
+++ Theme.bat
@@ -36,11 +36,11 @@ reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\DesktopSpotlight\Setting
 
 rem # Accent color
 rem # Default (Windows 11 Blue / RGB: 0 120 215) = 0xff0078d7
-rem # Win2000/WinXP Light Blue (RGB: 58 110 165) = 0xffaf7745
+rem # Win2000/WinXP Light Blue (RGB: 58 110 165) = 0xff446ea5
 rem # Navy (Windows 95 Titlebar Blue / RGB: 0 0 95) = 0xff5f0000
 rem # Teal (RGB: 0 128 128) = 0xff808000
 rem # Seafoam (RGB: 0 183 195) = 0xffc3b700
-reg add "HKCU\Software\Microsoft\Windows\DWM" /v "AccentColor" /t REG_DWORD /d "0xffaf7745" /f
+reg add "HKCU\Software\Microsoft\Windows\DWM" /v "AccentColor" /t REG_DWORD /d "0xff446ea5" /f
 
 rem # Transparency Effects
 rem # 1 = Enabled \ 0 = Disabled
