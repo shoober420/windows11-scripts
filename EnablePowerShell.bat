a6d2928bc77f09024b8d6546244b099d0926b2cc
shoober420 <shoober420@gmail.com>
2025-03-31 09:14:52 -0400

    Update EnablePowerShell.bat

--- EnablePowerShell.bat
+++ EnablePowerShell.bat
@@ -19,11 +19,14 @@ ECHO Yes | reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\E
 powershell.exe Enable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2Root
 
 rem # Disable PowerShell script execution
-rem https://teamt5.org/en/posts/a-deep-dive-into-powershell-s-constrained-language-mode
-rem https://www.thewindowsclub.com/how-to-disable-powershell-windows-10
+rem # https://teamt5.org/en/posts/a-deep-dive-into-powershell-s-constrained-language-mode
+rem # https://www.thewindowsclub.com/how-to-disable-powershell-windows-10
+
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell" /v "ExecutionPolicy" /t REG_SZ /d "Unrestricted" /f
 reg add "HKLM\Software\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v "ExecutionPolicy" /t REG_SZ /d "Unrestricted" /f
 reg add "HKLM\Software\Microsoft\PowerShell\1\ShellIds\ScriptedDiagnostics" /v "ExecutionPolicy" /t REG_SZ /d "Unrestricted" /f
 reg add "HKLM\Software\WOW6432Node\Microsoft\PowerShell\1\ShellIds\ScriptedDiagnostics" /v "ExecutionPolicy" /t REG_SZ /d "Unrestricted" /f
+
 reg add "HKLM\Software\Policies\Microsoft\PowerShellCore" /v "EnableScripts" /t REG_DWORD /d "1" /f
 reg add "HKLM\Software\Policies\Microsoft\Windows\PowerShell" /v "EnableScripts" /t REG_DWORD /d "1" /f
 reg add "HKLM\Software\WOW6432Node\Policies\Microsoft\PowerShellCore" /v "EnableScripts" /t REG_DWORD /d "1" /f
