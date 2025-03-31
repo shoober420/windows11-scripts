a4879b0e9fbbd1bbdf37848ee625757b0f5390be
shoober420 <shoober420@gmail.com>
2025-03-31 08:26:51 -0400

    Update EnableTouchPad.bat

--- EnableTouchPad.bat
+++ EnableTouchPad.bat
@@ -1,4 +1,7 @@
 rem # Enable TouchPad
+
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\PrecisionTouchPad\Status" /v "Enabled" /t REG_DWORD /d "1" /f
+
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PrecisionTouchPad" /v "AAPDisabled" /t REG_DWORD /d "0" /f
 
 PAUSE
