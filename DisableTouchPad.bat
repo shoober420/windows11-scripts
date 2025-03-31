8996bd1e41c27f87aee352223bcabba4331eb34e
shoober420 <shoober420@gmail.com>
2025-03-31 08:26:31 -0400

    Update DisableTouchPad.bat

--- DisableTouchPad.bat
+++ DisableTouchPad.bat
@@ -2,4 +2,6 @@ rem # Disable TouchPad
 
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\PrecisionTouchPad\Status" /v "Enabled" /t REG_DWORD /d "0" /f
 
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PrecisionTouchPad" /v "AAPDisabled" /t REG_DWORD /d "1" /f
+
 PAUSE
