f4ffec25cd43cb74d2def07b3d6fc3f5aefff54f
shoober420 <shoober420@gmail.com>
2025-04-03 13:19:46 -0400

    Update EnableTouchPad.bat

--- EnableTouchPad.bat
+++ EnableTouchPad.bat
@@ -4,4 +4,8 @@ reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\PrecisionTouchPad\Status
 
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PrecisionTouchPad" /v "AAPDisabled" /t REG_DWORD /d "0" /f
 
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\PrecisionTouchPad" /v "ThreeFingerTapEnabled" /t REG_DWORD /d "1" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\PrecisionTouchPad" /v "FourFingerSlideEnabled" /t REG_DWORD /d "1" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\PrecisionTouchPad" /v "FourFingerTapEnabled" /t REG_DWORD /d "1" /f
+
 PAUSE
