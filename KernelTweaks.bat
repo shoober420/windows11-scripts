34b1036a80443936c8512a3cb8ca7521f0286040
shoober420 <shoober420@gmail.com>
2025-04-03 15:50:33 -0400

    Update KernelTweaks.bat

--- KernelTweaks.bat
+++ KernelTweaks.bat
@@ -138,4 +138,17 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Control\KernelVelocity" /v "DisableFGBoos
 rem # Kernel Deadlock Detection
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager" /v "EnableDeadGwdTimers" /t REG_DWORD /d "1" /f
 
+rem # https://sites.google.com/site/tweakradje/windows/windows-tweaking
+
+rem # Copy Buffer Size
+rem # 0x40000/256Kb - 0x2000/8Kb - 0x100000/1Mb
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "CopyBufferSize" /t REG_DWORD /d "0x100000" /f
+
+rem # 32KB blocks
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "CopyFileChunkSize" /t REG_DWORD /d "0x8000" /f
+
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "CopyFileOverlappedCount" /t REG_DWORD /d "0x20" /f
+
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "CopyFileBufferedSynchronousIo" /t REG_DWORD /d "0x40" /f
+
 PAUSE
