a6d5db9129a909a53202baa465f72f9b9ed37643
shoober420 <shoober420@gmail.com>
2025-04-03 18:48:12 -0400

    Update InternetSettings.bat

--- InternetSettings.bat
+++ InternetSettings.bat
@@ -14,6 +14,8 @@ rem # https://www.ibm.com/docs/en/was/9.0.5?topic=systems-tuning-windows
 
 rem # https://www.cyberdasm.com/publ/volume_2/computer_tips/how_to_reduce_lag_in_bdo/14-1-0-311
 
+rem # https://github.com/DeDf/afd/blob/master/registry.txt
+
 rem # software firewall is bloat, use router firewall
 
 rem # RECOMMENDED TO RUN DNS.ps1 FOR OPTIMAL DNS SETTINGS
