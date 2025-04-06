019ee811552dd595c3562a6670d0f2c3c95098a8
shoober420 <shoober420@gmail.com>
2025-04-06 12:21:04 -0400

    Update Theme.bat

--- Theme.bat
+++ Theme.bat
@@ -39,6 +39,9 @@ rem # Background Type
 rem # 0 = Picture / 1 = Solid Color / 2 = Slideshow / 3 = Windows Spotlight
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Wallpapers" /v "BackgroundType" /t REG_DWORD /d "1" /f
 
+rem # Disable Thumbnails
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "DisableThumbnails" /t REG_DWORD /d "1" /f
+
 rem # Disable Desktop Spotlight
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\DesktopSpotlight\Settings" /v "EnabledState" /t REG_DWORD /d "0" /f
 
