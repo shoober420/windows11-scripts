09d9675c18f4eb0a7aaa9857b0616a78e10acf0c
shoober420 <shoober420@gmail.com>
2025-04-06 11:59:10 -0400

    Update EnablePerProcessSystemDPI.bat

--- EnablePerProcessSystemDPI.bat
+++ EnablePerProcessSystemDPI.bat
@@ -5,4 +5,8 @@ reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Control Panel\Desktop" /v "Ena
 reg add "HKCU\Software\Policies\Microsoft\Windows\Display" /v "EnablePerProcessSystemDPIForProcesses" /t REG_DWORD /d "1" /f
 reg add "HKLM\Software\Policies\Microsoft\Windows\Display" /v "EnablePerProcessSystemDPIForProcesses" /t REG_DWORD /d "1" /f
 
+reg add "HKCU\Control Panel\Desktop" /v "LogPixels" /t REG_DWORD /d "0x60" /f
+reg add "HKCU\Control Panel\Desktop" /v "Win8DpiScaling" /t REG_DWORD /d "0" /f
+reg add "HKCU\Control Panel\Desktop\WindowMetrics" /v "AppliedDPI" /t REG_DWORD /d "0x60" /f
+
 PAUSE
