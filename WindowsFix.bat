2253bbb40f78eb43c1eed553f7da8f6aca1a62aa
shoober420 <shoober420@gmail.com>
2025-04-09 19:49:47 -0400

    Update WindowsFix.bat

--- WindowsFix.bat
+++ WindowsFix.bat
@@ -1,10 +1,15 @@
-REM Standard commands to run on Windows systems that are having issues and bugs
-REM /startcomponentcleanup cleans WinSxS directory
+rem # Standard commands to run on Windows systems that are having issues and bugs
+
+rem # /startcomponentcleanup cleans WinSxS directory
 
 DISM /Online /Cleanup-Image /RestoreHealth
+
 dism /online /cleanup-image /startcomponentcleanup
+
 sfc /scannow
+
 chkdsk c: /sdcleanup /offlinescanandfix
+
 chkdsk c: /f /r /x /b
 
 PAUSE
