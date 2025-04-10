4c6c7cf44998aea3bc99abf328aff3005583351d
shoober420 <shoober420@gmail.com>
2025-04-09 20:09:20 -0400

    Update StartGraphicsPerfSvc.bat

--- StartGraphicsPerfSvc.bat
+++ StartGraphicsPerfSvc.bat
@@ -1,7 +1,8 @@
-rem Starts and enables GraphicsPerfSvc (improves performance)
+rem # Starts and enables GraphicsPerfSvc (improves performance)
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\GraphicsPerfSvc" /v "Start" /t REG_DWORD /d "2" /f
 sc config winmgmt start= auto
+
 net start GraphicsPerfSvc
 
 PAUSE
