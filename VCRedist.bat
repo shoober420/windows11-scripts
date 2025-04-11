7e8b3ad5c6991dfa09b9981e409fa85c7aadcd66
shoober420 <shoober420@gmail.com>
2025-04-11 17:57:33 -0400

    Update VCRedist.bat

--- VCRedist.bat
+++ VCRedist.bat
@@ -7,7 +7,7 @@ PAUSE
 cd C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_1.20.1081.0_x64__8wekyb3d8bbwe
 winget --info
 
-winget install --id=Microsoft.VCRedist.2005.x86 -e
+ECHO Y | winget install --id=Microsoft.VCRedist.2005.x86 -e
 winget install --id=Microsoft.VCRedist.2005.x64 -e
 winget install --id=Microsoft.VCRedist.2008.x86 -e
 winget install --id=Microsoft.VCRedist.2008.x64 -e
