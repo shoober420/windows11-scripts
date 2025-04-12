e767acaf4671e4aec5e6a7f3e2a50ac443e74c7f
shoober420 <shoober420@gmail.com>
2025-04-12 04:57:23 -0400

    Create Expert_Install.bat

---
+++ Expert_Install.bat
@@ -0,0 +1,18 @@
+rem # Expert Installation script
+rem # Launches scripts to disable Windows Defender and other security features
+rem # Launch in SAFE MODE
+
+
+
+< NUL call DisableWindowsDefender.bat
+< NUL call DisableWindowsFirewall.bat
+< NUL call DisableWindowsSecurityCenter.bat
+< NUL call DisableEventTraceSessions.bat
+< NUL call DisableSAM.bat
+< NUL call DisableLanmanServer.bat
+< NUL call DisableStartMenu.bat
+< NUL call DisableComponents.bat
+< NUL call DisableWindowsSearch.bat
+< NUL call DeleteFonts.bat
+
+PAUSE
