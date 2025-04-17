f49a90f2a3ed92ceb632faa60338b77a117f4821
shoober420 <shoober420@gmail.com>
2025-04-17 19:57:45 -0400

    Update EnableActivateWindows.bat

--- EnableActivateWindows.bat
+++ EnableActivateWindows.bat
@@ -1,5 +1,12 @@
 rem # Enable Activate Windows services
-rem # Used to remove Activate Windows watermark when sync with Windows servers is absent for tooo g
+
+rem # Used to remove Activate Windows watermark when sync with Windows servers is absent for too long
+
+rem # REBOOT REQUIRED
+
+rem # It takes a couple minutes after reboot for the activation sync to occur and Windows being activated again
+
+PAUSE
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\LicenseManager" /v "Start" /t REG_DWORD /d "2" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\sppsvc" /v "Start" /t REG_DWORD /d "2" /f
