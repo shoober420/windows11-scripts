a886c5e694a4625cc1f3cea2c2b73530222a74b6
shoober420 <shoober420@gmail.com>
2025-04-25 10:52:15 -0400

    Update PowerPlanUltra.bat

--- PowerPlanUltra.bat
+++ PowerPlanUltra.bat
@@ -17,6 +17,15 @@ powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 00000000-0000-000
 
 powercfg /setactive 00000000-0000-0000-0000-000000001234
 
+rem # Delete "Balanced" power config plan
+powercfg -delete 381b4222-f694-41f0-9685-ff5bb260df2e
+
+rem # Delete "Power saver" config plan
+powercfg -delete a1841308-3541-4fab-bc81-f71556f20b4a
+
+rem # Delete "High Performance" power config plan (use Ultimate Performance instead, PowerPlanUltra.bat)
+powercfg -delete 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+
 rem # Power Plan Settings
 
 TIMEOUT 5
