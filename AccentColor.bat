d1583f7e92caf14109466988a166b0b4fc4d1b0c
shoober420 <shoober420@gmail.com>
2025-04-28 03:43:38 -0400

    Update AccentColor.bat

--- AccentColor.bat
+++ AccentColor.bat
@@ -60,8 +60,9 @@ reg add "HKCU\Control Panel\Colors" /v "WindowText" /t REG_SZ /d "0 0 0" /f
 rem # Windows 95/98 Black Font for Menu Text
 reg add "HKCU\Control Panel\Colors" /v "MenuText" /t REG_SZ /d "0 0 0" /f
 
-rem # Windows 95/98 Black Font for Info Text
+rem # Windows 95/98 Black Font and White Background for Info Text
 reg add "HKCU\Control Panel\Colors" /v "InfoText" /t REG_SZ /d "0 0 0" /f
+reg add "HKCU\Control Panel\Colors" /v "InfoWindow" /t REG_SZ /d "255 255 255" /f
 
 rem # Windows 95 Gray Inactive Title Bar
 reg add "HKCU\Software\Microsoft\Windows\DWM" /v "AccentColorInactive" /t REG_DWORD /d "0xff808080" /f
@@ -69,7 +70,33 @@ reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Accent" /v "Acc
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "AccentColorInactive" /t REG_DWORD /d "0xff808080" /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "AccentColorInactive" /t REG_DWORD /d "0xff808080" /f
 
+rem # Windows 95 Title Bars
+reg add "HKCU\Control Panel\Colors" /v "ActiveTitle" /t REG_SZ /d "0 0 128" /f
+reg add "HKCU\Control Panel\Colors" /v "InactiveTitle" /t REG_SZ /d "128 128 128" /f
+reg add "HKCU\Control Panel\Colors" /v "TitleText" /t REG_SZ /d "255 255 255" /f
 reg add "HKCU\Control Panel\Colors" /v "InactiveTitleText" /t REG_SZ /d "192 192 192" /f
+
+rem # Windows 95 Borders
+reg add "HKCU\Control Panel\Colors" /v "ActiveBorder" /t REG_SZ /d "192 192 192" /f
+reg add "HKCU\Control Panel\Colors" /v "InactiveBorder" /t REG_SZ /d "192 192 192" /f
+
+rem # Windows 95 White Window Background
+reg add "HKCU\Control Panel\Colors" /v "Window" /t REG_SZ /d "255 255 255" /f
+
+rem # Windows 95 Menu Border Color
+reg add "HKCU\Control Panel\Colors" /v "WindowFrame" /t REG_SZ /d "0 0 0" /f
+
+rem # Windows 95 Menus
+reg add "HKCU\Control Panel\Colors" /v "GrayText" /t REG_SZ /d "128 128 128" /f
+reg add "HKCU\Control Panel\Colors" /v "Menu" /t REG_SZ /d "192 192 192" /f
+
+rem # Windows 95 Buttons
+rem # Light Golden Rod Yellow - RGB: 223 223 223 / BGR: 223 223 223 = 0xffdfdfdf / 0xffdfdfdf
+reg add "HKCU\Control Panel\Colors" /v "ButtonDkShadow" /t REG_SZ /d "0 0 0" /f
+reg add "HKCU\Control Panel\Colors" /v "ButtonFace" /t REG_SZ /d "192 192 192" /f
+reg add "HKCU\Control Panel\Colors" /v "ButtonHilight" /t REG_SZ /d "255 255 255" /f
+reg add "HKCU\Control Panel\Colors" /v "ButtonLight" /t REG_SZ /d "223 223 223" /f
+reg add "HKCU\Control Panel\Colors" /v "ButtonShadow" /t REG_SZ /d "128 128 128" /f
 
 rem # Windows 95 Navy Hilight
 rem # Highlighted Menu Color
