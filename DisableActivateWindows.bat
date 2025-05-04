4273a2e0dfc35aac987b7b9ee3c114926905025b
shoober420 <shoober420@gmail.com>
2025-05-04 10:16:10 -0400

    Update DisableActivateWindows.bat

--- DisableActivateWindows.bat
+++ DisableActivateWindows.bat
@@ -1,5 +1,7 @@
 rem # Disable Activate Windows services
 
+rem # Activate Windows watermark and taskbar always visible in fullscreen when genuine windows sync is stopped
+
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\LicenseManager" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\sppsvc" /v "Start" /t REG_DWORD /d "4" /f
 
