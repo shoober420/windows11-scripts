df2b82bd9e703b2075c3effdfc94e14786ed5777
shoober420 <shoober420@gmail.com>
2025-05-10 15:48:07 -0400

    Update AMDGPUTweaks.bat

--- AMDGPUTweaks.bat
+++ AMDGPUTweaks.bat
@@ -17,6 +17,12 @@ rem # Sources:
 rem # https://sites.google.com/view/melodystweaks/basictweaks
 rem # https://github.com/sherifmagdy32/gaming_os_tweaker
 
+rem # Enable and start WMI
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Winmgmt" /v "Start" /t REG_DWORD /d "2" /f
+sc config winmgmt start= auto
+net start winmgmt
+
 rem # https://www.elevenforum.com/members/garlin.5387/
 rem # https://www.elevenforum.com/t/gpu-tweaks-batch-script.30134/post-521530
 if not exist C:\Windows\System32\wbem\WMIC.exe (
