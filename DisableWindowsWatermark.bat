9caf4c699d8403164c97c5a7f77d69c28b631ad3
shoober420 <shoober420@gmail.com>
2025-05-10 16:37:47 -0400

    Update DisableWindowsWatermark.bat

--- DisableWindowsWatermark.bat
+++ DisableWindowsWatermark.bat
@@ -1,5 +1,5 @@
 rem # Disable Windows watermark and version at the bottom right of the screen
 
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows" /v "DisplayVersion" /t REG_DWORD /d "1" /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows" /v "DisplayVersion" /t REG_DWORD /d "0" /f
 
 PAUSE
