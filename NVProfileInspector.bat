eed4e913d538d4407743bb8ee481412960610fca
shoober420 <shoober420@gmail.com>
2025-05-15 15:00:31 -0400

    Create NVProfileInspector.bat

---
+++ NVProfileInspector.bat
@@ -0,0 +1,5 @@
+rem # NVIDIA Profile Inspector settings loader
+
+start "%USERPROFILE%\Downloads\NvidiaProfileInspector\nvidiaProfileInspector.exe" "%USERPROFILE%\Downloads\NvidiaProfileInspector\Performance.nip"
+
+PAUSE
