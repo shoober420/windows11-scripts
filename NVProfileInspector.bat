a147f843e455293328002caf825ef1d1c049b69d
shoober420 <shoober420@gmail.com>
2025-05-15 15:04:41 -0400

    Update NVProfileInspector.bat

--- NVProfileInspector.bat
+++ NVProfileInspector.bat
@@ -1,5 +1,40 @@
-rem # NVIDIA Profile Inspector settings loader
+rem # NVIDIA Profile Inspector selector
+
+@echo off
+
+@echo off
+
+echo.
+echo NVIDIA Profile Inspector selector
+echo.
+echo 1. Performance
+echo 2. High Quality OGL
+echo 3. High Quality D3D
+echo C. Cancel
+echo.
+choice /c 12C /m "Choose an option :"
+
+if 4 EQU %ERRORLEVEL% (
+   echo User chose to cancel.
+) else if 2 EQU %ERRORLEVEL% (
+   call :d3d
+) else if 2 EQU %ERRORLEVEL% (
+   call :ogl
+) else if 1 EQU %ERRORLEVEL% (
+   call :perf
+) else if 0 EQU %ERRORLEVEL% (
+   echo User bailed out.
+)
+
+goto :eof
+
+:perf
+echo User chose Performance
 
 start "%USERPROFILE%\Downloads\NvidiaProfileInspector\nvidiaProfileInspector.exe" "%USERPROFILE%\Downloads\NvidiaProfileInspector\Performance.nip"
+
+goto :end
+
+
 
 PAUSE
