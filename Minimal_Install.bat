8070fe9de67c6488bbcb2fd0e38df994f2b77f5c
shoober420 <shoober420@gmail.com>
2025-05-17 12:16:27 -0400

    Update Minimal_Install.bat

--- Minimal_Install.bat
+++ Minimal_Install.bat
@@ -70,6 +70,49 @@ call NetworkTweaks.bat
 
 cd "%~dp0"
 
+echo.
+echo 1. Vim
+echo 2. Notepad++
+echo 3. SKIP
+echo C. Cancel
+echo.
+choice /c 123C /m "Choose an option :"
+
+if 4 EQU %ERRORLEVEL% (
+   echo User chose to cancel.
+) else if 3 EQU %ERRORLEVEL% (
+   call :skippy
+) else if 2 EQU %ERRORLEVEL% (
+   call :note
+) else if 1 EQU %ERRORLEVEL% (
+   call :vim
+) else if 0 EQU %ERRORLEVEL% (
+   echo User bailed out.
+)
+
+goto :eof
+
+:vim
+echo User chose Vim
+
+call InstallVim.bat
+
+goto :end
+
+:vim
+echo User chose Notepad++
+
+call InstallNotepad++.bat
+
+goto :end
+
+:skippy
+echo User chose SKIP
+
+goto :end
+
+:end
+
 ECHO R | powershell.exe ./DisableNetBIOS.ps1
 cd "%~dp0"
 
