cfcbaaea98afa84f47dd676838a671ff745e34eb
shoober420 <shoober420@gmail.com>
2025-05-24 23:37:02 -0400

    Update DisableNull.bat

--- DisableNull.bat
+++ DisableNull.bat
@@ -1,5 +1,7 @@
 rem # Disable Null service
 
+rem # required for Android Debug Bridge (ADB)
+
 net stop Null
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Null" /v "Start" /t REG_DWORD /d "4" /f
