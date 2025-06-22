12e46eff46405e2a45a4b2250e3e060d1a943b44
shoober420 <shoober420@gmail.com>
2025-06-22 12:58:50 -0400

    Update DisableDHCP.bat

--- DisableDHCP.bat
+++ DisableDHCP.bat
@@ -1,13 +1,5 @@
 rem # Disable DHCP service
 
-rem # !!! WARNING !!!
-
-rem # BREAKS EPIC GAMES LAUNCHER
-
-rem # !!! WARNING !!!
-
-PAUSE
-
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Dhcp" /v "Start" /t REG_DWORD /d "4" /f
 
 bcdedit /set dhcp Off
