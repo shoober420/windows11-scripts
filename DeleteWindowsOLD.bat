f53e0fd88fc508d516495ce26c8b5fa395054dea
shoober420 <shoober420@gmail.com>
2025-06-26 19:06:07 -0400

    Create DeleteWindowsOLD.bat

---
+++ DeleteWindowsOLD.bat
@@ -0,0 +1,31 @@
+rem # Delete Windows.old directory
+
+rem # Launch SetACL.bat to take ownership of reg keys
+cd "%~dp0"
+ECHO R | powershell.exe ./SetACL.bat
+cd "%USERPROFILE%\Downloads"
+
+cd "%USERPROFILE%\Downloads\windows11-scripts-main"
+ECHO R | powershell.exe ./SetACL.bat
+cd "%USERPROFILE%\Downloads"
+
+cd "%USERPROFILE%\Downloads\windows11-scripts-main\windows11-scripts-main"
+ECHO R | powershell.exe ./SetACL.bat
+cd "%USERPROFILE%\Downloads"
+
+cd "%USERPROFILE%\Downloads\windows11-scripts\windows11-scripts"
+ECHO R | powershell.exe ./SetACL.bat
+cd "%USERPROFILE%\Downloads"
+
+cd "%USERPROFILE%\Downloads\windows11-scripts"
+ECHO R | powershell.exe ./SetACL.bat
+cd "%USERPROFILE%\Downloads"
+
+
+
+takeown /s %computername% /u %username% /f "C:\Windows.old"
+icacls "C:\Windows.old" /grant:r %username%:F
+rem ren "C:\Windows.old" "C:\Windows.old.bak"
+del "C:\Windows.old" /s /f /q
+
+PAUSE
