a88d944595336e31de736ce6d9c7126a80cbe067
shoober420 <shoober420@gmail.com>
2025-07-01 03:25:39 -0400

    Update InstallStore.bat

--- InstallStore.bat
+++ InstallStore.bat
@@ -1,5 +1,7 @@
 rem # Install Microsoft Store
 
+rem # Required for Minecraft
+
 powershell -command "Get-AppxPackage -AllUsers *Microsoft.WindowsStore* | Add-AppxPackage"
 
 powershell -command "Get-AppxPackage -AllUsers *Microsoft.StorePurchaseApp* | Add-AppxPackage"
