4a77e1d1165100e5c8619c041ac948ec8c3eaa9d
shoober420 <shoober420@gmail.com>
2025-07-01 03:11:58 -0400

    Update RemoveStore.bat

--- RemoveStore.bat
+++ RemoveStore.bat
@@ -1,5 +1,13 @@
 rem # Remove Microsoft Store
 
+rem # !!! WARNING !!!
+
+rem # BREAKS MINECRAFT
+
+rem # !!! WARNING !!!
+
+PAUSE
+
 powershell -command "Get-AppxPackage -AllUsers *Microsoft.WindowsStore* | Remove-AppxPackage"
 
 powershell -command "Get-AppxPackage -AllUsers *Microsoft.StorePurchaseApp* | Remove-AppxPackage"
