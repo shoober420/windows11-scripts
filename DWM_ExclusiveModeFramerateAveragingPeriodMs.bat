60bc08abbac65783714a3e7623fe381e7086a123
shoober420 <shoober420@gmail.com>
2025-07-19 19:18:06 -0400

    Update DWM_ExclusiveModeFramerateAveragingPeriodMs.bat

--- DWM_ExclusiveModeFramerateAveragingPeriodMs.bat
+++ DWM_ExclusiveModeFramerateAveragingPeriodMs.bat
@@ -23,10 +23,10 @@ call SetTimerResolution.bat
 rem # https://sites.google.com/site/tweakradje/windows/windows-tweaking
 
 rem # Default (Hex / Decimal): 0x3e8 = 1000
-rem # 0x2ee = 750
-rem # 0x1f4 = 500
-rem # 0xfa = 250
-rem # 0x64 = 100
+rem # 0x2ee = 750 (FAST SYSTEMS ONLY)
+rem # 0x1f4 = 500 (VERY FAST SYSTEMS ONLY)
+rem # 0xfa = 250 (VERY VERY FAST SYSTEMS ONLY)
+rem # 0x64 = 100 (ULTRA FAST SYSTEMS ONLY)
 reg add "HKLM\SOFTWARE\Microsoft\Windows\Dwm\ExtendedComposition" /v "ExclusiveModeFramerateAveragingPeriodMs" /t REG_DWORD /d "0x3e8" /f
 
 PAUSE
