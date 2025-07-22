6b6663c257446504d2ab5353df3079a43e1ce768
shoober420 <shoober420@gmail.com>
2025-07-22 16:28:04 -0400

    Update StartGraphicsPerfSvc.bat

--- StartGraphicsPerfSvc.bat
+++ StartGraphicsPerfSvc.bat
@@ -1,7 +1,10 @@
-rem # Starts and enables GraphicsPerfSvc (improves performance)
+rem # Starts and enables GraphicsPerfSvc
+
+rem # REQUIRED FOR HAGS (GPU SCHEDULING) AND DLSS
+
+rem # https://steamcommunity.com/discussions/forum/11/2798375980835798574/#c3458220484816217479
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\GraphicsPerfSvc" /v "Start" /t REG_DWORD /d "2" /f
-sc config winmgmt start= auto
 
 net start GraphicsPerfSvc
 
