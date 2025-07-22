2df997d7a4f9d4c4bec554b0aee54332010a88e4
shoober420 <shoober420@gmail.com>
2025-07-22 01:03:56 -0400

    Update GPUTweaks.bat

--- GPUTweaks.bat
+++ GPUTweaks.bat
@@ -40,13 +40,6 @@ if not exist C:\Windows\System32\wbem\WMIC.exe (
 rem # MAY CAUSE CRASHES
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "PlatformSupportMiracast" /t REG_DWORD /d "0" /f
 
-rem # Enable Hardware Accelerated GPU Scheduling (HAGS)
-rem # GPU to handle some of its own scheduling tasks, potentially reducing CPU overhead and latency
-rem # Required for DLSS Frame Generation
-rem # 2 = HAGS ON / 1 = HAGS OFF
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "HwSchMode" /t REG_DWORD /d "2" /f
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "HwSchedMode" /t REG_DWORD /d "2" /f
-
 rem # Affinity tweak
 rem # garlin: https://www.elevenforum.com/members/garlin.5387/
 rem # https://www.elevenforum.com/t/wmic-query-cpu-core-count-to-execute-setting.30155/post-521450
@@ -356,5 +349,58 @@ reg add "HKLM\SOFTWARE\Wow6432Node\Microsoft\DirectDraw" /v "ForceNoSysLock" /t 
 
 rem # Intel iGPU Dedicated Segment Size
 rem reg add "HKLM\SOFTWARE\Intel\GMM" /v "DedicatedSegmentSize" /t REG_DWORD /d "512"
+
+
+
+@echo off
+
+
+
+echo.
+echo Hardware Accelerated GPU Scheduling (HAGS)
+echo.
+echo 1. HAGS ENABLED (REQUIRED FOR DLSS)
+echo 2. HAGS DISABLED
+echo C. Cancel
+echo.
+choice /c 12C /m "Choose an option :"
+
+if 3 EQU %ERRORLEVEL% (
+   echo User chose to cancel.
+) else if 2 EQU %ERRORLEVEL% (
+   call :hagsoff
+) else if 1 EQU %ERRORLEVEL% (
+   call :hagson
+) else if 0 EQU %ERRORLEVEL% (
+   echo User bailed out.
+)
+
+goto :eof
+
+:hagson
+echo User chose HAGS ENABLED (REQUIRED FOR DLSS)
+
+rem # Enable Hardware Accelerated GPU Scheduling (HAGS)
+rem # GPU to handle some of its own scheduling tasks, potentially reducing CPU overhead and latency
+rem # Required for DLSS Frame Generation
+rem # 2 = HAGS ON / 1 = HAGS OFF
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "HwSchMode" /t REG_DWORD /d "2" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "HwSchedMode" /t REG_DWORD /d "2" /f
+
+goto :end
+
+:hagsoff
+echo User chose HAGS DISABLED
+
+rem # Disable Hardware Accelerated GPU Scheduling (HAGS)
+rem # GPU to handle some of its own scheduling tasks, potentially reducing CPU overhead and latency
+rem # Required for DLSS Frame Generation
+rem # 2 = HAGS ON / 1 = HAGS OFF
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "HwSchMode" /t REG_DWORD /d "1" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "HwSchedMode" /t REG_DWORD /d "1" /f
+
+goto :end
+
+:end
 
 PAUSE
