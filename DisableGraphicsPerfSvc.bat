6dcbad6d6b96b11fa5fd25126380e437d825f057
shoober420 <shoober420@gmail.com>
2025-07-22 16:26:46 -0400

    Update DisableGraphicsPerfSvc.bat

--- DisableGraphicsPerfSvc.bat
+++ DisableGraphicsPerfSvc.bat
@@ -1,5 +1,7 @@
 rem # Disable GraphicsPerfSvc
 
+rem # https://steamcommunity.com/discussions/forum/11/2798375980835798574/#c3458220484816217479
+
 rem # !!! WARNING !!!
 
 rem # BREAKS HAGS (GPU SCHEDULING) AND DLSS
