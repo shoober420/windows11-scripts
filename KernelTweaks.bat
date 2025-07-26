a9f3ea705cb762aee52ef9bc808848ac5fe20a8e
shoober420 <shoober420@gmail.com>
2025-07-26 02:18:21 -0400

    Update KernelTweaks.bat

--- KernelTweaks.bat
+++ KernelTweaks.bat
@@ -38,6 +38,7 @@ rem # Default value is 0, meaning the system uses its own heuristics to determin
 rem # 0 and 1 lower FPS
 rem # 256 causes kernel error at boot
 rem # AlchemyTweaks = 128
+rem # MIGHT CAUSE UNBOOTABLE MACHINE ONCE MANUALLY TWEAKED
 reg delete "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel\MaximumSharedReadyQueueSize" /f
 rem reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "MaximumSharedReadyQueueSize" /t REG_DWORD /d "128" /f
 
