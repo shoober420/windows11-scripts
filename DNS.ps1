8f4ec1004f92c6519f4e8804b3965b82e921c98a
shoober420 <shoober420@gmail.com>
2025-07-30 23:55:47 -0400

    Update DNS.ps1

--- DNS.ps1
+++ DNS.ps1
@@ -1,6 +1,7 @@
 # Set DNS servers to Cloudflare 1.1.1.1 and use encrypted DoH
+
 $i = Get-NetAdapter -Physical
-$i | Get-DnsClientServerAddress -AddressFamily IPv4 | Set-DnsClientServerAddress -ServerAddresses '1.1.1.1', '1.0.0.1'
+$i | Get-DnsClientServerAddress -AddressFamily IPv4 | Set-DnsClientServerAddress -ServerAddresses '1.1.1.1', '1.0.0.1' -AllowFallbackToUdp $False -AutoUpgrade $True
 $i | ForEach-Object {
 $s1 = 'HKLM:System\CurrentControlSet\Services\Dnscache\InterfaceSpecificParameters\' + $_.InterfaceGuid + '\DohInterfaceSettings\Doh\1.1.1.1'; New-Item -Path $s1 -Force | New-ItemProperty -Name "DohFlags" -Value 1 -PropertyType Qword
 $s2 = 'HKLM:System\CurrentControlSet\Services\Dnscache\InterfaceSpecificParameters\' + $_.InterfaceGuid + '\DohInterfaceSettings\Doh\1.0.0.1'; New-Item -Path $s2 -Force  | New-ItemProperty -Name "DohFlags" -Value 1 -PropertyType Qword
