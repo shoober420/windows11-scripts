060db269a992a7a4698db1dbea8d031ed245ee2f
shoober420 <shoober420@gmail.com>
2025-08-01 19:49:18 -0400

    Update Expert_Install.bat

--- Expert_Install.bat
+++ Expert_Install.bat
@@ -122,6 +122,22 @@ goto :end
 
 :end
 
+
+
+rem # Disable AMD External Events Utility
+rem # V3nilla
+rem # https://github.com/shoober420/windows11-scripts/issues/14
+
+rem # !!! WARNING !!!
+
+rem # Locks FPS to monitor refresh rate for some games
+
+rem # !!! WARNING !!!
+
+rem reg add "HKLM\SYSTEM\CurrentControlSet\Services\AMD External Events Utility" /v "Start" /t REG_DWORD /d "4" /f
+
+
+
 PAUSE
 
 rem # SETUP CUSTOM RESOLUTION UTILITY
