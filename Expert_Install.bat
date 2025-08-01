fd194dcacbfffed64f86207c6c32615bd42c3974
shoober420 <shoober420@gmail.com>
2025-08-01 19:57:15 -0400

    Update Expert_Install.bat

--- Expert_Install.bat
+++ Expert_Install.bat
@@ -124,6 +124,44 @@ goto :end
 
 
 
+echo.
+echo 1. Enable AMD External Events Utility service
+echo 2. Disable AMD External Events Utility service (Locks FPS to monitor refresh rate in some games)
+echo 3. SKIP
+echo C. Cancel
+echo.
+choice /c 123C /m "Choose an option :"
+
+if 4 EQU %ERRORLEVEL% (
+   echo User chose to cancel.
+) else if 3 EQU %ERRORLEVEL% (
+   call :scyp
+) else if 2 EQU %ERRORLEVEL% (
+   call :amdexf
+) else if 1 EQU %ERRORLEVEL% (
+   call :amdexo
+) else if 0 EQU %ERRORLEVEL% (
+   echo User bailed out.
+)
+
+goto :eof
+
+:amdexo
+echo User chose Enable AMD External Events Utility service
+
+rem # Enable AMD External Events Utility
+rem # V3nilla
+rem # https://github.com/shoober420/windows11-scripts/issues/14
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\AMD External Events Utility" /v "Start" /t REG_DWORD /d "2" /f
+
+net start "AMD External Events Utility"
+
+goto :end
+
+:amdexf
+echo User chose Disable AMD External Events Utility service (Locks FPS to monitor refresh rate in some games)
+
 rem # Disable AMD External Events Utility
 rem # V3nilla
 rem # https://github.com/shoober420/windows11-scripts/issues/14
@@ -134,7 +172,18 @@ rem # Locks FPS to monitor refresh rate for some games
 
 rem # !!! WARNING !!!
 
-rem reg add "HKLM\SYSTEM\CurrentControlSet\Services\AMD External Events Utility" /v "Start" /t REG_DWORD /d "4" /f
+net stop "AMD External Events Utility"
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\AMD External Events Utility" /v "Start" /t REG_DWORD /d "4" /f
+
+goto :end
+
+:scyp
+echo User chose SKIP
+
+goto :end
+
+:end
 
 
 
