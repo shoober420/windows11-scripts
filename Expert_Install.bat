9fa946cd3ee2cf3fc231ac203027b87ca8f71434
shoober420 <shoober420@gmail.com>
2025-08-01 01:15:26 -0400

    Update Expert_Install.bat

--- Expert_Install.bat
+++ Expert_Install.bat
@@ -1,5 +1,41 @@
 rem # Expert Installation script
 
+rem # Credits:
+
+rem # https://www.elevenforum.com/members/garlin.5387
+rem # https://www.elevenforum.com/members/csmc.38355
+rem # jdallmann
+rem # Kizzimo
+rem # foss-lover34
+rem # brusk9060
+rem # dersk111y
+rem # V3nilla
+rem # ja2forever
+rem # https://www.mdgx.com
+rem # https://github.com/Batleman
+rem # https://github.com/AlchemyTweaks
+rem # https://github.com/TairikuOokami
+rem # https://github.com/ionuttbara
+rem # https://github.com/Hyyote
+rem # https://github.com/MoriEdan
+rem # https://github.com/NicholasBly
+rem # https://github.com/rahilpathan
+rem # https://github.com/ChrisTitusTech
+rem # https://github.com/raspi
+rem # https://github.com/simeononsecurity
+rem # https://github.com/alufena
+rem # https://sites.google.com/site/tweakradje/windows/windows-tweaking
+rem # https://admx.help
+rem # https://github.com/ancel1x/Ancels-Performance-Batch
+rem # https://github.com/Batlez/Batlez-Tweaks
+rem # https://sites.google.com/view/melodystweaks/basictweaks
+rem # https://github.com/sherifmagdy32/gaming_os_tweaker
+rem # https://github.com/HakanFly/Windows-Tweaks
+rem # http://bbs.c3.wuyou.net/forum.php?mod=viewthread&tid=437913
+rem # https://github.com/CrackedStuffEZ
+
+
+
 rem # Launches scripts to disable Windows Defender and other security features
 
 rem # Recommended to launch in SAFE MODE
