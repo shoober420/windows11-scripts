72b47770b37afd24e35c8366f2599c1b8eb5d84c
shoober420 <shoober420@gmail.com>
2025-08-02 05:45:13 -0400

    Update EnableFullscreenOptimizations.bat

--- EnableFullscreenOptimizations.bat
+++ EnableFullscreenOptimizations.bat
@@ -1,11 +1,20 @@
 rem # Enable Fullscreen Optimizations (FSO)
 
-rem # FSO ON = 0 / FSO OFF = 2
+rem # https://learn.microsoft.com/en-us/answers/questions/3741077/fullscreen-optimizations-windows-registry?forum=windows-all&referrer=answers
+
+rem # https://en.ittrip.xyz/windows10/registry-tweaks-gamedvr#google_vignette
+
+rem # Fullscreen Exclusive (FSE)
+rem # Enhanced Fullscreen Exclusive (EFSE)
+rem # Desktop Screen Experience (DSE)
+
+rem # 0 = FSO: ON / FSE: OFF
+rem # 2 = FSO: OFF / FSE: ON
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_FSEBehavior" /t REG_DWORD /d "0" /f
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_FSEBehaviorMode" /t REG_DWORD /d "0" /f
 
 rem # Honor User adjusted FSE value
-reg add "HKCU\System\GameConfigStore" /v "GameDVR_HonorUserFSEBehaviorMode" /t REG_DWORD /d "0" /f
+reg add "HKCU\System\GameConfigStore" /v "GameDVR_HonorUserFSEBehaviorMode" /t REG_DWORD /d "1" /f
 
 rem # Disable Enhanced Fullscreen Exclusive (EFSE)
 rem # 0 = Disable EFSE
@@ -19,13 +28,13 @@ rem # Disable GameDVR recording
 rem # 0 = Enable More FSO Features / 1 = Disable FSO Features
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_DXGIHonorFSEWindowsCompatible" /t REG_DWORD /d "0" /f
 
-rem # Disable GameDVR Overlay
+rem # Enable GameDVR Overlay
 rem # ON = 0 / OFF = 2
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_DSEBehavior" /t REG_DWORD /d "0" /f
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_DSEBehaviorMode" /t REG_DWORD /d "0" /f
 
 rem # Honor User adjusted DSE value
-reg add "HKCU\System\GameConfigStore" /v "GameDVR_HonorUserDSEBehaviorMode" /t REG_DWORD /d "0" /f
+reg add "HKCU\System\GameConfigStore" /v "GameDVR_HonorUserDSEBehaviorMode" /t REG_DWORD /d "1" /f
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v "DisableFullScreenOptimizations" /t REG_DWORD /d "0" /f
 
