f01ff0ea7a10b337b14dffe19f69d28a81819cf2
shoober420 <shoober420@gmail.com>
2025-08-01 20:24:02 -0400

    Update DisableFullscreenOptimizations.bat

--- DisableFullscreenOptimizations.bat
+++ DisableFullscreenOptimizations.bat
@@ -1,14 +1,14 @@
-rem # Disable Fullscreen Optimizations
+rem # Disable Fullscreen Optimizations (FSO)
 
-rem # Disable Fullscreen Optimizations for Current User / 0 - Enabled / 2 - Disabled
+rem # FSO ON = 0 / FSO OFF = 2
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_FSEBehavior" /t REG_DWORD /d "2" /f
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_FSEBehaviorMode" /t REG_DWORD /d "2" /f
 
 rem # Honor User adjusted FSE value
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_HonorUserFSEBehaviorMode" /t REG_DWORD /d "1" /f
 
-rem # Disable Fullscreen Optimizations (FSO)
-rem # 0 = Enabled / 2 = Disabled
+rem # Disable Desktop Session Environment (DSE)
+rem # DSE ON = 0 / DSE OFF = 2
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_DSEBehavior" /t REG_DWORD /d "2" /f
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_DSEBehaviorMode" /t REG_DWORD /d "2" /f
 
