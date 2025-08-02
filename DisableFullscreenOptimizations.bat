a7635ceaf19a365d5010de6e73858448cf2a134c
shoober420 <shoober420@gmail.com>
2025-08-02 05:43:03 -0400

    Update DisableFullscreenOptimizations.bat

--- DisableFullscreenOptimizations.bat
+++ DisableFullscreenOptimizations.bat
@@ -22,6 +22,12 @@ rem # 0x01 = Enable ONLY EFSE
 rem # 0x03 = Enable EFSE + Flip Model Swap Chains
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_EFSEFeatureFlags" /t REG_DWORD /d "0" /f
 
+rem # Disable more FSO features
+rem # Disable Color Management
+rem # Disable GameDVR recording
+rem # 0 = Enable More FSO Features / 1 = Disable FSO Features
+reg add "HKCU\System\GameConfigStore" /v "GameDVR_DXGIHonorFSEWindowsCompatible" /t REG_DWORD /d "1" /f
+
 rem # Disable GameDVR Overlay
 rem # ON = 0 / OFF = 2
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_DSEBehavior" /t REG_DWORD /d "2" /f
@@ -29,12 +35,6 @@ reg add "HKCU\System\GameConfigStore" /v "GameDVR_DSEBehaviorMode" /t REG_DWORD 
 
 rem # Honor User adjusted DSE value
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_HonorUserDSEBehaviorMode" /t REG_DWORD /d "1" /f
-
-rem # Disable more FSO features
-rem # Disable Color Management
-rem # Disable GameDVR recording
-rem # 0 = Enable More FSO Features / 1 = Disable FSO Features
-reg add "HKCU\System\GameConfigStore" /v "GameDVR_DXGIHonorFSEWindowsCompatible" /t REG_DWORD /d "1" /f
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v "DisableFullScreenOptimizations" /t REG_DWORD /d "1" /f
 
