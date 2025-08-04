4bf2cc20c97bea36fcb52a2a3836aef3f0301fce
shoober420 <shoober420@gmail.com>
2025-08-04 04:29:11 -0400

    Update EnableThreadDpc.bat

--- EnableThreadDpc.bat
+++ EnableThreadDpc.bat
@@ -1,4 +1,4 @@
-rem # Enable ThreadDpcEnable
+rem # Enable Thread DPC
 
 rem # Deferred Procedure Call (DPC)
 rem # Allows high-priority tasks to defer required but lower-priority tasks for later execution
@@ -10,7 +10,7 @@ rem # https://forums.blurbusters.com/viewtopic.php?t=13323
 
 rem # https://www.tenforums.com/performance-maintenance/146774-cacheawarescheduling-what-how-does-windows-benefit.html
 
-rem # Enable ThreadDpcEnable
+rem # Enable Thread DPC
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "ThreadDpcEnable" /t REG_DWORD /d "1" /f
 
 PAUSE
