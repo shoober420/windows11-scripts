55d054a564ebcd1cd65b83ebb475558209f63848
shoober420 <shoober420@gmail.com>
2025-08-06 04:19:04 -0400

    Update DisableGPUPreemption.bat

--- DisableGPUPreemption.bat
+++ DisableGPUPreemption.bat
@@ -64,19 +64,6 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers\Scheduler" /v "En
 
 
 
-
-
-
-
-
-
-
-
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers\Power" /v "EnableSCGMidBufferPreemption" /t REG_DWORD /d "0" /f
-
-
-
-
 for /f %%i in ('wmic path Win32_VideoController get PNPDeviceID^| findstr /L "PCI\VEN_"') do (
 for /f "tokens=3" %%a in ('reg query "HKLM\SYSTEM\ControlSet001\Enum\%%i" /v "Driver"') do (
 for /f %%i in ('echo %%a ^| findstr "{"') do (
