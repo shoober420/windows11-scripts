22815a4dfe608e265bb44ffc2af912a62b5baa63
shoober420 <shoober420@gmail.com>
2025-08-12 05:32:08 -0400

    Update DisableAMD_FrameRateTargetControl.bat

--- DisableAMD_FrameRateTargetControl.bat
+++ DisableAMD_FrameRateTargetControl.bat
@@ -6,6 +6,10 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Services\Winmgmt" /v "Start" /t REG_DWORD
 sc config winmgmt start= auto
 net start winmgmt
 
+for /f %%i in ('wmic path Win32_VideoController get PNPDeviceID^| findstr /L "PCI\VEN_"') do (
+        for /f "tokens=3" %%a in ('reg query "HKLM\SYSTEM\ControlSet001\Enum\%%i" /v "Driver"') do (
+                for /f %%i in ('echo %%a ^| findstr "{"') do (
+
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i" /v "KMD_FRTEnabled" /t REG_DWORD /d "0" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i\UMD" /v "KMD_FRTEnabled" /t REG_DWORD /d "0" /f
 
@@ -15,5 +19,9 @@ rem # 100FPS = 0x64
 rem # 60FPS = 0x3C
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i" /v "KMD_MaxFrameRateRequested" /t REG_DWORD /d "0x3E7" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i\UMD" /v "KMD_MaxFrameRateRequested" /t REG_DWORD /d "0x3E7" /f
+
+)
+)
+)
 
 PAUSE
