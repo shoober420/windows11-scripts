7862e8864f7d49b1d1792f4abb298f82fb2a56aa
shoober420 <shoober420@gmail.com>
2025-08-12 03:55:59 -0400

    Update AMDAdrenalinSettingsVSYNC_OFF.bat

--- AMDAdrenalinSettingsVSYNC_OFF.bat
+++ AMDAdrenalinSettingsVSYNC_OFF.bat
@@ -10,7 +10,7 @@ for /f %%i in ('wmic path Win32_VideoController get PNPDeviceID^| findstr /L "PC
         for /f "tokens=3" %%a in ('reg query "HKLM\SYSTEM\ControlSet001\Enum\%%i" /v "Driver"') do (
                 for /f %%i in ('echo %%a ^| findstr "{"') do (
 
-rem # 0 = Always Off / 1 = Default Off / 2 = Default On / 3 = Always On
+rem # 0 = Always Off / 3 = Dynamic VSync / 4 = Always On
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i" /v "VSyncControl" /t REG_BINARY /d "3000" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i" /v "VSyncControl_DEF" /t REG_SZ /d "0" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i" /v "VSyncControl_NA" /t REG_SZ /d "0" /f
