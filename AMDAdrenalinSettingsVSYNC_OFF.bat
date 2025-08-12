5656091908750a62c27f63a82511a18ea1f3fd19
shoober420 <shoober420@gmail.com>
2025-08-12 03:20:32 -0400

    Update AMDAdrenalinSettingsVSYNC_OFF.bat

--- AMDAdrenalinSettingsVSYNC_OFF.bat
+++ AMDAdrenalinSettingsVSYNC_OFF.bat
@@ -1,5 +1,9 @@
 rem # AMD Adrenalin VSYNC Disabled
 
+for /f %%i in ('wmic path Win32_VideoController get PNPDeviceID^| findstr /L "PCI\VEN_"') do (
+        for /f "tokens=3" %%a in ('reg query "HKLM\SYSTEM\ControlSet001\Enum\%%i" /v "Driver"') do (
+                for /f %%i in ('echo %%a ^| findstr "{"') do (
+
 rem # 0 = Always Off / 1 = Default Off / 2 = Default On / 3 = Always On
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i" /v "VSyncControl" /t REG_BINARY /d "3000" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i" /v "VSyncControl_DEF" /t REG_SZ /d "0" /f
@@ -7,5 +11,9 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i" /v "VSyncControl_NA" /
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i\UMD" /v "VSyncControl" /t REG_BINARY /d "3000" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i\UMD" /v "VSyncControl_DEF" /t REG_SZ /d "0" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i\UMD" /v "VSyncControl_NA" /t REG_SZ /d "0" /f
+
+)
+)
+)
 
 PAUSE
