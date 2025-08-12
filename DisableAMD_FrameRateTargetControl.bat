73c9a090e63d69044a3bae55d93fca9358cefcb4
shoober420 <shoober420@gmail.com>
2025-08-12 05:25:37 -0400

    Create DisableAMD_FrameRateTargetControl.bat

---
+++ DisableAMD_FrameRateTargetControl.bat
@@ -0,0 +1,13 @@
+rem # Disable AMD Frame Rate Target Control (FRTC)
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i" /v "KMD_FRTEnabled" /t REG_DWORD /d "0" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i\UMD" /v "KMD_FRTEnabled" /t REG_DWORD /d "0" /f
+
+rem # FPS in Hexidecimal
+rem # 999FPS = 0x3E7
+rem # 100FPS = 0x64
+rem # 60FPS = 0x3C
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i" /v "KMD_MaxFrameRateRequested" /t REG_DWORD /d "0x3E7" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i\UMD" /v "KMD_MaxFrameRateRequested" /t REG_DWORD /d "0x3E7" /f
+
+PAUSE
