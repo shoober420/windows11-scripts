846f9278f3ebb038a48ea3ff5bf88d26fad6f1d2
shoober420 <shoober420@gmail.com>
2025-08-12 05:31:29 -0400

    Update DisableAMD_FrameRateTargetControl.bat

--- DisableAMD_FrameRateTargetControl.bat
+++ DisableAMD_FrameRateTargetControl.bat
@@ -1,5 +1,11 @@
 rem # Disable AMD Frame Rate Target Control (FRTC)
 
+rem # Enable and start WMI
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Winmgmt" /v "Start" /t REG_DWORD /d "2" /f
+sc config winmgmt start= auto
+net start winmgmt
+
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i" /v "KMD_FRTEnabled" /t REG_DWORD /d "0" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i\UMD" /v "KMD_FRTEnabled" /t REG_DWORD /d "0" /f
 
