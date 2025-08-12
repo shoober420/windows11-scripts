9bb3c3bca7f8d34de708130bc52425af4f49b6e2
shoober420 <shoober420@gmail.com>
2025-08-12 05:32:40 -0400

    Update EnableAMD_FrameRateTargetControl.bat

--- EnableAMD_FrameRateTargetControl.bat
+++ EnableAMD_FrameRateTargetControl.bat
@@ -1,5 +1,15 @@
 rem # Enable AMD Frame Rate Target Control (FRTC)
 
+rem # Enable and start WMI
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Winmgmt" /v "Start" /t REG_DWORD /d "2" /f
+sc config winmgmt start= auto
+net start winmgmt
+
+for /f %%i in ('wmic path Win32_VideoController get PNPDeviceID^| findstr /L "PCI\VEN_"') do (
+        for /f "tokens=3" %%a in ('reg query "HKLM\SYSTEM\ControlSet001\Enum\%%i" /v "Driver"') do (
+                for /f %%i in ('echo %%a ^| findstr "{"') do (
+
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i" /v "KMD_FRTEnabled" /t REG_DWORD /d "1" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i\UMD" /v "KMD_FRTEnabled" /t REG_DWORD /d "1" /f
 
@@ -9,5 +19,9 @@ rem # 100FPS = 0x64
 rem # 60FPS = 0x3C
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i" /v "KMD_MaxFrameRateRequested" /t REG_DWORD /d "0x3C" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i\UMD" /v "KMD_MaxFrameRateRequested" /t REG_DWORD /d "0x3C" /f
+
+)
+)
+)
 
 PAUSE
