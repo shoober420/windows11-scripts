73eebc82950059d9c10b8ba1430aed2bc5b9923b
shoober420 <shoober420@gmail.com>
2025-08-15 15:02:29 -0400

    Update AMDAdrenalinSettingsPERF.bat

--- AMDAdrenalinSettingsPERF.bat
+++ AMDAdrenalinSettingsPERF.bat
@@ -3,6 +3,9 @@ rem # PERFORMANCE
 
 rem # Last key may vary from 0000 to 0001 if dual NVIDIA/AMD machine is used
 
+rem # _NA = Not Available
+rem # 0 = Available / 1 = Not Available
+
 rem # https://forums.guru3d.com/threads/radeonmod-tweak-utility.403389/page-12
 
 rem # REBOOT REQUIRED
