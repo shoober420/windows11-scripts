14cbb634f4d9baf4a76bf221a241d65bfa207133
shoober420 <shoober420@gmail.com>
2025-08-16 09:25:53 -0400

    Update Remove.NETDesktopRuntime6.bat

--- Remove.NETDesktopRuntime6.bat
+++ Remove.NETDesktopRuntime6.bat
@@ -1,6 +1,6 @@
 rem # Uninstall .NET Desktop Runtime 6
 
-winget remove --architecture x86 Microsoft.DotNet.DesktopRuntime6
-winget remove --architecture x64 Microsoft.DotNet.DesktopRuntime6
+winget remove --architecture x86 Microsoft.DotNet.DesktopRuntime.6
+winget remove --architecture x64 Microsoft.DotNet.DesktopRuntime.6
 
 PAUSE
