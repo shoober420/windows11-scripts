210e983efff6bbe9871e96a400be0ff41309f02d
shoober420 <shoober420@gmail.com>
2025-08-24 10:53:34 -0400

    Update GPUTweaks.bat

--- GPUTweaks.bat
+++ GPUTweaks.bat
@@ -493,6 +493,17 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers\Scheduler" /v "En
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers\Scheduler" /v "EnableFlipImmediateSwFlipQueue" /t REG_DWORD /d "1" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers\Scheduler" /v "EnableDirectSubmission" /t REG_DWORD /d "1" /f
 
+setx DXGL_FRAME_LATENCY_WAITABLE_OBJECT "1" /M
+setx DXGI_ALLOW_TEARING "1" /M
+setx DXGI_SWAPCHAIN_FLUSH "0" /M
+setx GPU_MAX_ALLOC_PERCENT "100" /M
+setx GPU_SINGLE_ALLOC_PERCENT "100" /M
+setx MOUSE_RAW_INPUT "1" /M
+setx KEYBOARD_FAST_INIT "1" /M
+setx DX12_AGGRESSIVE_PIPELINE "1" /M
+setx DX12_FORCE_WARP "0" /M
+setx GPU_SYNC_OBJECTS "0" /M
+
 
 
 rem # Intel iGPU Dedicated Segment Size
