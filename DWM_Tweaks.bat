4f1f7c22dfcc46eca865be4e1e6fcdf5b83a5ae7
shoober420 <shoober420@gmail.com>
2025-08-25 12:41:33 -0400

    Update DWM_Tweaks.bat

--- DWM_Tweaks.bat
+++ DWM_Tweaks.bat
@@ -287,6 +287,16 @@ reg add "HKCU\Software\Microsoft\Windows\DWM" /v "DisableDWM" /t REG_DWORD /d "0
 reg add "HKLM\SOFTWARE\Microsoft\Windows\Dwm\ExtendedComposition" /v "DisableDWM" /t REG_DWORD /d "0" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DWM" /v "DisableDWM" /t REG_DWORD /d "0" /f
 
+for /f %%i in ('wmic path Win32_VideoController get PNPDeviceID^| findstr /L "PCI\VEN_"') do (
+        for /f "tokens=3" %%a in ('reg query "HKLM\SYSTEM\ControlSet001\Enum\%%i" /v "Driver"') do (
+                for /f %%i in ('echo %%a ^| findstr "{"') do (
+
+
+
+)
+)
+)
+
 goto :end
 
 :compoff
@@ -307,6 +317,16 @@ reg add "HKLM\Software\Microsoft\Windows\DWM" /v "DisableDWM" /t REG_DWORD /d "1
 reg add "HKCU\Software\Microsoft\Windows\DWM" /v "DisableDWM" /t REG_DWORD /d "1" /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\Dwm\ExtendedComposition" /v "DisableDWM" /t REG_DWORD /d "1" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DWM" /v "DisableDWM" /t REG_DWORD /d "1" /f
+
+for /f %%i in ('wmic path Win32_VideoController get PNPDeviceID^| findstr /L "PCI\VEN_"') do (
+        for /f "tokens=3" %%a in ('reg query "HKLM\SYSTEM\ControlSet001\Enum\%%i" /v "Driver"') do (
+                for /f %%i in ('echo %%a ^| findstr "{"') do (
+
+
+
+)
+)
+)
 
 goto :end
 
