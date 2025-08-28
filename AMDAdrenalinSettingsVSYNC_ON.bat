6aedf7084562ba44619210996a895a82b1c56077
shoober420 <shoober420@gmail.com>
2025-08-28 11:30:01 -0400

    Update AMDAdrenalinSettingsVSYNC_ON.bat

--- AMDAdrenalinSettingsVSYNC_ON.bat
+++ AMDAdrenalinSettingsVSYNC_ON.bat
@@ -8,9 +8,23 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Services\Winmgmt" /v "Start" /t REG_DWORD
 sc config winmgmt start= auto
 net start winmgmt
 
+
+
+rem # https://www.elevenforum.com/members/garlin.5387/
+rem # https://www.elevenforum.com/t/gpu-tweaks-batch-script.30134/post-521530
+if not exist C:\Windows\System32\wbem\WMIC.exe (
+    echo Installing WMIC...
+    DISM /Online /Add-Capability /CapabilityName:WMIC~~~~
+    echo Done.
+)
+
+
+
 for /f %%i in ('wmic path Win32_VideoController get PNPDeviceID^| findstr /L "PCI\VEN_"') do (
         for /f "tokens=3" %%a in ('reg query "HKLM\SYSTEM\ControlSet001\Enum\%%i" /v "Driver"') do (
                 for /f %%i in ('echo %%a ^| findstr "{"') do (
+
+
 
 rem # 0 = Always Off / 3 = Dynamic VSync / 4 = Always On
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i" /v "VSyncControl" /t REG_BINARY /d "3400" /f
