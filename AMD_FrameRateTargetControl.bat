d378431699608e807ecd0d0c440368769f80f3e3
shoober420 <shoober420@gmail.com>
2025-08-28 11:31:02 -0400

    Update AMD_FrameRateTargetControl.bat

--- AMD_FrameRateTargetControl.bat
+++ AMD_FrameRateTargetControl.bat
@@ -10,6 +10,16 @@ net start winmgmt
 
 
 
+rem # https://www.elevenforum.com/members/garlin.5387/
+rem # https://www.elevenforum.com/t/gpu-tweaks-batch-script.30134/post-521530
+if not exist C:\Windows\System32\wbem\WMIC.exe (
+    echo Installing WMIC...
+    DISM /Online /Add-Capability /CapabilityName:WMIC~~~~
+    echo Done.
+)
+
+
+
 @echo off
 
 
