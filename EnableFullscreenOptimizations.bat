ad24a07f0afd7c78daf1d3f34816285f8ab42a74
shoober420 <shoober420@gmail.com>
2025-08-31 12:11:13 -0400

    Update EnableFullscreenOptimizations.bat

--- EnableFullscreenOptimizations.bat
+++ EnableFullscreenOptimizations.bat
@@ -59,6 +59,7 @@ rem # 0xf / 00001111 = Enable EFSE + Flip Model + Scaling + Color Space
 rem # 0x1f / 00011111 = Enable EFSE + Flip Model + Scaling + Color Space + HDR metadata
 rem # 0x3f / 00111111 = Enable EFSE + Flip Model + Scaling + Color Space + HDR metadata + Overlay
 rem # 0x3b / 00111011 = Enable EFSE + Flip Model + Color Space + HDR metadata + Overlay
+rem # 0x1b / 00011011 = Enable EFSE + Flip Model + Color Space + HDR metadata
 
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_EFSEFeatureFlags" /t REG_DWORD /d "0x3b" /f
 
