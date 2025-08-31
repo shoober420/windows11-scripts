93ededea0e3f5b8da7caffc21faaeba1ee1875a5
shoober420 <shoober420@gmail.com>
2025-08-31 08:37:41 -0400

    Update EnableFullscreenOptimizations.bat

--- EnableFullscreenOptimizations.bat
+++ EnableFullscreenOptimizations.bat
@@ -6,6 +6,10 @@ rem # https://en.ittrip.xyz/windows10/registry-tweaks-gamedvr#google_vignette
 
 rem # https://youtu.be/JUXEr5QsaCc?si
 
+rem # DirectX 12 Ultimate (12_2) requires GeForce RTX 20/30 or newer and Radeon RX 6000 or newer
+
+rem # Check MPO support using DXDiag > "Save All Information" and looking in DxDiag.txt file
+
 rem # Check Flip Mode with PresentMon
 rem # https://github.com/GameTechDev/PresentMon
 
