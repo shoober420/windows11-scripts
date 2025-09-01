d040a20e9735b9dfb2a0398a271f93e5a22bd537
shoober420 <shoober420@gmail.com>
2025-09-01 12:18:56 -0400

    Update DisableSoundEnhancements.ps1

--- DisableSoundEnhancements.ps1
+++ DisableSoundEnhancements.ps1
@@ -1,7 +1,7 @@
 # Disable Sound Enhancements
 
 $Key = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\MMDevices\Audio\Render'
-Get-ChildItem $Key -Recurse | Where-Object {(Get-ItemProperty -Path $_.PSPath -Name '{1da5d803-d492-4edd-8c23-e0c0ffee7f0e},5' -ErrorAction SilentlyContinue)} | ForEach-Object {Set-ItemProperty -Path $_.PSPath -Name '{1da5d803-d492-4edd-8c23-e0c0ffee7f0e},5' -Value '1'}
+Get-ChildItem $Key -Recurse | Where-Object {(Get-ItemProperty -Path $_.PSPath -Name '{1da5d803-d492-4edd-8c23-e0c0ffee7f0e},5' -ErrorAction SilentlyContinue)} | ForEach-Object {Set-ItemProperty -Path $_.PSPath -Name '{1da5d803-d492-4edd-8c23-e0c0ffee7f0e},5' -Value '0'}
 
 cmd /k 'pause'
 
