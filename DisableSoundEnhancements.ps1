614f8e13e98ddbbc820cadc8b9bb187afa213b6c
shoober420 <shoober420@gmail.com>
2025-09-01 12:12:13 -0400

    Create DisableSoundEnhancements.ps1

---
+++ DisableSoundEnhancements.ps1
@@ -0,0 +1,6 @@
+# Disable Sound Enhancements
+
+$Key = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\MMDevices\Audio\Render'
+Get-ChildItem $Key -Recurse | Where-Object {(Get-ItemProperty -Path $_.PSPath -Name '{1da5d803-d492-4edd-8c23-e0c0ffee7f0e},5' -ErrorAction SilentlyContinue)} | ForEach-Object {Set-ItemProperty -Path $_.PSPath -Name '{1da5d803-d492-4edd-8c23-e0c0ffee7f0e},5' -Value '1'}
+
+
