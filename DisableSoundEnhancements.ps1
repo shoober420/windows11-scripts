f1c6265264b58af5a6343fda8c5cc13b1bf8f5f3
shoober420 <shoober420@gmail.com>
2025-09-02 04:46:23 -0400

    Update DisableSoundEnhancements.ps1

--- DisableSoundEnhancements.ps1
+++ DisableSoundEnhancements.ps1
@@ -5,6 +5,6 @@
 Start-Process -FilePath "$PSScriptRoot\SetACL.bat" -Wait
 
 $Key = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\MMDevices\Audio\Render'
-Get-ChildItem $Key -Recurse | Where-Object {(Get-ItemProperty -Path $_.PSPath -Name '{1da5d803-d492-4edd-8c23-e0c0ffee7f0e},5' -ErrorAction SilentlyContinue)} | ForEach-Object {Set-ItemProperty -Path $_.PSPath -Name '{1da5d803-d492-4edd-8c23-e0c0ffee7f0e},5' -Value '0'}
+Get-ChildItem $Key -Recurse | Where-Object {(Get-ItemProperty -Path $_.PSPath -Name '{1da5d803-d492-4edd-8c23-e0c0ffee7f0e},5' -ErrorAction SilentlyContinue)} | ForEach-Object {Set-ItemProperty -Path $_.PSPath -Name '{1da5d803-d492-4edd-8c23-e0c0ffee7f0e},5' -Value '1'}
 
 cmd /k 'pause'
