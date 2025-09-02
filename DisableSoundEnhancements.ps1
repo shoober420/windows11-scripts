a1d1cad9f3369e1b96d1d87b8546e417d122cfdc
shoober420 <shoober420@gmail.com>
2025-09-02 04:29:21 -0400

    Update DisableSoundEnhancements.ps1

--- DisableSoundEnhancements.ps1
+++ DisableSoundEnhancements.ps1
@@ -1,7 +1,8 @@
 # Disable Sound Enhancements
 
 # Requires key permissions
-Start-Process -FilePath "powershell.exe" -ArgumentList "-File", cd "%~dp0\SetACL.bat", "-Parameter1", "Value1"
+# Launch SetACL.bat
+Start-Process -FilePath "%~dp0\SetACL.bat" -Wait
 
 $Key = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\MMDevices\Audio\Render'
 Get-ChildItem $Key -Recurse | Where-Object {(Get-ItemProperty -Path $_.PSPath -Name '{1da5d803-d492-4edd-8c23-e0c0ffee7f0e},5' -ErrorAction SilentlyContinue)} | ForEach-Object {Set-ItemProperty -Path $_.PSPath -Name '{1da5d803-d492-4edd-8c23-e0c0ffee7f0e},5' -Value '0'}
