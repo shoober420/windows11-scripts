f65bf20d30123c4dafcf68594ccf4967469282f1
shoober420 <shoober420@gmail.com>
2025-09-08 00:02:30 -0400

    Update EnableSysMain.bat

--- EnableSysMain.bat
+++ EnableSysMain.bat
@@ -1,7 +1,7 @@
 rem # Enable SysMain service (SuperFetch)
 
 rem 0 - Disable SuperFetch / 1 - Enable SuperFetch when the application starts up / 2 - Enable SuperFetch when the device starts up / 3 - Enable SuperFetch when the application or device starts up
-reg add "HKLM\System\CurrentControlSet\Control\Session Manager\Memory Management\PrefetchParameters" /v "EnableSuperfetch" /t REG_DWORD /d "1" /f
+reg add "HKLM\System\CurrentControlSet\Control\Session Manager\Memory Management\PrefetchParameters" /v "EnableSuperfetch" /t REG_DWORD /d "3" /f
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\SysMain" /v "Start" /t REG_DWORD /d "2" /f
 net start SysMain
