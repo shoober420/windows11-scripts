5aa6ece3fa93e7d8337790ab21254a236d1407e9
shoober420 <shoober420@gmail.com>
2025-09-08 00:05:42 -0400

    Update EnablePrefetcher.bat

--- EnablePrefetcher.bat
+++ EnablePrefetcher.bat
@@ -4,6 +4,7 @@ rem # https://en.m.wikipedia.org/wiki/Prefetcher
 
 rem # 0 - Disable Prefetch / 1 - Enable Prefetch when the application starts / 2 - Enable Prefetch when the device starts up / 3 - Enable Prefetch when the application or device starts up (default)
 
+reg add "HKLM\System\CurrentControlSet\Control\Session Manager\Memory Management\PrefetchParameters" /v "EnablePrefetch" /t REG_DWORD /d "3" /f
 reg add "HKLM\System\CurrentControlSet\Control\Session Manager\Memory Management\PrefetchParameters" /v "EnablePrefetcher" /t REG_DWORD /d "3" /f
 
 PAUSE
