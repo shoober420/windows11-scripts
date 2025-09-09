0d9167d5ace0cb63975b80cc8caa5b53dc44ae65
shoober420 <shoober420@gmail.com>
2025-09-09 18:49:00 -0400

    Create TextFileAssociationsVim.bat

---
+++ TextFileAssociationsVim.bat
@@ -0,0 +1,22 @@
+# Remove Default File Associations and assign notepad++ as default text editor
+
+# https://github.com/timabell
+# https://gist.github.com/timabell/bc90e0808ec1cda173ca09225a16e194
+
+Remove-Item -Path "HKCU:Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts" -Recurse -Force
+
+$exts=@(
+        "cfg",
+        "ini",
+	    "log",
+	    "txt")
+echo "## setting up file associations"
+foreach ($ext in $exts){
+	$extfile=$ext+"file"
+	$dotext="." + $ext
+	cmd /c assoc $dotext=$extfile
+	cmd /c "ftype $extfile=""C:\Program Files\Vim\vim91\gvim.exe"" ""%1"""
+	echo ""
+}
+
+cmd /k 'pause'
