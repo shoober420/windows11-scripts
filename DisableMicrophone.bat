6a3d717a740396888fbfe48180486edc431b8e42
shoober420 <shoober420@gmail.com>
2025-09-11 01:54:07 -0400

    Update DisableMicrophone.bat

--- DisableMicrophone.bat
+++ DisableMicrophone.bat
@@ -4,16 +4,20 @@ rem =================================== Windows Settings =======================
 rem ---------------------------------- Privacy & security ----------------------------------
 rem ...................................... Microphone ......................................
 
-rem # Capability Access Manager Service (camsvc) is required for microphone to work
+rem # Capability Access Manager Service (camsvc) MIGHT be required for microphone to work
 
-net stop camsvc
+rem net stop camsvc
 
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\camsvc" /v "Start" /t REG_DWORD /d "4" /f
+rem reg add "HKLM\SYSTEM\CurrentControlSet\Services\camsvc" /v "Start" /t REG_DWORD /d "4" /f
 
 rem # Allow/Deny - Microphone access
 reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\microphone" /v "Value" /t REG_SZ /d "Deny" /f
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\microphone\NonPackaged" /v "Value" /t REG_SZ /d "Deny" /f
 
 rem # Allow/Deny - Let apps access your microphone
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\microphone" /v "Value" /t REG_SZ /d "Deny" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\microphone\NonPackaged" /v "Value" /t REG_SZ /d "Deny" /f
+
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\AppPrivacy" /v "LetAppsAccessMicrophone" /t REG_DWORD /d "2" /f
 
 PAUSE
