4b1a7f931aac3533efb458ac0795ec36204ddf06
shoober420 <shoober420@gmail.com>
2025-09-19 09:34:37 -0400

    Update EnableFullscreenOptimizations.bat

--- EnableFullscreenOptimizations.bat
+++ EnableFullscreenOptimizations.bat
@@ -40,7 +40,7 @@ rem # Multiplane Overlay (MPO)
 
 rem # 0 = FSO: ON / FSE: ON
 rem # 2 = FSO: OFF / FSE: ON
-rem reg add "HKCU\System\GameConfigStore" /v "GameDVR_FSEBehavior" /t REG_DWORD /d "0" /f
+reg add "HKCU\System\GameConfigStore" /v "GameDVR_FSEBehavior" /t REG_DWORD /d "0" /f
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_FSEBehaviorMode" /t REG_DWORD /d "0" /f
 
 rem # Honor User adjusted FSE value
