48df759cc587c4d67312e69a589af507e3c797b5
shoober420 <shoober420@gmail.com>
2025-09-18 23:57:57 -0400

    Update CPU_Frequency.bat

--- CPU_Frequency.bat
+++ CPU_Frequency.bat
@@ -15,7 +15,12 @@ PAUSE
 echo "Enter CPU Max Turbo Speed in MHz"
 set /p MHz=""
 
+rem # Maximum processor frequency (PROCFREQMAX)
 powercfg -setacvalueindex SCHEME_CURRENT 54533251-82be-4824-96c1-47b60b740d00 75b0ae3f-bce0-45a7-8c89-c9611c25e100 %MHz%
+
+rem # Maximum processor frequency for Processor Power Efficiency Class 1 (PROCFREQMAX1)
+powercfg -setacvalueindex SCHEME_CURRENT 54533251-82be-4824-96c1-47b60b740d00 75b0ae3f-bce0-45a7-8c89-c9611c25e101 %MHz%
+
 
 PAUSE
 
