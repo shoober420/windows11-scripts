acc8c8a94b2e7278d208aa8dcb2d74fbac1371f6
shoober420 <shoober420@gmail.com>
2025-09-19 00:05:13 -0400

    Update CPU_Frequency.bat

--- CPU_Frequency.bat
+++ CPU_Frequency.bat
@@ -17,16 +17,22 @@ set /p MHz=""
 
 rem # Maximum processor frequency
 
+reg add "HKLM\SYSTEM\ControlSet001\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\75b0ae3f-bce0-45a7-8c89-c9611c25e100" /v "ValueMax" /t REG_DWORD /d "%MHz%" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\75b0ae3f-bce0-45a7-8c89-c9611c25e100" /v "ValueMax" /t REG_DWORD /d "%MHz%" /f
+reg add "HKLM\SYSTEM\ControlSet001\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\75b0ae3f-bce0-45a7-8c89-c9611c25e100" /v "ValueMin" /t REG_DWORD /d "%MHz%" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\75b0ae3f-bce0-45a7-8c89-c9611c25e100" /v "ValueMin" /t REG_DWORD /d "%MHz%" /f
+
 powercfg -setacvalueindex scheme_current SUB_PROCESSOR PROCFREQMAX %MHz%
 
-powercfg -setacvalueindex SCHEME_CURRENT 54533251-82be-4824-96c1-47b60b740d00 75b0ae3f-bce0-45a7-8c89-c9611c25e100 %MHz%
 
 rem # Maximum processor frequency for Processor Power Efficiency Class 1
 
+reg add "HKLM\SYSTEM\ControlSet001\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\75b0ae3f-bce0-45a7-8c89-c9611c25e101" /v "ValueMax" /t REG_DWORD /d "%MHz%" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\75b0ae3f-bce0-45a7-8c89-c9611c25e101" /v "ValueMax" /t REG_DWORD /d "%MHz%" /f
+reg add "HKLM\SYSTEM\ControlSet001\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\75b0ae3f-bce0-45a7-8c89-c9611c25e101" /v "ValueMin" /t REG_DWORD /d "%MHz%" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\75b0ae3f-bce0-45a7-8c89-c9611c25e101" /v "ValueMin" /t REG_DWORD /d "%MHz%" /f
+
 powercfg -setacvalueindex scheme_current SUB_PROCESSOR PROCFREQMAX1 %MHz%
-
-powercfg -setacvalueindex SCHEME_CURRENT 54533251-82be-4824-96c1-47b60b740d00 75b0ae3f-bce0-45a7-8c89-c9611c25e101 %MHz%
-
 
 PAUSE
 
