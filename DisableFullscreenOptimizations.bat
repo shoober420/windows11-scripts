dc3b1954fb4939cbd31a9fa0366a097e54ed3956
shoober420 <shoober420@gmail.com>
2025-09-20 03:15:02 -0400

    Update DisableFullscreenOptimizations.bat

--- DisableFullscreenOptimizations.bat
+++ DisableFullscreenOptimizations.bat
@@ -1,6 +1,6 @@
 rem # Disable Fullscreen Optimizations (FSO)
 
-rem # https://learn.microsoft.com/en-us/answers/questions/3741077/fullscreen-optimizations-windows-registry?forum=windows-all&referrer=answers
+rem # https://learn.microsoft.com/en-us/answers/questions/3741077/fullscreen-optimizations-windows-registry
 
 rem # https://en.ittrip.xyz/windows10/registry-tweaks-gamedvr#google_vignette
 
@@ -43,6 +43,10 @@ if not exist C:\Windows\System32\wbem\WMIC.exe (
 rem # 0 = FSO: ON / FSE: ON
 rem # 2 = FSO: OFF / FSE: ON
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_FSEBehavior" /t REG_DWORD /d "2" /f
+
+rem # 0 = Applies GameDVR_FSEBehavior to games marked as "High Impact"
+rem # 1 = Applies GameDVR_FSEBehavior to all fullscreen games
+rem # 2 = DISABLE
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_FSEBehaviorMode" /t REG_DWORD /d "2" /f
 
 rem # Honor User adjusted FSE value
@@ -80,9 +84,11 @@ rem # 0 = Enable More FSO Features / 1 = Disable FSO Features
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_DXGIHonorFSEWindowsCompatible" /t REG_DWORD /d "1" /f
 
 rem # Disable GameDVR Overlay
+rem # GameDVR uses system resources for recording and broadcasting while in DSE
 rem # ON = 0 / OFF = 2
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_DSEBehavior" /t REG_DWORD /d "2" /f
-reg add "HKCU\System\GameConfigStore" /v "GameDVR_DSEBehaviorMode" /t REG_DWORD /d "2" /f
+
+rem reg add "HKCU\System\GameConfigStore" /v "GameDVR_DSEBehaviorMode" /t REG_DWORD /d "2" /f
 
 rem # Honor User adjusted DSE value
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_HonorUserDSEBehaviorMode" /t REG_DWORD /d "1" /f
