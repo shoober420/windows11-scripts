274d74c2f52e4079a89c5322e857ff5708b1f882
shoober420 <shoober420@gmail.com>
2025-09-20 03:18:26 -0400

    Update EnableFullscreenOptimizations.bat

--- EnableFullscreenOptimizations.bat
+++ EnableFullscreenOptimizations.bat
@@ -41,7 +41,11 @@ rem # Multiplane Overlay (MPO)
 rem # 0 = FSO: ON / FSE: ON
 rem # 2 = FSO: OFF / FSE: ON
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_FSEBehavior" /t REG_DWORD /d "0" /f
-reg add "HKCU\System\GameConfigStore" /v "GameDVR_FSEBehaviorMode" /t REG_DWORD /d "0" /f
+
+rem # 0 = Applies GameDVR_FSEBehavior to games marked as "High Impact"
+rem # 1 = Applies GameDVR_FSEBehavior to all fullscreen games
+rem # 2 = DISABLE
+reg add "HKCU\System\GameConfigStore" /v "GameDVR_FSEBehaviorMode" /t REG_DWORD /d "1" /f
 
 rem # Honor User adjusted FSE value
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_HonorUserFSEBehaviorMode" /t REG_DWORD /d "1" /f
@@ -57,7 +61,8 @@ reg add "HKCU\System\GameConfigStore" /v "GameDVR_DXGIHonorFSEWindowsCompatible"
 rem # Enable GameDVR Overlay
 rem # ON = 0 / OFF = 2
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_DSEBehavior" /t REG_DWORD /d "0" /f
-reg add "HKCU\System\GameConfigStore" /v "GameDVR_DSEBehaviorMode" /t REG_DWORD /d "0" /f
+
+rem reg add "HKCU\System\GameConfigStore" /v "GameDVR_DSEBehaviorMode" /t REG_DWORD /d "0" /f
 
 rem # Honor User adjusted DSE value
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_HonorUserDSEBehaviorMode" /t REG_DWORD /d "1" /f
