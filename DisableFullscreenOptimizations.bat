364467aa4dc86ad70a08757f38994225bac6b242
shoober420 <shoober420@gmail.com>
2025-09-21 15:12:26 -0400

    Update DisableFullscreenOptimizations.bat

--- DisableFullscreenOptimizations.bat
+++ DisableFullscreenOptimizations.bat
@@ -117,6 +117,12 @@ reg add "HKLM\SOFTWARE\Microsoft\Windows\Dwm" /v "OverlayTestMode" /t REG_DWORD 
 rem # 1 = LOWER FPS
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "EnableMultiPlaneOverlay3DDIs" /t REG_DWORD /d "0" /f
 
+rem # 1 = ENABLES VSYNC
+rem # FORCES HARDWARE COMPOSITED INDEPENDENT FLIP
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers\Scheduler" /v "ForceFlipTrueImmediateMode" /t REG_DWORD /d "0" /f
+
+
+
 rem # Disable optimizations for windowed games
 
 call DisableHDR.bat
