5949348536df284193d8512d97f7893ac6024ec4
shoober420 <shoober420@gmail.com>
2025-09-21 10:51:39 -0400

    Update EnableFullscreenOptimizations.bat

--- EnableFullscreenOptimizations.bat
+++ EnableFullscreenOptimizations.bat
@@ -72,7 +72,7 @@ reg delete "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Managem
 reg delete "HKLM\SOFTWARE\Microsoft\Windows\Dwm" /v "OverlayTestMode" /f
 
 rem # 1 = LOWER FPS
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "EnableMultiPlaneOverlay3DDIs" /t REG_DWORD /d "0" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "EnableMultiPlaneOverlay3DDIs" /t REG_DWORD /d "1" /f
 
 
 
