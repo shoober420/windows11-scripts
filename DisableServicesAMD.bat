e31896e0ba3091b0b0b37e32a4f084afeaef66f9
shoober420 <shoober420@gmail.com>
2025-09-24 22:22:06 -0400

    Update DisableServicesAMD.bat

--- DisableServicesAMD.bat
+++ DisableServicesAMD.bat
@@ -1,5 +1,14 @@
 rem # Disable AMD CPU Services (bloat)
 
+net stop amdfendr
+net stop amdfendrmgr
+net stop "AMD Crash Defender Service"
+net stop AMDXE
+net stop amdpsp
+net stop AMDSAFD
+net stop AUEPLauncher
+net stop amdlog
+
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\amdfendr" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\amdfendrmgr" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\AMD Crash Defender Service" /v "Start" /t REG_DWORD /d "4" /f
