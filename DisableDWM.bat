e24ec5480da34503bcf3191cc5b49bd2ea6084ef
shoober420 <shoober420@gmail.com>
2025-09-27 05:18:55 -0400

    Update DisableDWM.bat

--- DisableDWM.bat
+++ DisableDWM.bat
@@ -9,15 +9,23 @@ ren "C:\Windows\System32\dwm.exe" "dwm.exe.bak"
 
 copy "C:\Windows\System32\cmd.exe" "C:\Windows\System32\dwm.exe"
 
+rem # Fix Black Screen at Login
 takeown /s %computername% /u %username% /f "C:\Windows\System32\Windows.UI.logon.dll"
 icacls "C:\Windows\System32\Windows.UI.logon.dll" /grant:r %username%:F
 rem taskkill /im Windows.UI.logon.dll /f
 rem del "C:\Windows\System32\Windows.UI.logon.dll" /s /f /q
 ren "C:\Windows\System32\Windows.UI.logon.dll" "Windows.UI.logon.dll.bak"
 
+rem # Fix Mouse
+takeown /s %computername% /u %username% /f "C:\Windows\System32\DWMInit.dll"
+icacls "C:\Windows\System32\DWMInit.dll" /grant:r %username%:F
+rem taskkill /im DWMInit.dll /f
+rem del "C:\Windows\System32\DWMInit.dll" /s /f /q
+ren "C:\Windows\System32\DWMInit.dll" "DWMInit.dll.bak"
+
 cd "%~dp0"
 
-rem call DisableShellInfrastructureHost.bat
+call DisableShellInfrastructureHost.bat
 
 cd "%~dp0"
 
