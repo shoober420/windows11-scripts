49562510fb25f395aafe1301acd3fe86ff88c3f8
shoober420 <shoober420@gmail.com>
2025-09-27 13:08:03 -0400

    Update DisableDWM.bat

--- DisableDWM.bat
+++ DisableDWM.bat
@@ -90,6 +90,33 @@ goto :end
 
 
 
+echo.
+echo Resources folder
+echo.
+echo 1. Enable Classic Theme
+echo 2. Disable Classic Theme
+echo 3. SKIP
+echo C. Cancel
+echo.
+choice /c 123C /m "Choose an option :"
+
+if 4 EQU %ERRORLEVEL% (
+   echo User chose to cancel.
+) else if 3 EQU %ERRORLEVEL% (
+   call :zkeppp
+) else if 2 EQU %ERRORLEVEL% (
+   call :classof
+) else if 1 EQU %ERRORLEVEL% (
+   call :classon
+) else if 0 EQU %ERRORLEVEL% (
+   echo User bailed out.
+)
+
+goto :eof
+
+:classon
+echo User chose Enable Classic Theme
+
 rem # Disable Resources
 rem # Enable Classic Theme
 takeown /s %computername% /u %username% /f "C:\Windows\Resources"
@@ -97,10 +124,74 @@ icacls "C:\Windows\Resources" /grant:r %username%:F
 rem del "C:\Windows\Resources" /s /f /q
 ren "C:\Windows\Resources" "Resourcesbak"
 
-cd "%~dp0"
+goto :end
 
+:classoff
+echo User chose Disable Classic Theme
+
+rem # Enable Resources
+rem # Disable Classic Theme
+takeown /s %computername% /u %username% /f "C:\Windows\Resources"
+icacls "C:\Windows\Resources" /grant:r %username%:F
+rem del "C:\Windows\Resources" /s /f /q
+ren "C:\Windows\Resourcesbak" "Resources"
+
+goto :end
+
+:zkeppp
+echo User chose SKIP
+
+goto :end
+
+:end
+
+
+
+echo.
+echo Shell Infrastructure Host (sihost.exe)
+echo.
+echo 1. Disable Shell Infrastructure Host (sihost.exe)
+echo 2. Enable Shell Infrastructure Host (sihost.exe)
+echo 3. SKIP
+echo C. Cancel
+echo.
+choice /c 123C /m "Choose an option :"
+
+if 4 EQU %ERRORLEVEL% (
+   echo User chose to cancel.
+) else if 3 EQU %ERRORLEVEL% (
+   call :zkepppp
+) else if 2 EQU %ERRORLEVEL% (
+   call :sion
+) else if 1 EQU %ERRORLEVEL% (
+   call :sioff
+) else if 0 EQU %ERRORLEVEL% (
+   echo User bailed out.
+)
+
+goto :eof
+
+:sioff
+echo User chose Disable Shell Infrastructure Host (sihost.exe)
+
+cd "%~dp0"
 call DisableShellInfrastructureHost.bat
 
+goto :end
+
+:sion
+echo User chose Enable Shell Infrastructure Host (sihost.exe)
+
 cd "%~dp0"
+call EnableShellInfrastructureHost.bat
+
+goto :end
+
+:zkepppp
+echo User chose SKIP
+
+goto :end
+
+:end
 
 PAUSE
