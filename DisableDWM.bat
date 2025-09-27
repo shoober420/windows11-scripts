485da1acabd1d502fdcd599d8a5d02d36c71a359
shoober420 <shoober420@gmail.com>
2025-09-27 11:32:07 -0400

    Update DisableDWM.bat

--- DisableDWM.bat
+++ DisableDWM.bat
@@ -29,6 +29,8 @@ rem taskkill /im DWMInit.dll /f
 rem del "C:\Windows\System32\DWMInit.dll" /s /f /q
 ren "C:\Windows\System32\DWMInit.dll" "DWMInit.dll.bak"
 
+
+
 @echo off
 
 
@@ -38,30 +40,55 @@ echo UWP Apps (SystemApps)
 echo.
 echo 1. Disable UWP Apps
 echo 2. Enable UWP Apps
+echo 3. SKIP
 echo C. Cancel
 echo.
 choice /c 123C /m "Choose an option :"
 
-if 3 EQU %ERRORLEVEL% (
+if 4 EQU %ERRORLEVEL% (
    echo User chose to cancel.
+) else if 3 EQU %ERRORLEVEL% (
+   call :zkepp
 ) else if 2 EQU %ERRORLEVEL% (
-   call :frtcoff
+   call :uwpon
 ) else if 1 EQU %ERRORLEVEL% (
-   call :fr
+   call :uwpoff
 ) else if 0 EQU %ERRORLEVEL% (
    echo User bailed out.
 )
 
 goto :eof
 
-:frtcon
-echo User chose FRTC ON
+:uwpoff
+echo User chose Disable UWP Apps
 
 rem # Disable UWP apps
 takeown /s %computername% /u %username% /f "C:\Windows\SystemApps"
 icacls "C:\Windows\SystemApps" /grant:r %username%:F
 rem del "C:\Windows\SystemApps" /s /f /q
-ren "C:\Windows\SystemApps" "SystemAppsbak"
+ren "C:\Windows\SystemAppsbak" "SystemApps"
+
+goto :end
+
+:uwpon
+echo User chose Enable UWP Apps
+
+rem # Enable UWP apps
+takeown /s %computername% /u %username% /f "C:\Windows\SystemApps"
+icacls "C:\Windows\SystemApps" /grant:r %username%:F
+rem del "C:\Windows\SystemApps" /s /f /q
+ren "C:\Windows\SystemAppsbak" "SystemApps"
+
+goto :end
+
+:zkepp
+echo User chose SKIP
+
+goto :end
+
+:end
+
+
 
 rem # Disable Resources
 rem # Enable Classic Theme
